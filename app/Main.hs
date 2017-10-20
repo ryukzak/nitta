711b8cc859a2c3d930eed053b3f676303f6b1d23
Aleksandr Penskoi <aleksandr.penskoi@gmail.com>
2017-10-20 14:41:35 +0300

    Reworked Main.hs. In current state timeline works fine.

--- app/Main.hs
+++ app/Main.hs
@@ -41,68 +41,130 @@ import           Text.StringTemplate
 
 type T = TaggedTime String Int
 
-fram = PU (def :: Fram String T)
-accum = PU (def :: A.Accum String T)
-
-net = busNetwork
-  [ ("fram1", fram)
-  , ("fram2", fram)
-  , ("accum", accum)
-  ]
-  $ array (0, 19) [ (19, [("accum", S (A.NEG  :: Signal (A.Accum String T)))])
-                  , (18, [("accum", S (A.LOAD :: Signal (A.Accum String T)))])
-                  , (17, [("accum", S (A.INIT :: Signal (A.Accum String T)))])
-                  , (16, [("accum", S (A.OE   :: Signal (A.Accum String T)))])
-
-                  , (15, [("fram1", S (OE :: Signal (Fram String T)))])
-                  , (14, [("fram1", S (WR :: Signal (Fram String T)))])
-                  , (13, [])
-                  , (12, [])
-
-                  , (11, [("fram1", S (ADDR 3 :: Signal (Fram String T)))])
-                  , (10, [("fram1", S (ADDR 2 :: Signal (Fram String T)))])
-                  , ( 9, [("fram1", S (ADDR 1 :: Signal (Fram String T)))])
-                  , ( 8, [("fram1", S (ADDR 0 :: Signal (Fram String T)))])
-
-                  , ( 7, [("fram2", S (OE :: Signal (Fram String T)))])
-                  , ( 6, [("fram2", S (WR :: Signal (Fram String T)))])
-                  , ( 5, [])
-                  , ( 4, [])
-
-                  , ( 3, [("fram2", S (ADDR 3 :: Signal (Fram String T)))])
-                  , ( 2, [("fram2", S (ADDR 2 :: Signal (Fram String T)))])
-                  , ( 1, [("fram2", S (ADDR 1 :: Signal (Fram String T)))])
-                  , ( 0, [("fram2", S (ADDR 0 :: Signal (Fram String T)))])
-                  ]
 
 
-dataFlow = Stage
-  [ Actor $ FB.framInput 0 $ O [ "cond", "cond'" ]
-  , Actor $ FB.framInput 1 $ O [ "x1", "x2" ]
-  , Actor $ FB.framOutput 2 $ I "cond'"
-  , Paths "cond"
-    [ (0, Stage [ Actor $ FB.reg (I "x1") $ O ["y1"], Actor $ FB.framOutput 10 $ I "y1" ])
-    , (1, Stage [ Actor $ FB.reg (I "x2") $ O ["y2"], Actor $ FB.framOutput 11 $ I "y2" ])
-    ]
-  ]
+-- | Пример работы с ветвящимся временем.
+--
+-- - TODO: планирование вычислительного процесса (пропуск переменных, колизии потоков управления и
+--   данных, привязок функциональных блоков).
+-- - TODO: генерация машинного кода.
+-- - TODO: генерация аппаратуры.
+-- - TODO: testbench.
+bushExample
+  = let fram = PU (def :: Fram String T)
+        accum = PU (def :: A.Accum String T)
+        net = busNetwork
+          [ ("fram1", fram)
+          , ("fram2", fram)
+          , ("accum", accum)
+          ]
+          $ array (0, 19) [ (19, [("accum", S (A.NEG  :: Signal (A.Accum String T)))])
+                          , (18, [("accum", S (A.LOAD :: Signal (A.Accum String T)))])
+                          , (17, [("accum", S (A.INIT :: Signal (A.Accum String T)))])
+                          , (16, [("accum", S (A.OE   :: Signal (A.Accum String T)))])
 
-net' = bindAll (net :: BusNetwork String String T) $ functionalBlocks dataFlow
+                          , (15, [("fram1", S (OE :: Signal (Fram String T)))])
+                          , (14, [("fram1", S (WR :: Signal (Fram String T)))])
+                          , (13, [])
+                          , (12, [])
+
+                          , (11, [("fram1", S (ADDR 3 :: Signal (Fram String T)))])
+                          , (10, [("fram1", S (ADDR 2 :: Signal (Fram String T)))])
+                          , ( 9, [("fram1", S (ADDR 1 :: Signal (Fram String T)))])
+                          , ( 8, [("fram1", S (ADDR 0 :: Signal (Fram String T)))])
+
+                          , ( 7, [("fram2", S (OE :: Signal (Fram String T)))])
+                          , ( 6, [("fram2", S (WR :: Signal (Fram String T)))])
+                          , ( 5, [])
+                          , ( 4, [])
+
+                          , ( 3, [("fram2", S (ADDR 3 :: Signal (Fram String T)))])
+                          , ( 2, [("fram2", S (ADDR 2 :: Signal (Fram String T)))])
+                          , ( 1, [("fram2", S (ADDR 1 :: Signal (Fram String T)))])
+                          , ( 0, [("fram2", S (ADDR 0 :: Signal (Fram String T)))])
+                          ]
+        dataFlow = Stage
+          [ Actor $ FB.framInput 0 $ O [ "cond", "cond'" ]
+          , Actor $ FB.framInput 1 $ O [ "x1", "x2" ]
+          , Actor $ FB.framOutput 2 $ I "cond'"
+          , Paths "cond"
+            [ (0, Stage [ Actor $ FB.reg (I "x1") $ O ["y1"], Actor $ FB.framOutput 10 $ I "y1" ])
+            , (1, Stage [ Actor $ FB.reg (I "x2") $ O ["y2"], Actor $ FB.framOutput 11 $ I "y2" ])
+            ]
+          ]
+        net' = bindAll (net :: BusNetwork String String T) $ functionalBlocks dataFlow
+        initialBranch = Branch net' (dataFlow2controlFlow dataFlow) Nothing []
+        Branch{ topPU=pu } = foldl (\b _ -> naive def b) initialBranch (take 50 $ repeat ())
+    in pu
+
+
+-- | Пример работы с единым временем.
+branchExample
+  = let fram = PU (def :: Fram String T)
+        accum = PU (def :: A.Accum String T)
+        net = busNetwork
+          [ ("fram1", fram)
+          , ("fram2", fram)
+          , ("accum", accum)
+          ]
+          $ array (0, 19) [ (19, [("accum", S (A.NEG  :: Signal (A.Accum String T)))])
+                          , (18, [("accum", S (A.LOAD :: Signal (A.Accum String T)))])
+                          , (17, [("accum", S (A.INIT :: Signal (A.Accum String T)))])
+                          , (16, [("accum", S (A.OE   :: Signal (A.Accum String T)))])
+
+                          , (15, [("fram1", S (OE :: Signal (Fram String T)))])
+                          , (14, [("fram1", S (WR :: Signal (Fram String T)))])
+                          , (13, [])
+                          , (12, [])
+
+                          , (11, [("fram1", S (ADDR 3 :: Signal (Fram String T)))])
+                          , (10, [("fram1", S (ADDR 2 :: Signal (Fram String T)))])
+                          , ( 9, [("fram1", S (ADDR 1 :: Signal (Fram String T)))])
+                          , ( 8, [("fram1", S (ADDR 0 :: Signal (Fram String T)))])
+
+                          , ( 7, [("fram2", S (OE :: Signal (Fram String T)))])
+                          , ( 6, [("fram2", S (WR :: Signal (Fram String T)))])
+                          , ( 5, [])
+                          , ( 4, [])
+
+                          , ( 3, [("fram2", S (ADDR 3 :: Signal (Fram String T)))])
+                          , ( 2, [("fram2", S (ADDR 2 :: Signal (Fram String T)))])
+                          , ( 1, [("fram2", S (ADDR 1 :: Signal (Fram String T)))])
+                          , ( 0, [("fram2", S (ADDR 0 :: Signal (Fram String T)))])
+                          ]
+        alg = [ FB.framInput 3 $ O [ "a"
+                                   , "d"
+                                   ]
+              , FB.framInput 4 $ O [ "b"
+                                   , "c"
+                                   , "e"
+                                   ]
+              , FB.reg (I "a") $ O ["x"]
+              , FB.reg (I "b") $ O ["y"]
+              , FB.reg (I "c") $ O ["z"]
+              , FB.framOutput 5 $ I "x"
+              , FB.framOutput 6 $ I "y"
+              , FB.framOutput 7 $ I "z"
+              , FB.framOutput 0 $ I "sum"
+              , FB.loop (O ["f"]) $ I "g"
+              , FB.reg (I "f") $ O ["g"]
+              , FB $ FB.Add (I "d") (I "e") (O ["sum"])
+              ]
+        dataFlow = Stage $ map Actor alg
+        net' = bindAll (net :: BusNetwork String String T) $ functionalBlocks dataFlow
+        initialBranch = Branch net' (dataFlow2controlFlow dataFlow) Nothing []
+        Branch{ topPU=pu } = foldl (\b _ -> naive def b) initialBranch (take 50 $ repeat ())
+    in pu
 
 
 ---------------------------------------------------------------------------------
 
 
 main = do
-  let compiler = Branch net' (dataFlow2controlFlow dataFlow) Nothing []
-  let p = foldl (\comp _ -> naive def comp) compiler (take 50 $ repeat ())
-  let Branch{ topPU=pu } = p
-  -- let BranchingInProgress{ currentBranch=Fork{ topPU=pu } } = p
-
-  -- putStrLn $ show $ graph dataFlow
-  timeline "resource/data.json" pu
-  -- r <- testBench pu ([] :: [(String, Int)])
-  -- if r then putStrLn "Success"
-  -- else putStrLn "Fail"
+  timeline "resource/data.json" branchExample
+  r <- testBench branchExample ([] :: [(String, Int)])
+  if r then putStrLn "Success"
+  else putStrLn "Fail"
   print "ok"
 
 getPU puTitle net
