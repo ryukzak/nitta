75df87de57e457d46130fbc9b1d99ed8cbd07ea8
Dmitry Emelyanov <dmitriy.emelyanov.de@gmail.com>
2018-01-10 20:27:33 +0300

    Changing the buffer and pu_spi

--- hdl/spi/pu_buffer.v
+++ hdl/spi/pu_buffer.v
@@ -1,54 +1,54 @@
 module pu_buffer
   #( parameter DATA_WIDTH  = 8
-  ,  parameter BUF_SIZE    = 6
+  ,  parameter BUF_SIZE    = 8
   )
   ( input                         clk
-  , input                         ready
+  , input                         oe  
+  , input                         wr  
   , input                         rst
   , input       [DATA_WIDTH-1:0]  data_in
   , output reg  [DATA_WIDTH-1:0]  data_out
+  , output                        buffer_full
   );
 
 localparam ADDR_WIDTH  = $clog2( BUF_SIZE );
 
 reg [DATA_WIDTH-1:0] memory [0:BUF_SIZE-1];
-reg [DATA_WIDTH-1:0]        next_word_to_send;
-reg [ADDR_WIDTH-1:0]        addres;
-reg                         state;
-reg addr_incremented;
-
-localparam STATE_START_READY  = 0;
-localparam STATE_STOP_READY   = 1;
+reg [ADDR_WIDTH-1:0]          addres_oe; 
+reg [ADDR_WIDTH:0]            addres_wr; // +1 на переполнение
+reg addr_incremented_oe;
+reg addr_incremented_wr;
 
 always @(posedge clk or posedge rst) begin
   if ( rst ) begin
-    addres <= 0;
-    addr_incremented <= 0;
-    state <= STATE_START_READY;
+    addres_oe <= 0;
+    addres_wr <= 0;
+    addr_incremented_oe <= 0;
+    addr_incremented_wr <= 0;
   end
   else begin
-    if ( ready ) begin
-      case ( state )
-        STATE_START_READY: begin
-          next_word_to_send <= memory [ addres ]; // Save in the first line
-          memory [ addres + 1 ] <= data_in;	// Write in the second line
-          state <= STATE_STOP_READY;
-        end
-        STATE_STOP_READY: begin
-          data_out <= next_word_to_send;
-          state <= STATE_START_READY;
-        end
-      endcase
-    end
-    if ( ready ) begin
-      if ( !addr_incremented ) begin
-        addr_incremented <= 1;
-        addres <= addres + 1;
+    if ( wr ) begin
+      memory [ addres_wr ] <= data_in;
+      addr_incremented_wr = 1;
+      addres_wr <= { 1'b0, addres_wr[ ADDR_WIDTH-1:0 ]}; 
+    end else begin 
+      if ( addr_incremented_wr ) begin
+        addres_wr <= addres_wr + 1'b1;
+        addr_incremented_wr = 0;
       end
+    end
+    if ( oe ) begin
+      data_out <= memory [ addres_oe ];
+      addr_incremented_oe <= 1;
     end else begin
-      addr_incremented <= 0;
+      if ( addr_incremented_oe ) begin
+        addres_oe <= addres_oe + 1'b1;
+        addr_incremented_oe = 0;
+      end
     end
   end
 end
+
+assign buffer_full = { addres_wr [ADDR_WIDTH] };
 
 endmodule
--- hdl/spi/pu_spi.v
+++ hdl/spi/pu_spi.v
@@ -1,13 +1,18 @@
 module pu_spi
-  #( parameter DATA_WIDTH = 8
- ,   parameter SCLK_HALFPERIOD = 1
+  #( parameter DATA_WIDTH       = 8
+,    parameter NUMBER_OF_BUFFER = 3 
    )
   ( input             clk
   , input             rst
   // system interface
-  , input  [DATA_WIDTH-1:0] data_in
-  , output [DATA_WIDTH-1:0] data_out
+  , input  [DATA_WIDTH-1:0] data_in  
+  , output [DATA_WIDTH-1:0] data_out 
   , output                  ready
+  , output            oe
+  , output            wr
+  , input             cycle // Сигнал о начале вычислительного цикла
+  , output reg        start // Выходной сигнал о начале цикла передачи данных
+  , output reg        stop  // Выходной сигнал о конце цикла передачи данных
   // SPI interface
   , output wire       miso
   , output            mosi
@@ -15,30 +20,48 @@ module pu_spi
   , input             cs
   );
 
-assign slave_spi_mosi = mosi;
-assign slave_spi_sclk = sclk;
-assign slave_spi_cs   = cs;
-assign slave_ready    = ready;
-assign miso = slave_spi_miso;
+localparam ADDR_WIDTH_BUF  = $clog2( NUMBER_OF_BUFFER );
 
-spi_slave_driver pu_spi_inner ( 
-    .clk(clk)
-  , .rst(rst)  
-  , .data_in(data_in)
-  , .ready(slave_ready)
-  , .data_out(data_out)  
-  , .mosi(slave_spi_mosi)
-  , .miso(slave_spi_miso)
-  , .sclk(slave_spi_sclk)
-  , .cs(slave_spi_cs)
-  );
+wire [DATA_WIDTH-1:0] data_in_pu_spi   [ADDR_WIDTH_BUF:0];
+wire [DATA_WIDTH-1:0] data_out_pu_spi  [ADDR_WIDTH_BUF:0];
+reg                   flag;
+reg [1:0]             count_cycle;
 
-pu_buffer buffer (
-    .clk(clk)
-  , .rst(rst)
-  , .ready(slave_ready)
-  , .data_in(data_out)
-  , .data_out(data_in)
-);
+genvar index;
+generate  
+  for ( index = 0 ; index < NUMBER_OF_BUFFER ; index = index + 1 )  
+  begin: gen_buffer
+    pu_buffer buffer (
+      .clk(clk)
+    , .rst(rst) 
+    , .oe(oe)
+    , .wr(wr)
+    , .data_in(data_out_pu_spi[index])
+    , .data_out(data_in_pu_spi[index])
+    );
+  end
+endgenerate 
+
+always @(posedge clk or posedge rst) begin
+  if ( rst ) begin
+    flag <= 0;
+  end else begin
+    if ( cycle ) begin
+      flag = 1;
+    end else begin
+      if ( flag ) begin
+
+        // Организовать работу
+        // flag = 0;
+        
+      end
+    end
+  end
+end
+
+// Переключение буферов (count_cycle используется вместо переменной index)
+always @(posedge cycle) begin
+  count_cycle <= count_cycle + 1'b1;
+end
 
 endmodule
