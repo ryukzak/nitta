d88475f721fbcf34c6b11ba17430d738f2324c75
Dmitry Emelyanov <dmitriy.emelyanov.de@gmail.com>
2018-01-11 21:08:29 +0300

    Changed pu_buffer and pu_spi under nitta interface

--- hdl/spi/pu_buffer_tb.v
+++ hdl/spi/pu_buffer_tb.v
@@ -1,78 +1,108 @@
-`timescale 1ns/1ps
+`timescale 1 ms/ 1 ms
 module pu_buffer_tb
-#( parameter DATA_WIDTH  = 8
- , parameter BUF_SIZE    = 6
- )
+  #( parameter DATA_WIDTH       = 8
+  ,  parameter ATTR_WIDTH       = 4
+  ,  parameter VALID            = 1
+  ,  parameter BUFFER_SIZE      = 3
+   )
 ();
 
-reg clk;
-reg ready;
-reg rst;
+reg  clk;
+reg  rst;
 
-reg  [DATA_WIDTH-1:0] data_in;
-wire [DATA_WIDTH-1:0] data_out;
+reg  [13:0] signals_out;
+reg  [DATA_WIDTH-1:0] data_bus;
+wire [DATA_WIDTH-1:0] spi_data_out;
+reg  [ATTR_WIDTH-1:0] attr_bus;
+wire [ATTR_WIDTH-1:0] spi_attr_out;
 
-pu_buffer #( .DATA_WIDTH(DATA_WIDTH)
-           , .BUF_SIZE(BUF_SIZE)
-					 ) buffer
-  ( .clk(clk)
-	, .ready(ready)
-	, .rst(rst)
-	, .data_in(data_in)
-	, .data_out(data_out)
-  );
+reg  nitta_cycle;
+wire spi_start;
+wire spi_stop;
+
+wire spi_mosi;
+wire spi_miso;
+reg  spi_sclk;
+reg  spi_cs;
+
+pu_spi pu_spi_test(
+
+	.clk(clk)
+,	.rst(rst)
+
+,	.signal_wr( signals_out[ 12 ] )
+,	.data_in( data_bus )
+, .attr_in( attr_bus )
+
+,	.signal_oe( signals_out[ 13 ] )
+, .data_out( spi_data_out )
+, .attr_out( spi_attr_out )
+
+, .flag_cycle( nitta_cycle )
+, .flag_start( spi_start )
+, .flag_stop( spi_stop )
+
+, .mosi( spi_mosi )
+, .miso( spi_miso )
+, .sclk( spi_sclk )
+, .cs( spi_cs )
+
+);
 
 always begin
   #5 clk = ~clk;
-end
+end 
 
-initial
+task Nop;
+    begin
+       signals_out[ 12 ] <= 0;
+			 signals_out[ 13 ] <= 0;
+			 nitta_cycle <= 0; 
+    end
+endtask
+
+task Send;
+		input [DATA_WIDTH-1:0] data;
+    begin
+			data_bus <= data;
+      signals_out[ 12 ] <= 1;
+			signals_out[ 13 ] <= 0;
+    end
+endtask
+
+task Receive;
+    begin
+      signals_out[ 13 ] <= 1;  
+			signals_out[ 12 ] <= 0;
+    end
+endtask
+
+initial 
   begin
-		$display("Start");
-		clk = 0; rst = 1;
-		data_in = 0; ready = 0;
+	$display("Start");
 
-		buffer.memory[0] = 0;
-		buffer.memory[1] = 1;
-		buffer.memory[2] = 2;
-		buffer.memory[3] = 3;
+	clk = 0;       @(posedge clk); 
 
-		repeat (4) @(posedge clk);
-		rst = 0; @(posedge clk);
+	rst = 1;       @(posedge clk); 
+	rst = 0;       @(posedge clk);
 
-		data_in = 2; ready = 1; @(posedge clk);
-		@(posedge clk);
-		ready	= 0; @(posedge clk);
-		@(posedge clk);@(posedge clk);@(posedge clk);
-		data_in = 3;
-		ready   = 1; @(posedge clk); @(posedge clk);
-		ready	= 0; @(posedge clk);
-		@(posedge clk);@(posedge clk);@(posedge clk);
-		data_in = 4;
-		ready   = 1; @(posedge clk); @(posedge clk);
-		ready	= 0; @(posedge clk);
-		@(posedge clk);@(posedge clk);@(posedge clk);
-		data_in = 5;
-		ready   = 1; @(posedge clk); @(posedge clk);
-		ready	= 0; @(posedge clk);
-		@(posedge clk);@(posedge clk);@(posedge clk);
-		data_in = 6;
-		ready   = 1; @(posedge clk); @(posedge clk);
-		ready	= 0; @(posedge clk);
+	Nop();		  	 @(posedge clk);
+	Send(2);       @(posedge clk); @(posedge clk);
+  Send(2);       @(posedge clk); @(posedge clk);
+	Nop();		  	 @(posedge clk);
+	Receive();     @(posedge clk);@(posedge clk);
+	Send(3);       @(posedge clk); @(posedge clk);
+	Receive();     @(posedge clk);@(posedge clk);
+	Nop();
 
-
-		repeat(10) @(posedge clk);
-		$finish;
+	repeat(10) @(posedge clk); $finish;
   end
 
 initial
   begin
-		$dumpfile("pu_buffer_tb.vcd");
-		$dumpvars(-1, pu_buffer_tb);
-  end
+	$dumpfile("pu_spi_tb.vcd");
+	$dumpvars(-1, pu_buffer_tb);
+	$display("finish");
+  end 
 
 endmodule
-
-//  iverilog -o buffer buffer.v buffer_tb.v
-//  vvp buffer
-//  gtkwave buffer_tb.vcd
--- hdl/spi/pu_spi.v
+++ hdl/spi/pu_spi.v
@@ -1,67 +1,116 @@
 module pu_spi
   #( parameter DATA_WIDTH       = 8
-,    parameter NUMBER_OF_BUFFER = 3 
+  ,  parameter ATTR_WIDTH       = 4
+  ,  parameter VALID            = 1
+  ,  parameter BUFFER_SIZE      = 3
    )
-  ( input             clk
-  , input             rst
+  ( input                   clk
+  , input                   rst
+
   // system interface
-  , input  [DATA_WIDTH-1:0] data_in  
-  , output [DATA_WIDTH-1:0] data_out 
-  , output                  ready
-  , output            oe
-  , output            wr
-  , input             cycle // Сигнал о начале вычислительного цикла
-  , output reg        start // Выходной сигнал о начале цикла передачи данных
-  , output reg        stop  // Выходной сигнал о конце цикла передачи данных
+  , input  wire                  signal_wr 
+  , input  wire [DATA_WIDTH-1:0] data_in  
+  , input  wire [ATTR_WIDTH-1:0] attr_in 
+
+  , input  wire                  signal_oe 
+  , output reg  [DATA_WIDTH-1:0] data_out
+  , output reg  [ATTR_WIDTH-1:0] attr_out 
+
+  , input  wire       flag_cycle 
+  , output reg        flag_start
+  , output reg        flag_stop
+
   // SPI interface
   , output wire       miso
-  , output            mosi
-  , input             sclk
-  , input             cs
-  );
+  , output wire       mosi
+  , input  wire       sclk
+  , input  wire       cs
+);
 
-localparam ADDR_WIDTH_BUF  = $clog2( NUMBER_OF_BUFFER );
+localparam BUFFER_SIZE_WIDTH = $clog2( BUFFER_SIZE );
+reg  [BUFFER_SIZE_WIDTH-1:0] actual_buffer_sending_size;
+reg  [BUFFER_SIZE_WIDTH-1:0] actual_buffer_receiving_size;
 
-wire [DATA_WIDTH-1:0] data_in_pu_spi   [ADDR_WIDTH_BUF:0];
-wire [DATA_WIDTH-1:0] data_out_pu_spi  [ADDR_WIDTH_BUF:0];
-reg                   flag;
-reg [1:0]             count_cycle;
+wire [DATA_WIDTH-1:0] data_out_spi;
+reg  [DATA_WIDTH-1:0] data_in_spi;
+wire buffer_full;
+reg flag_wr;
+reg flag_oe;
+reg work_process;
 
-genvar index;
-generate  
-  for ( index = 0 ; index < NUMBER_OF_BUFFER ; index = index + 1 )  
-  begin: gen_buffer
-    pu_buffer buffer (
-      .clk(clk)
-    , .rst(rst) 
-    , .oe(oe)
-    , .wr(wr)
-    , .data_in(data_out_pu_spi[index])
-    , .data_out(data_in_pu_spi[index])
-    );
-  end
-endgenerate 
+pu_buffer buffer(
+  .clk(clk)
+, .rst(rst)
+, .oe(signal_oe)
+, .wr(signal_wr)
+, .data_in(data_in_spi)
+, .data_out(data_out_spi)
+, .buffer_full(buffer_full)
+); 
 
-always @(posedge clk or posedge rst) begin
+always @( posedge clk or posedge rst ) begin
   if ( rst ) begin
-    flag <= 0;
+    flag_start <= 0;
+    flag_stop  <= 0;
+    actual_buffer_sending_size <= 0; 
+    actual_buffer_receiving_size <= 0;
+    flag_wr <= 0;
+    flag_oe <= 0; 
+    work_process <= 0;
   end else begin
-    if ( cycle ) begin
-      flag = 1;
-    end else begin
-      if ( flag ) begin
-
-        // Организовать работу
-        // flag = 0;
-        
-      end
+    // Пришел сигнал на запись и буфер не заполнен
+    if ( signal_wr && ~buffer_full ) begin
+      data_in_spi <= data_in;
+      if ( flag_start ) begin        
+        actual_buffer_sending_size <= actual_buffer_sending_size + 1;
+      end 
+    end else if ( signal_wr && buffer_full ) begin 
+      // Если буфер заполнен и есть сигнал на запись новых значений
+    end
+  end
+  // Пришел сигнал на чтение и буфер не пуст
+  if ( signal_oe && !(actual_buffer_sending_size == 0) ) begin
+    data_out <= data_out_spi;
+    if ( flag_start ) begin      
+      actual_buffer_receiving_size <= actual_buffer_receiving_size + 1;
     end
   end
 end
 
-// Переключение буферов (count_cycle используется вместо переменной index)
-always @(posedge cycle) begin
-  count_cycle <= count_cycle + 1'b1;
+always @( posedge clk ) begin
+  // Для синхронизации. Работаем по сигналу flag_cycle
+  if ( flag_cycle ) begin
+    work_process <= 1;
+  end
+  if ( work_process ) begin
+    // Тута вся работа
+  end
+end
+
+// Переделать логику. Дописать flag_stop
+always @( posedge clk ) begin
+  if ( signal_wr && signal_oe ) begin
+   // Случай если одновременно Send and Recive
+  end else if ( signal_wr ) begin
+    if ( !flag_wr ) begin
+      flag_start <= 1;
+      flag_oe <= 0;
+      flag_wr <= 1;
+    end else begin
+      flag_start <= 0;
+    end
+  end else if ( signal_oe ) begin
+    if ( !flag_oe ) begin
+      flag_start <= 1;
+      flag_oe <= 1;
+    end else begin
+      flag_start <= 0;
+    end
+  end else begin
+    flag_start <= 0;
+    flag_wr <= 0;
+    flag_oe <= 0;
+  end
 end
 
 endmodule
