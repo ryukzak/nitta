98faadb9b72bdeafe88943a1fb9d428eee427b7f
Aleksandr Penskoi <aleksandr.penskoi@gmail.com>
2018-03-14 17:33:25 +0300

    Migrated to servant 0.13. Refactored API.hs. js is broken.

--- src/NITTA/API.hs
+++ src/NITTA/API.hs
@@ -24,7 +24,7 @@ import           GHC.Generics
 import           ListT                  (toList)
 import           NITTA.Compiler
 import           NITTA.Flows
-import           NITTA.Types            hiding (Synthesis)
+import           NITTA.Types            hiding (Synthesis, steps)
 import           NITTA.Utils.JSON       ()
 import           Servant
 import qualified STMContainers.Map      as M
@@ -33,125 +33,123 @@ import qualified STMContainers.Map      as M
 data Synthesis
   = Synthesis{ parent :: Maybe (String, Int) -- ^ (name, tick)
              , childs :: [String] -- TODO: привязка к шагу процесса синтеза, а не к названию синтеза.
-             , states :: [ST]
+             , steps  :: [ST]
              } deriving ( Generic )
 
 
+type T = TaggedTime String Int
+type ST = CompilerStep String String String (TaggedTime String Int)
+instance ToJSON ST
 instance ToJSON Synthesis
 
 
 instance Default Synthesis where
   def = Synthesis{ parent=Nothing
                  , childs=[]
-                 , states=[]
+                 , steps=[]
                  }
 
-type T = TaggedTime String Int
-type ST = CompilerStep String String String (TaggedTime String Int)
-instance ToJSON ST
 
 
+type SynthesisAPI
+     = "synthesis" :> Get '[JSON] (Map String Synthesis)
+  :<|> "synthesis" :> Capture "sId" String :>
+       ( Get '[JSON] Synthesis
+    :<|> QueryParam' '[Required] "parent" String :>
+         QueryParam' '[Required] "step" Int :>
+         PostNoContent '[JSON] ()
+    :<|> StepsAPI
+       )
 
-type SynthesisAPI = "synthesis" :>
-     ( Get '[JSON] (Map String Synthesis)
-  :<|> Capture "sid" String :> Get '[JSON] Synthesis
-  -- TODO: servant 0.13, use Strict Param
-  :<|> Capture "sid" String :> QueryParam "parent" String :> QueryParam "decision" Int :> PostNoContent '[JSON] ()
-  :<|> Capture "sid" String :> StepsAPI
-     )
-
-synthesisServer state
-     = fmap fromList ( liftIO $ atomically $ toList $ M.stream state )
-  :<|> getSynthesis state
-  :<|> postSynthesis
-  :<|> stepsServer state
+synthesisServer stm
+     = fmap fromList ( liftIO $ atomically $ toList $ M.stream stm )
+  :<|> \ sId -> getSynthesis stm sId
+           :<|> ( \ pId stepId -> liftSTM $ postSynthesis sId pId stepId )
+           :<|> stepsServer stm sId
   where
-    postSynthesis _ Nothing _ = throwError err400{ errBody = "Parameter `parent` not defined." }
-    postSynthesis _ _ Nothing = throwError err400{ errBody = "Parameter `decision` not defined." }
-    postSynthesis sid (Just pid) (Just did) = liftSTM $ postSynthesis' sid pid did
-
-    postSynthesis' sid pid did = do
-      s <- M.lookup sid state
-      when ( isJust s ) $ throwSTM err409{ errBody = "Synthesis already exist." }
-      parent <- M.lookup pid state
-      when ( isNothing parent ) $ throwSTM err404{ errBody = "Parent not found." }
+    postSynthesis sId pId stepId = do
+      syn <- M.lookup sId stm
+      when ( isJust syn ) $ throwSTM err409{ errBody="Synthesis already exist." }
+      parent <- M.lookup pId stm
+      when ( isNothing parent ) $ throwSTM err404{ errBody="Parent not found." }
       let Just parent'@Synthesis{ childs=cs } = parent
-      M.insert parent'{ childs=sid:cs } pid state
-      M.insert parent'{ parent=Just (pid, did)
-                      -- TODO: crop states by did
-                      } sid state
+      M.insert parent'{ childs=sId : cs } pId stm
+      M.insert parent'{ parent=Just ( pId, stepId )
+                      -- TODO: crop steps by stepId
+                      } sId stm
 
 
 
-type StepsAPI = "steps" :>
-     ( Get '[JSON] [ST]
-     -- step - необходимо обеспечить сохранность индексов для разных веток синтеза.
-  :<|> Capture "step" Int :> Get '[JSON] ST
-  :<|> Capture "step" Int :> QueryParam "manual" Int :> Post '[JSON] ST
-  :<|> Capture "step" Int :> "config" :> Get '[JSON] NaiveOpt
-  -- :<|> Capture "step" Int :> "options0" :> Get '[JSON] [Option (CompilerDT String String String T)]
-  -- :<|> Capture "step" Int :> "options" :> Capture "oid" Int :> "metrics" :> Get '[JSON] [Option (CompilerDT String String String T)]
-  -- :<|> Capture "step" Int :> "options" :> QueryParam "sort" Int :> Get '[JSON] [Option (CompilerDT String String String T)]
-  :<|> Capture "step" Int :> "decisions" :> Get '[JSON] [Decision (CompilerDT String String String T)]
-  :<|> Capture "step" Int :> "options" :> Get '[JSON] [ ( Int
-                                                          , GlobalMetrics
-                                                          , SpecialMetrics
-                                                          , Option (CompilerDT String String String T)
-                                                          , Decision (CompilerDT String String String T)
-                                                          )
-                                                        ]
-     )
+type StepsAPI
+     = "steps" :> Get '[JSON] [ST]
+  :<|> "steps" :> Capture "step" Int :>
+         -- TODO: step - необходимо обеспечить сохранность индексов для разных
+         -- веток синтеза.
+       ( Get '[JSON] ST
+         -- Дублирование auto в path - костыль. Проблема в следующем - параметры
+         -- и флаги не влияют на имя функции в автоматически генерируемом API
+         -- для JS, что приводит к утере одного из методов. Что бы решить эту
+         -- проблему - параметр был явно указан в path.
+    :<|> "auto" :> QueryFlag "auto" :> Post '[JSON] ST
+    :<|> QueryParam' '[Required] "manual" Int :> Post '[JSON] ST
+    :<|> "config" :> Get '[JSON] NaiveOpt
+    :<|> "decisions" :> Get '[JSON] [Decision (CompilerDT String String String T)]
+    :<|> "options" :> Get '[JSON] [ ( Int
+                                    , GlobalMetrics
+                                    , SpecialMetrics
+                                    , Option (CompilerDT String String String T)
+                                    , Decision (CompilerDT String String String T)
+                                    )
+                                  ]
+       )
 
-stepsServer state sid
-     = ( reverse . states <$> getSynthesis state sid )
-  :<|> getStep
-  :<|> postStep
-  :<|> ( fmap config . getStep )
-  -- :<|> ( fmap (options compiler) . getStep )
-  :<|> ( fmap (map option2decision . options compiler) <$> getStep )
-  :<|> ( fmap optionsWithMetrics <$> getStep )
+stepsServer stm sId
+     = ( reverse . steps <$> getSynthesis stm sId )
+  :<|> \ stepId -> getStep stepId
+              :<|> ( \ True -> liftSTM $ autoPostStep stepId )
+              :<|> ( liftSTM . manualPostStep stepId )
+              :<|> ( config <$> getStep stepId )
+              :<|> ( map option2decision . options compiler <$> getStep stepId )
+              :<|> ( optionsWithMetrics <$> getStep stepId )
   where
     getStep = liftSTM . getStep'
-    getStep' step = do
-      steps <- states <$> getSynthesis' state sid
-      unless (length steps > step) $ throwSTM err409{ errBody="Step not exists." }
-      return $ steps !! step
-    postStep step Nothing     = liftSTM $ autoPostStep step
-    postStep step (Just (-1)) = liftSTM $ autoPostStep step
-    postStep step (Just d)    = liftSTM $ manualPostStep step d
-    autoPostStep step = do
-      s@Synthesis{..} <- getSynthesis' state sid
-      unless (length states <= step) $ throwSTM err409{ errBody="Steps already exist." }
-      let states' = foldl (\(x:xs) _ -> mkStep x : x : xs) states [length states .. step]
-      M.insert s{ states=states' } sid state
-      return $ head states'
-    manualPostStep step d = do
-      s@Synthesis{ states=states@(st:_) } <- getSynthesis' state sid
-      unless (length states == step) $ throwSTM err409{ errBody="Only one manual step at a time." }
-      let d' = ((!! d) . map option2decision . options compiler) st
-      let st' = decision compiler st d'
-      let s' = s{ states=st' : states }
-      M.insert s' sid state
-      return st'
-    mkStep st = fromMaybe (error "Synthesis is over.") $ naive' st
+    getStep' stepId = do
+      Synthesis{..} <- getSynthesis' stm sId
+      unless ( length steps > stepId ) $ throwSTM err409{ errBody="Step not exists." }
+      return $ reverse steps !! stepId
+    autoPostStep stepId = do
+      syn@Synthesis{..} <- getSynthesis' stm sId
+      unless ( length steps <= stepId ) $ throwSTM err409{ errBody="Steps already exist." }
+      let steps' = foldl (\(x:xs) _ -> mkStep x : x : xs) steps [length steps .. stepId]
+      M.insert syn{ steps=steps' } sId stm
+      return $ head steps'
+    manualPostStep stepId decisionId = do
+      syn@Synthesis{ steps=steps@(step:_) } <- getSynthesis' stm sId
+      unless (length steps == stepId) $ throwSTM err409{ errBody="Only one manual step at a time." }
+      let d = ((!! decisionId) . map option2decision . options compiler) step
+      let step' = decision compiler step d
+      let syn' = syn{ steps=step' : steps }
+      M.insert syn' sId stm
+      return step'
+    mkStep step = fromMaybe (error "Synthesis is over.") $ naive' step
 
 
 liftSTM stm = liftIO (atomically $ catchSTM (Right <$> stm) (return . Left)) >>= either throwError return
 
-getSynthesis state sid = liftIO $ atomically $ getSynthesis' state sid
-getSynthesis' state sid = do
-  v <- M.lookup sid state
-  maybe (throwSTM err404) return v
+getSynthesis stm sId = liftIO $ atomically $ getSynthesis' stm sId
+getSynthesis' stm sId = do
+  syn <- M.lookup sId stm
+  maybe (throwSTM err404) return syn
 
 
-getDecision state sid did = do
-  s <- getSynthesis state sid
-  return $ states s !! did
+getDecision stm sId stepId = do
+  Synthesis{..} <- getSynthesis stm sId
+  return $ steps !! stepId
 
 
 app root = do
-  state <- atomically $ do
+  stm <- atomically $ do
     st <- M.new
-    M.insert def{ states=[root] } "root" st
+    M.insert def{ steps=[root] } "root" st
     return st
-  return $ serve (Proxy :: Proxy SynthesisAPI) $ synthesisServer state
+  return $ serve (Proxy :: Proxy SynthesisAPI) $ synthesisServer stm
