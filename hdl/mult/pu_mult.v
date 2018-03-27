6cf1425cbf7bfb953dfd8624b405c40ff3adf116
PavelCorepanow <P.Corepanow@gmail.com>
2018-03-27 13:35:12 +0300

    fix v3

--- hdl/mult/pu_mult.v
+++ hdl/mult/pu_mult.v
@@ -1,7 +1,7 @@
 module pu_mult 
-  #( parameter DATA_WIDTH        = 32
-   , parameter ATTR_WIDTH        = 4
-   , parameter INVALID  			= 0 
+  #( parameter DATA_WIDTH = 32
+   , parameter ATTR_WIDTH = 4
+   , parameter INVALID    = 0 
    )
   ( input  wire                  clk
   , input  wire                  rst
@@ -17,9 +17,9 @@ module pu_mult
   );
 
 
-reg [DATA_WIDTH-1:0]         					  stage1 [0:1];
-reg                          			stage1_invalid [0:1];
-reg [DATA_WIDTH-1:DATA_WIDTH/2-1]		  stage_half[0:1]; 
+reg [DATA_WIDTH-1:0]              stage1 [0:1];
+reg                               stage1_invalid [0:1];
+reg [DATA_WIDTH-1:DATA_WIDTH/2-1] stage_half[0:1]; 
 
 
 always @(posedge clk) begin
@@ -29,7 +29,7 @@ always @(posedge clk) begin
 	end else begin 
     if ( signal_wr ) begin
       stage1[signal_sel] <= data_in[DATA_WIDTH-1:0];
-		stage_half[signal_sel] <= data_in[DATA_WIDTH-1:DATA_WIDTH/2-1];
+      stage_half[signal_sel] <= data_in[DATA_WIDTH-1:DATA_WIDTH/2-1];
       stage1_invalid[signal_sel] <= attr_in[INVALID];
 	  end
   end
@@ -37,11 +37,11 @@ end
 
 
 function invalid_value1;
-parameter DATA_WIDTH        		 = 32; 
-input [DATA_WIDTH-1:DATA_WIDTH/2-1] a;
-input [DATA_WIDTH-1:DATA_WIDTH/2-1] b;
-input c;
-input d;
+parameter DATA_WIDTH = 32; 
+input [DATA_WIDTH-1:DATA_WIDTH/2-1] data_arg1;
+input [DATA_WIDTH-1:DATA_WIDTH/2-1] data_arg2;
+input atr_arg1;
+input atr_arg2;
 
 reg is_highhalf_value[0:1];
 reg is_inv_highhalf_value[0:1];
@@ -50,22 +50,16 @@ reg e1;
 reg e2;
 
 begin
-
-  e1 = ~a == {(DATA_WIDTH/2+1){1'b0}}; 
-  e2 = ~b == {(DATA_WIDTH/2+1){1'b0}};
-
-  is_highhalf_value[0] = (  a ==  0 );
+  e1 = ~data_arg1 == {(DATA_WIDTH/2+1){1'b0}}; 
+  e2 = ~data_arg2 == {(DATA_WIDTH/2+1){1'b0}};
+  is_highhalf_value[0] = (data_arg1 ==  0);
   is_inv_highhalf_value[0] = e1;
-
-
-  is_highhalf_value[1] = (  b ==  0 );
+  is_highhalf_value[1] = (data_arg2 ==  0);
   is_inv_highhalf_value[1] = e2;
-
   operand_xor[0] = !(is_highhalf_value[0] ^ is_inv_highhalf_value[0]);
   operand_xor[1] = !(is_highhalf_value[1] ^ is_inv_highhalf_value[1]); 
-                              
-  invalid_value1 =   operand_xor[0] || c
-                  || operand_xor[1] || d;
+  invalid_value1 =   operand_xor[0] || atr_arg1
+                  || operand_xor[1] || atr_arg2;
 end
 endfunction
 
@@ -77,9 +71,9 @@ mult_inner mult_i1
   , .result( mult_result )
   );
 
-reg                           					f;
-reg                           write_multresult;
-reg 									   invalid_value;
+reg f;
+reg write_multresult;
+reg invalid_value;
 
 always @(posedge clk) begin
 invalid_value <= invalid_value1(stage_half [0],stage_half [1],stage1_invalid[0],stage1_invalid[1]);
@@ -93,17 +87,17 @@ invalid_value <= invalid_value1(stage_half [0],stage_half [1],stage1_invalid[0],
   end
 end
 
-reg                            invalid_result;
-reg [DATA_WIDTH-1:0]          data_multresult;
+reg                  invalid_result;
+reg [DATA_WIDTH-1:0] data_multresult;
 
 always @(posedge clk) begin
   if ( rst ) begin
     data_multresult <= 0;
-     invalid_result <= 0;
+    invalid_result <= 0;
   end else begin
     if ( write_multresult ) begin
       invalid_result <= invalid_value;
-       data_multresult <= mult_result;
+      data_multresult <= mult_result;
     end
   end
 end
