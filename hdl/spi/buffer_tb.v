63ce68cdf574e7ff2d58268abb502185b4b050b8
Aleksandr Penskoi <aleksandr.penskoi@gmail.com>
2018-05-01 17:20:20 +0300

    Added assertions.

--- hdl/spi/buffer_tb.v
+++ hdl/spi/buffer_tb.v
@@ -31,31 +31,42 @@ always begin
   forever #5 clk <= ~clk;
 end 
 
+task assert32;
+  input [31:0] a;
+  input [31:0] b;
+  begin
+    if ( !(a === b) ) begin
+      $display("ASSERT FAIL: %h === %h", a, b);
+      $finish(2);
+    end
+  end
+endtask
+
 initial begin
   wr <= 0; data_in <= 32'h00000000; oe <= 0;
 
   @(negedge rst);
-  wr <= 1; data_in <= 32'h00112233; oe <= 0; @(posedge clk); // data_out == 'xxxxxxxx'
-  wr <= 0; data_in <= 32'h00000000; oe <= 0; @(posedge clk); // data_out == 'xxxxxxxx'
-  wr <= 1; data_in <= 32'h44556677; oe <= 0; @(posedge clk); // data_out == 'xxxxxxxx'
-  wr <= 1; data_in <= 32'h8899AABB; oe <= 0; @(posedge clk); // data_out == 'xxxxxxxx'
-  wr <= 0; data_in <= 32'h00000000; oe <= 0; @(posedge clk); // data_out == 'xxxxxxxx'
-  wr <= 0; data_in <= 32'h00000000; oe <= 0; @(posedge clk); // data_out == 'xxxxxxxx'
-  wr <= 1; data_in <= 32'hCCDDEEFF; oe <= 0; @(posedge clk); // data_out == 'xxxxxxxx'
-  wr <= 0; data_in <= 32'h00000000; oe <= 0; @(posedge clk); // data_out == 'xxxxxxxx'
+  wr <= 1; data_in <= 32'h00112233; oe <= 0;   #1assert32(data_out, 32'hxxxxxxxx); @(posedge clk);
+  wr <= 0; data_in <= 32'h00000000; oe <= 0;   #1assert32(data_out, 32'hxxxxxxxx); @(posedge clk);
+  wr <= 1; data_in <= 32'h44556677; oe <= 0;   #1assert32(data_out, 32'hxxxxxxxx); @(posedge clk);
+  wr <= 1; data_in <= 32'h8899AABB; oe <= 0;   #1assert32(data_out, 32'hxxxxxxxx); @(posedge clk);
+  wr <= 0; data_in <= 32'h00000000; oe <= 0;   #1assert32(data_out, 32'hxxxxxxxx); @(posedge clk);
+  wr <= 0; data_in <= 32'h00000000; oe <= 0;   #1assert32(data_out, 32'hxxxxxxxx); @(posedge clk);
+  wr <= 1; data_in <= 32'hCCDDEEFF; oe <= 0;   #1assert32(data_out, 32'hxxxxxxxx); @(posedge clk);
+  wr <= 0; data_in <= 32'h00000000; oe <= 0;   #1assert32(data_out, 32'hxxxxxxxx); @(posedge clk);
   repeat(4) @(posedge clk);
 
   rst <= 1; @(posedge clk);
   rst <= 0; @(posedge clk);
-  wr <= 0; data_in <= 32'h00000000; oe <= 0; @(posedge clk); // data_out == '00112233'
-  wr <= 0; data_in <= 32'h00000000; oe <= 1; @(posedge clk); // data_out == '44556677'
-  wr <= 0; data_in <= 32'h00000000; oe <= 0; @(posedge clk); // data_out == '44556677'
-  wr <= 0; data_in <= 32'h00000000; oe <= 1; @(posedge clk); // data_out == '8899AABB'
-  wr <= 0; data_in <= 32'h00000000; oe <= 1; @(posedge clk); // data_out == 'CCDDEEFF'
-  wr <= 0; data_in <= 32'h00000000; oe <= 0; @(posedge clk); // data_out == 'CCDDEEFF'
+  wr <= 0; data_in <= 32'h00000000; oe <= 0;   #1assert32(data_out, 32'h00112233); @(posedge clk);
+  wr <= 0; data_in <= 32'h00000000; oe <= 1;   #1assert32(data_out, 32'h44556677); @(posedge clk);
+  wr <= 0; data_in <= 32'h00000000; oe <= 0;   #1assert32(data_out, 32'h44556677); @(posedge clk);
+  wr <= 0; data_in <= 32'h00000000; oe <= 1;   #1assert32(data_out, 32'h8899AABB); @(posedge clk);
+  wr <= 0; data_in <= 32'h00000000; oe <= 1;   #1assert32(data_out, 32'hCCDDEEFF); @(posedge clk);
+  wr <= 0; data_in <= 32'h00000000; oe <= 0;   #1assert32(data_out, 32'hCCDDEEFF); @(posedge clk);
 
-  wr <= 0; data_in <= 32'h00000000; oe <= 1; @(posedge clk); // data_out == 'xxxxxxxx'
-  wr <= 0; data_in <= 32'h00000000; oe <= 0; @(posedge clk); // data_out == 'xxxxxxxx'
+  wr <= 0; data_in <= 32'h00000000; oe <= 1;   #1assert32(data_out, 32'hxxxxxxxx); @(posedge clk);
+  wr <= 0; data_in <= 32'h00000000; oe <= 0;   #1assert32(data_out, 32'hxxxxxxxx); @(posedge clk);
   repeat(10) @(posedge clk); $finish;
 end
 
--- hdl/spi/nitta_to_spi_splitter_tb.v
+++ hdl/spi/nitta_to_spi_splitter_tb.v
@@ -33,40 +33,62 @@ always begin
   forever #5 clk <= ~clk;
 end 
 
+task assert8;
+  input [7:0] a;
+  input [7:0] b;
+  begin
+    if ( !(a === b) ) begin
+      $display("ASSERT FAIL: %h === %h", a, b);
+      $finish(2);
+    end
+  end
+endtask
+
+task assert1;
+  input a;
+  input b;
+  begin
+    if ( !(a === b) ) begin
+      $display("ASSERT FAIL: %h === %h", a, b);
+      $finish(2);
+    end
+  end
+endtask
+
 initial begin
   spi_ready <= 0; from_nitta <= 32'hA0B1C2D3;
 
   @(negedge rst);
-  spi_ready <= 0; from_nitta <= 32'hA0B1C2D3; @(posedge clk); // A0
-  spi_ready <= 1; from_nitta <= 32'hA0B1C2D3; @(posedge clk); // B1
-  spi_ready <= 0; from_nitta <= 32'hA0B1C2D3; @(posedge clk); // B1
-  spi_ready <= 1; from_nitta <= 32'hA0B1C2D3; @(posedge clk); // C2
-  spi_ready <= 0; from_nitta <= 32'hA0B1C2D3; @(posedge clk); // C2
-  spi_ready <= 0; from_nitta <= 32'hA0B1C2D3; @(posedge clk); // C2
-  spi_ready <= 0; from_nitta <= 32'hA0B1C2D3; @(posedge clk); // C2
-  spi_ready <= 1; from_nitta <= 32'hA0B1C2D3; @(posedge clk); // D3
-  spi_ready <= 0; from_nitta <= 32'hA0B1C2D3; @(posedge clk); // D3
-  spi_ready <= 1; from_nitta <= 32'hA0B1C2D3; @(posedge clk); // A0, splitter_ready
-  spi_ready <= 0; from_nitta <= 32'hA0B1C2D3; @(posedge clk); // A0
+  spi_ready <= 0; from_nitta <= 32'hA0B1C2D3;   #1assert8(to_spi, 8'hA0); assert1(splitter_ready, 0); @(posedge clk);
+  spi_ready <= 1; from_nitta <= 32'hA0B1C2D3;   #1assert8(to_spi, 8'hB1); assert1(splitter_ready, 0); @(posedge clk);
+  spi_ready <= 0; from_nitta <= 32'hA0B1C2D3;   #1assert8(to_spi, 8'hB1); assert1(splitter_ready, 0); @(posedge clk);
+  spi_ready <= 1; from_nitta <= 32'hA0B1C2D3;   #1assert8(to_spi, 8'hC2); assert1(splitter_ready, 0); @(posedge clk);
+  spi_ready <= 0; from_nitta <= 32'hA0B1C2D3;   #1assert8(to_spi, 8'hC2); assert1(splitter_ready, 0); @(posedge clk);
+  spi_ready <= 0; from_nitta <= 32'hA0B1C2D3;   #1assert8(to_spi, 8'hC2); assert1(splitter_ready, 0); @(posedge clk);
+  spi_ready <= 0; from_nitta <= 32'hA0B1C2D3;   #1assert8(to_spi, 8'hC2); assert1(splitter_ready, 0); @(posedge clk);
+  spi_ready <= 1; from_nitta <= 32'hA0B1C2D3;   #1assert8(to_spi, 8'hD3); assert1(splitter_ready, 0); @(posedge clk);
+  spi_ready <= 0; from_nitta <= 32'hA0B1C2D3;   #1assert8(to_spi, 8'hD3); assert1(splitter_ready, 0); @(posedge clk);
+  spi_ready <= 1; from_nitta <= 32'hA0B1C2D3;   #1assert8(to_spi, 8'hA0); assert1(splitter_ready, 1); @(posedge clk);
+  spi_ready <= 0; from_nitta <= 32'hA0B1C2D3;   #1assert8(to_spi, 8'hA0); assert1(splitter_ready, 0); @(posedge clk);
   repeat(4) @(posedge clk);
 
   rst <= 1; @(posedge clk);
   rst <= 0; @(posedge clk);
-  spi_ready <= 0; from_nitta <= 32'hA0B1C2D3; @(posedge clk); // A0
-  spi_ready <= 1; from_nitta <= 32'hA0B1C2D3; @(posedge clk); // B1
-  spi_ready <= 1; from_nitta <= 32'hA0B1C2D3; @(posedge clk); // B1
-  spi_ready <= 1; from_nitta <= 32'hA0B1C2D3; @(posedge clk); // B1
-  spi_ready <= 1; from_nitta <= 32'hA0B1C2D3; @(posedge clk); // B1
-  spi_ready <= 0; from_nitta <= 32'hA0B1C2D3; @(posedge clk); // B1
-  spi_ready <= 1; from_nitta <= 32'hA0B1C2D3; @(posedge clk); // C2
-  spi_ready <= 1; from_nitta <= 32'hA0B1C2D3; @(posedge clk); // C2
-  spi_ready <= 0; from_nitta <= 32'hA0B1C2D3; @(posedge clk); // C2
-  spi_ready <= 1; from_nitta <= 32'hA0B1C2D3; @(posedge clk); // D3
-  spi_ready <= 1; from_nitta <= 32'hA0B1C2D3; @(posedge clk); // D3
-  spi_ready <= 0; from_nitta <= 32'hA0B1C2D3; @(posedge clk); // D3
-  spi_ready <= 1; from_nitta <= 32'hA0B1C2D3; @(posedge clk); // A0, splitter_ready
-  spi_ready <= 1; from_nitta <= 32'hA0B1C2D3; @(posedge clk); // A0
-  spi_ready <= 0; from_nitta <= 32'hA0B1C2D3; @(posedge clk); // A0
+  spi_ready <= 0; from_nitta <= 32'hA0B1C2D3;   #1assert8(to_spi, 8'hA0); assert1(splitter_ready, 0); @(posedge clk);
+  spi_ready <= 1; from_nitta <= 32'hA0B1C2D3;   #1assert8(to_spi, 8'hB1); assert1(splitter_ready, 0); @(posedge clk);
+  spi_ready <= 1; from_nitta <= 32'hA0B1C2D3;   #1assert8(to_spi, 8'hB1); assert1(splitter_ready, 0); @(posedge clk);
+  spi_ready <= 1; from_nitta <= 32'hA0B1C2D3;   #1assert8(to_spi, 8'hB1); assert1(splitter_ready, 0); @(posedge clk);
+  spi_ready <= 1; from_nitta <= 32'hA0B1C2D3;   #1assert8(to_spi, 8'hB1); assert1(splitter_ready, 0); @(posedge clk);
+  spi_ready <= 0; from_nitta <= 32'hA0B1C2D3;   #1assert8(to_spi, 8'hB1); assert1(splitter_ready, 0); @(posedge clk);
+  spi_ready <= 1; from_nitta <= 32'hA0B1C2D3;   #1assert8(to_spi, 8'hC2); assert1(splitter_ready, 0); @(posedge clk);
+  spi_ready <= 1; from_nitta <= 32'hA0B1C2D3;   #1assert8(to_spi, 8'hC2); assert1(splitter_ready, 0); @(posedge clk);
+  spi_ready <= 0; from_nitta <= 32'hA0B1C2D3;   #1assert8(to_spi, 8'hC2); assert1(splitter_ready, 0); @(posedge clk);
+  spi_ready <= 1; from_nitta <= 32'hA0B1C2D3;   #1assert8(to_spi, 8'hD3); assert1(splitter_ready, 0); @(posedge clk);
+  spi_ready <= 1; from_nitta <= 32'hA0B1C2D3;   #1assert8(to_spi, 8'hD3); assert1(splitter_ready, 0); @(posedge clk);
+  spi_ready <= 0; from_nitta <= 32'hA0B1C2D3;   #1assert8(to_spi, 8'hD3); assert1(splitter_ready, 0); @(posedge clk);
+  spi_ready <= 1; from_nitta <= 32'hA0B1C2D3;   #1assert8(to_spi, 8'hA0); assert1(splitter_ready, 1); @(posedge clk);
+  spi_ready <= 1; from_nitta <= 32'hA0B1C2D3;   #1assert8(to_spi, 8'hA0); assert1(splitter_ready, 0); @(posedge clk);
+  spi_ready <= 0; from_nitta <= 32'hA0B1C2D3;   #1assert8(to_spi, 8'hA0); assert1(splitter_ready, 0); @(posedge clk);
   repeat(10) @(posedge clk); $finish;
 end
 
