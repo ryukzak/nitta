891636480e98b922ceec7e8ff1100162ce9e368f
Aleksandr Penskoi <aleksandr.penskoi@gmail.com>
2018-05-01 20:01:46 +0300

    Added spi_slave_driver_tb. Fixed spi_slave_driver.

--- hdl/spi/spi_slave_driver.v
+++ hdl/spi/spi_slave_driver.v
@@ -12,81 +12,67 @@ module spi_slave_driver
   // system interface
   , input  [DATA_WIDTH-1:0] data_in  // data that master can read from slave
   , output                  ready    // transaction is not processed now 
-  , output                  premature_ready
+  // , output                  premature_ready
   , output [DATA_WIDTH-1:0] data_out // data written to slave in last transaction
   // SPI iterface
-  , output reg       miso
+  , output           miso
   , input            mosi
   , input            sclk
   , input            cs
   );
 
-reg   [DATA_WIDTH-1:0] shiftreg;
+reg [DATA_WIDTH-1:0] shiftreg;
+reg miso_buf;
 
-localparam STATE_IDLE = 0;        // wait for transaction begin
-localparam STATE_WAIT_SCLK_1 = 1; // wait for SCLK to become 1
-localparam STATE_WAIT_SCLK_0 = 2; // wait for SCLK to become 0
-localparam STATE_DELAY = 3;
-reg   [1:0] state;
+localparam STATE_WAIT_SCLK_1 = 0;
+localparam STATE_WAIT_SCLK_0 = 1;
+localparam STATE_READY = 2;
+reg [1:0] state;
 
-localparam DATA_COUNTER_WIDTH  = $clog2( DATA_WIDTH + 1 );
-reg   [DATA_COUNTER_WIDTH-1:0] data_counter;
+localparam DATA_COUNTER_WIDTH = $clog2( DATA_WIDTH + 1 );
+reg [DATA_COUNTER_WIDTH-1:0] data_counter;
 
 always @( posedge rst, posedge clk ) begin
-  if ( rst ) begin
-    shiftreg <= 0;
-    miso <= 0;
+  if ( rst || cs ) begin
     data_counter <= 0;
-    state <= STATE_IDLE;
+    state <= STATE_WAIT_SCLK_0;
+    miso_buf <= 0;
   end else begin
-    if ( cs ) begin
-      data_counter <= 0;
-      state <= STATE_IDLE;
-    end else begin
-      case ( state )
-        STATE_IDLE: begin
-          shiftreg <= data_in; 
-          data_counter <= 0;        
-          state <= STATE_WAIT_SCLK_1;
+    case ( state )
+
+      STATE_WAIT_SCLK_1, STATE_READY: if ( sclk ) begin
+        if ( data_counter == 0 || data_counter == DATA_WIDTH ) begin
+          shiftreg <= data_in;
+          miso_buf <= data_in[ DATA_WIDTH - 1 ];
+        end else begin
+          miso_buf <= shiftreg[ DATA_WIDTH - 1 ];
         end
-        STATE_WAIT_SCLK_1: begin
-          if ( sclk ) begin
-            miso <= shiftreg[ DATA_WIDTH - 1 ];
-            if ( data_counter == DATA_WIDTH ) begin
-              data_counter <= 1;
-            end else begin
-              data_counter <= data_counter + 1;   
-            end
-            state <= STATE_WAIT_SCLK_0;           
-          end
-        end
-        STATE_WAIT_SCLK_0, STATE_DELAY: begin
-          if ( !sclk ) begin
-            if ( SUB_FRAME && data_counter == DATA_WIDTH ) begin
-              if ( state == STATE_WAIT_SCLK_0 ) begin
-                shiftreg <= { shiftreg[DATA_WIDTH - 2:0], mosi } ;
-                state <= STATE_DELAY;
-              end else begin
-                shiftreg <= data_in;
-                state <= STATE_WAIT_SCLK_1;
-              end
-            end else begin
-              shiftreg <= { shiftreg[DATA_WIDTH - 2:0], mosi } ;
-              state <= STATE_WAIT_SCLK_1;              
-            end
-          end
-        end
-        default: state <= STATE_IDLE;
-      endcase
-    end
+        if ( data_counter == DATA_WIDTH) data_counter <= 1;
+        else data_counter <= data_counter + 1;   
+        state <= STATE_WAIT_SCLK_0;
+      end else if ( state == STATE_READY )
+        state <= STATE_WAIT_SCLK_1;
+  
+      STATE_WAIT_SCLK_0: if ( !sclk ) begin
+        shiftreg <= { shiftreg[DATA_WIDTH - 2:0], mosi } ;
+        if ( data_counter == DATA_WIDTH ) state <= STATE_READY;
+        else state <= STATE_WAIT_SCLK_1;
+      end
+
+      default: state <= STATE_WAIT_SCLK_0;
+    endcase
   end
 end
 
+assign miso = state == STATE_WAIT_SCLK_1 && sclk
+            ? ( data_counter == 0 || data_counter == DATA_WIDTH
+              ? data_in[ DATA_WIDTH - 1 ]
+              : shiftreg[ DATA_WIDTH - 1 ]
+              )
+            : miso_buf;
+
 assign data_out = shiftreg;
-assign ready = state == STATE_IDLE
-            || data_counter == DATA_WIDTH && sclk && state == STATE_WAIT_SCLK_1 ;
-assign premature_ready = state == STATE_IDLE
-                      || data_counter == DATA_WIDTH && !sclk && ( state == STATE_WAIT_SCLK_0 ) ;
+assign ready = state == STATE_READY;
             
   
 endmodule
