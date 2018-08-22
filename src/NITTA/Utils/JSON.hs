46ad97247cead4a62ca9b77e256d8566f28fee49
Aleksandr Penskoi <aleksandr.penskoi@gmail.com>
2018-08-22 15:19:01 +0300

    Reformated.

--- src/NITTA/Utils/JSON.hs
+++ src/NITTA/Utils/JSON.hs
@@ -19,7 +19,7 @@ import           Numeric.Interval
 
 
 instance ( Show a ) => ToJSON (Interval a) where
-  toJSON = String . pack . show
+    toJSON = String . pack . show
 
 
 instance ( ToJSON tag
@@ -27,12 +27,13 @@ instance ( ToJSON tag
          ) => ToJSON (TaggedTime tag t)
 instance ( Show v
          ) => ToJSON (F (Parcel v x)) where
-  toJSON = String . pack . show
+    toJSON = String . pack . show
 instance ( ToJSON t, Time t
          ) => ToJSON (TimeConstrain t) where
-  toJSON TimeConstrain{..} = object [ "available" .= tcAvailable
-                                    , "duration" .= tcDuration
-                                    ]
+    toJSON TimeConstrain{..} = object
+        [ "available" .= tcAvailable
+        , "duration" .= tcDuration
+        ]
 
 
 instance ToJSON NaiveOpt
@@ -65,10 +66,10 @@ instance ( ToJSON title
          ) => ToJSON (Decision (DataFlowDT title v t))
 instance ( Show title
          ) => ToJSON (Option (BindingDT title io)) where
-  toJSON (BindingO fb title) = toJSON [ show fb, show title ]
+    toJSON (BindingO fb title) = toJSON [ show fb, show title ]
 instance ( Show title
          ) => ToJSON (Decision (BindingDT title io)) where
-  toJSON (BindingD fb title) = toJSON [ show fb, show title ]
+    toJSON (BindingD fb title) = toJSON [ show fb, show title ]
 instance ( ToJSON v, Var v ) => ToJSON (Option (ControlDT v))
 instance ( ToJSON v, Var v ) => ToJSON (Decision (ControlDT v))
 
@@ -82,57 +83,54 @@ instance ( ToJSONKey title, ToJSON title, Typeable title, Ord title, Show title
          , ToJSONKey x
          , Show x
          ) => ToJSON (BusNetwork title v x t) where
-  toJSON n@BusNetwork{..}
-             -- , bnSignalBusWidth     :: Int
-    = object [ "width" .= bnSignalBusWidth
-             --   bnRemains            :: [F (Parcel v) v]
-             , "remain" .= bnRemains
-             -- , bnForwardedVariables :: [v]
-             , "forwardedVariables" .= map (String . pack . show) (transfered n)
-             -- , bnBinded             :: M.Map title [F (Parcel v) v]
-             , "binds" .= bnBinded
-             -- , bnProcess            :: Process v t
-             , "processLength" .= nextTick (process n)
-             -- , bnPus                :: M.Map title spu
-             , "processUnits" .= M.keys bnPus
-             , "process" .= process n
-             ]
+    toJSON n@BusNetwork{..} = object
+        -- , bnSignalBusWidth     :: Int
+        [ "width" .= bnSignalBusWidth
+        --   bnRemains            :: [F (Parcel v) v]
+        , "remain" .= bnRemains
+        -- , bnForwardedVariables :: [v]
+        , "forwardedVariables" .= map (String . pack . show) (transfered n)
+        -- , bnBinded             :: M.Map title [F (Parcel v) v]
+        , "binds" .= bnBinded
+        -- , bnProcess            :: Process v t
+        , "processLength" .= nextTick (process n)
+        -- , bnPus                :: M.Map title spu
+        , "processUnits" .= M.keys bnPus
+        , "process" .= process n
+        ]
 
 
 instance ( ToJSON t, Time t, Show v
          ) => ToJSON (Process (Parcel v x) t) where
-  toJSON Process{ steps, nextTick }
-    = object
-      [ "steps" .= filter (\Step{ sDesc } -> case sDesc of InstructionStep _ -> True; NestedStep _ (InstructionStep _) -> True; _ -> False) steps
-      , "nextTick" .= nextTick
-      ]
-      -- , relations :: [Relation] -- ^ Список отношений между шагами вычислительного процесса
-      -- , nextUid   :: ProcessUid -- ^ Следующий свободный идентификатор шага вычислительного процесса.
+    toJSON Process{ steps, nextTick } = object
+        [ "steps" .= filter (\Step{ sDesc } -> case sDesc of InstructionStep _ -> True; NestedStep _ (InstructionStep _) -> True; _ -> False) steps
+        , "nextTick" .= nextTick
+        ]
+        -- , relations :: [Relation] -- ^ Список отношений между шагами вычислительного процесса
+        -- , nextUid   :: ProcessUid -- ^ Следующий свободный идентификатор шага вычислительного процесса.
 
 instance ( ToJSON t, Time t, Show v
          ) => ToJSON (Step (Parcel v x) t) where
-  toJSON Step{ sKey, sTime=Event a, sDesc }
-    = toJSON
-      [ toJSON $ show sKey                         -- Task ID
-      , toJSON $ show sDesc                        -- Task Name
-                                                   -- Resource ID (optional)
-      , toJSON $ fromEnum a                        -- Start
-      , Null                                       -- End
-      , toJSON (0 :: Int)                          -- Duration (in milliseconds)
-      , toJSON (100 :: Int)                        -- Percent Complete
-      , Null                                       -- Dependencies
-      ]
-  toJSON Step{ sKey, sTime=Activity i, sDesc }
-    = toJSON
-      [ toJSON $ show sKey                         -- Task ID
-      , toJSON $ show sDesc                        -- Task Name
-                                                   -- Resource ID (optional)
-      , toJSON $ fromEnum (inf i)                  -- Start
-      , Null                                       -- End
-      , toJSON $ fromEnum (width i) + 1            -- Duration (in milliseconds)
-      , toJSON (100 :: Int)                        -- Percent Complete
-      , Null                                       -- Dependencies
-      ]
+    toJSON Step{ sKey, sTime=Event a, sDesc } = toJSON
+        [ toJSON $ show sKey                         -- Task ID
+        , toJSON $ show sDesc                        -- Task Name
+                                                     -- Resource ID (optional)
+        , toJSON $ fromEnum a                        -- Start
+        , Null                                       -- End
+        , toJSON (0 :: Int)                          -- Duration (in milliseconds)
+        , toJSON (100 :: Int)                        -- Percent Complete
+        , Null                                       -- Dependencies
+        ]
+    toJSON Step{ sKey, sTime=Activity i, sDesc } = toJSON
+        [ toJSON $ show sKey                         -- Task ID
+        , toJSON $ show sDesc                        -- Task Name
+                                                     -- Resource ID (optional)
+        , toJSON $ fromEnum (inf i)                  -- Start
+        , Null                                       -- End
+        , toJSON $ fromEnum (width i) + 1            -- Duration (in milliseconds)
+        , toJSON (100 :: Int)                        -- Percent Complete
+        , Null                                       -- Dependencies
+        ]
 
 
 instance ToJSON SpecialMetrics
