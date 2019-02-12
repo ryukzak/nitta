bad7a5a07d5bf7095a1574a2b4bc490f33ae7ca0
Aleksandr Penskoi <aleksandr.penskoi@gmail.com>
2019-02-12 21:52:10 +0300

    Fixed: explicit Network export.

--- src/NITTA/Types/Network.hs
+++ src/NITTA/Types/Network.hs
@@ -16,7 +16,19 @@ License     : BSD3
 Maintainer  : aleksandr.penskoi@gmail.com
 Stability   : experimental
 -}
-module NITTA.Types.Network where
+module NITTA.Types.Network
+    ( Connected(..)
+    , DataFlowDT, dataFlowDT, Option(..), Decision(..)
+    , Enviroment(..)
+    , Implementation(..)
+    , IOTest(..)
+    , NetEnv(..)
+    , Parameter(..)
+    , PU(..), castPU
+    , Signal(..), InputPort(..), OutputPort(..)
+    , Source, Target
+    , TargetSystemComponent(..)
+    ) where
 
 import           Data.Ix
 import qualified Data.Map         as M
