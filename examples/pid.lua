e4bd2fe31a2b130bc4250a08428745ed2278a7e5
Aleksandr Penskoi <aleksandr.penskoi@gmail.com>
2019-02-26 17:14:58 +0300

    Improved Show instance for data O. Add pid example.

---
+++ examples/pid.lua
@@ -0,0 +1,37 @@
+function pid(I, prev_error) 
+    local temperature_desired = 50
+    local getValueSPI = receive()
+    
+    error = temperature_desired - getValueSPI
+    D = error - prev_error
+    send(D)
+
+    pid(I, error)
+end
+
+pid(0, 0)
+
+
+
+
+
+-- function pid(I, prev_error) 
+--     local Kp = 2
+--     local Ki = 0
+--     local Kd = 0
+    
+--     local temperature_desired = 50
+
+--     local getValueSPI = receive()
+
+--     local error = temperature_desired - getValueSPI
+--     P = Kp * error
+--     I = I + Ki * error
+--     D = Kd * ( error - prev_error )
+--     local PID = P + I + D
+--     send(PID)
+
+--     pid(I, error)
+-- end
+
+-- pid(0, 0)
