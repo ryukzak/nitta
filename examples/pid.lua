fe899889055a56741e69718c4647610de80f1a79
Aleksandr Penskoi <aleksandr.penskoi@gmail.com>
2019-04-22 09:15:19 +0300

    Added synthesizable version of pid controller.

--- examples/pid.lua
+++ examples/pid.lua
@@ -1,35 +1,22 @@
-function pid(prev_error) 
+function pid(I, prev_err) 
+    local Kp = 2
+    local Ki = 0
+    local Kd = 0
+
     local temperature_desired = 50
     local getValueSPI = receive()
-    
-    error = temperature_desired - getValueSPI
-    D = error - reg(prev_error)
-    send(D)
 
-    pid(error)
+    prev_err = reg(prev_err)
+    err = reg(temperature_desired - getValueSPI)
+    
+    P = Kp * err
+    I = reg(I + Ki * err)
+    D = Kd * reg(err - prev_err)
+
+    local PID = reg(P + I) + D
+    send(PID)
+
+    pid(I, err)
 end
 
-pid(0)
-
-
-
--- function pid(I, prev_error) 
---     local Kp = 2
---     local Ki = 0
---     local Kd = 0
-    
---     local temperature_desired = 50
-
---     local getValueSPI = receive()
-
---     local error = temperature_desired - getValueSPI
---     P = Kp * error
---     I = I + Ki * error
---     D = Kd * ( error - prev_error )
---     local PID = P + I + D
---     send(PID)
-
---     pid(I, error)
--- end
-
--- pid(0, 0)
+pid(0, 0)
