2e04335425625b25c19dce22241c66945d1382be
daniilprohorov <daniilprohorov@gmail.com>
2019-08-28 18:53:29 +0300

    delete unused code and add a few blank lines

--- src/NITTA/Conf.hs
+++ src/NITTA/Conf.hs
@@ -1,16 +1,9 @@
 {-# LANGUAGE FlexibleContexts #-}
+
 module NITTA.Conf ( addManual, createMicroarch, endManual ) where
-import Control.Monad.State.Lazy
+
+import           Control.Monad.State.Lazy
 import           NITTA.Model.Networks.Bus
-import           NITTA.Intermediate.Types
-import           NITTA.Model.Networks.Bus
-import           NITTA.Model.Networks.Types
-import           NITTA.Model.ProcessorUnits
-import           NITTA.Model.ProcessorUnits.Types
-import           NITTA.Project                    (TargetSynthesis (..),
-                                                   mkModelWithOneNetwork,
-                                                   runTargetSynthesis)
-import           NITTA.UIBackend
 
 -- manual add PU
 addManual el = get >>= \lst -> put $ lst ++ [el]
@@ -21,5 +14,4 @@ endManual counter ioSync = get >>= (\pu -> return $ busNetwork counter ioSync pu
 -- eval state and create microarch
 createMicroarch net = evalState net []
 
--- TODO: do one function create microarch 
 
