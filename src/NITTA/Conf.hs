f58ad8a5471a4d10ed676f4d58e3775ca5e6f02c
daniilprohorov <daniilprohorov@gmail.com>
2019-08-28 18:32:40 +0300

    add end function

--- src/NITTA/Conf.hs
+++ src/NITTA/Conf.hs
@@ -1,18 +1,25 @@
 {-# LANGUAGE FlexibleContexts #-}
-module NITTA.Conf ( addManual, createMicroarch ) where
+module NITTA.Conf ( addManual, createMicroarch, endManual ) where
 import Control.Monad.State.Lazy
+import           NITTA.Model.Networks.Bus
+import           NITTA.Intermediate.Types
+import           NITTA.Model.Networks.Bus
+import           NITTA.Model.Networks.Types
+import           NITTA.Model.ProcessorUnits
+import           NITTA.Model.ProcessorUnits.Types
+import           NITTA.Project                    (TargetSynthesis (..),
+                                                   mkModelWithOneNetwork,
+                                                   runTargetSynthesis)
+import           NITTA.UIBackend
 
 -- manual add PU
 addManual el = get >>= \lst -> put $ lst ++ [el]
 
+-- manual create BusNetwork with width parameter counter
+endManual counter ioSync = get >>= (\pu -> return $ busNetwork counter ioSync pu)
 
--- make :: State [String] String
--- make = do
-    -- f <- get
-    -- put f
-    -- add "kek"
-    -- add "lol"
-    -- add "345"
-    -- return (busNetwork)
-
+-- eval state and create microarch
 createMicroarch net = evalState net []
+
+-- TODO: do one function create microarch 
+
