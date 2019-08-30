a4c962c26b1c72a46ca3cca3908faf82e2e4acd9
daniilprohorov <daniilprohorov@gmail.com>
2019-08-30 14:50:53 +0300

    add automatic network creation

--- app/Main.hs
+++ app/Main.hs
@@ -75,7 +75,7 @@ main = do
         else runTestbench $ lua2functions src
     case type_ of
         "fx24.32" -> runner (microarch io_sync :: BusNetwork String String (FX 24 32) Int)
-        "fx32.32" -> runner (microarch io_sync :: BusNetwork String String (FX 32 32) Int)
+        "fx32.32" -> runner (microarch io_sync :: BusNetwork String String (FX 6 6) Int)
         _ -> error "Wrong bus type"
 
 runWebUI port alg ma = backendServer port $ mkModelWithOneNetwork ma alg
@@ -96,61 +96,14 @@ runTestbench tDFG tMicroArch
 
 -- TODO: Необходимо иметь возможность указать, какая именно частота будет у целевого вычислителя. Данная задача связана
 -- с задачей о целевой платформе.
--- microarch ioSync = busNetwork 31 ioSync
---     [ ("fram1", PU def def FramPorts{ oe=SignalTag 11, wr=SignalTag 10, addr=map SignalTag [9, 8, 7, 6] } FramIO )
---     , ("fram2", PU def def FramPorts{ oe=SignalTag 5, wr=SignalTag 4, addr=map SignalTag [3, 2, 1, 0] } FramIO )
---     -- , ("shift", PU def S.Ports{ S.work=SignalTag 12, S.direction=SignalTag 13, S.mode=SignalTag 14, S.step=SignalTag 15, S.init=SignalTag 16, S.oe=SignalTag 17 })
---     , ("accum", PU def def AccumPorts{ init=SignalTag 18, load=SignalTag 19, neg=SignalTag 20, oe=SignalTag 21 } AccumIO )
---     , ("spi", PU def
---         (anySPI 0)
---         SimpleIOPorts
---             { wr=SignalTag 22, oe=SignalTag 23
---             , stop="stop"
---             }
---         $ case "slave" of
---             "slave" -> SPISlave
---                 { slave_mosi=InputPortTag "mosi"
---                 , slave_miso=OutputPortTag "miso"
---                 , slave_sclk=InputPortTag "sclk"
---                 , slave_cs=InputPortTag "cs"
---                 }
---             "master" -> SPIMaster
---                 { master_mosi=OutputPortTag "mosi"
---                 , master_miso=InputPortTag "miso"
---                 , master_sclk=OutputPortTag "sclk"
---                 , master_cs=OutputPortTag "cs"
---                 }
---         )
---     , ("mul", PU def (multiplier True) MultiplierPorts{ wr=SignalTag 24, wrSel=SignalTag 25, oe=SignalTag 26 } MultiplierIO )
---     , ("div", PU def (divider 4 True) DividerPorts{ wr=SignalTag 27, wrSel=SignalTag 28, oe=SignalTag 29, oeSel=SignalTag 30 } DividerIO )
---     ]
-
-network ioSync = do
-    addManual ("fram1", PU def def FramPorts{ oe=SignalTag 11, wr=SignalTag 10, addr=map SignalTag [9, 8, 7, 6] } FramIO )
-    addManual ("fram2", PU def def FramPorts{ oe=SignalTag 5, wr=SignalTag 4, addr=map SignalTag [3, 2, 1, 0] } FramIO )
-    addManual ("accum", PU def def AccumPorts{ init=SignalTag 18, load=SignalTag 19, neg=SignalTag 20, oe=SignalTag 21 } AccumIO )
-    addManual ("spi", PU def
-        (anySPI 0)
-        SimpleIOPorts
-            { wr=SignalTag 22, oe=SignalTag 23
-            , stop="stop"
-            }
-        $ case "slave" of
-            "slave" -> SPISlave
-                { slave_mosi=InputPortTag "mosi"
-                , slave_miso=OutputPortTag "miso"
-                , slave_sclk=InputPortTag "sclk"
-                , slave_cs=InputPortTag "cs"
-                }
-            "master" -> SPIMaster
-                { master_mosi=OutputPortTag "mosi"
-                , master_miso=InputPortTag "miso"
-                , master_sclk=OutputPortTag "sclk"
-                , master_cs=OutputPortTag "cs"
-                }
-        )
-    addManual ("mul", PU def (multiplier True) MultiplierPorts{ wr=SignalTag 24, wrSel=SignalTag 25, oe=SignalTag 26 } MultiplierIO )
-    addManual ("div", PU def (divider 4 True) DividerPorts{ wr=SignalTag 27, wrSel=SignalTag 28, oe=SignalTag 29, oeSel=SignalTag 30 } DividerIO )
-    endManual 31 ioSync 
-
-microarch = createMicroarch . network 
+microarch = evalNetwork . network
+    where
+      network ioSync = do
+        addFram "fram1"
+        addFram "fram2"
+        addFram "fram3"
+        addAccum "accum"
+        addDiv "div"
+        addMul "mul"
+        addSpi "spi" "slave"
+        end ioSync
--- src/NITTA/Conf.hs
+++ src/NITTA/Conf.hs
@@ -1,17 +1,96 @@
 {-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE DataKinds                 #-}
+{-# LANGUAGE DuplicateRecordFields     #-}
+{-# LANGUAGE NamedFieldPuns            #-}
+{-# LANGUAGE NoMonomorphismRestriction #-}
+{-# LANGUAGE PartialTypeSignatures     #-}
+{-# LANGUAGE ScopedTypeVariables       #-}
 
-module NITTA.Conf ( addManual, createMicroarch, endManual ) where
+module NITTA.Conf (addFram, addAccum, addMul, addDiv, addSpi, addManual, evalNetwork, evalNetworkM, endManual, end ) where
 
 import           Control.Monad.State.Lazy
 import           NITTA.Model.Networks.Bus
+import           NITTA.Model.Networks.Types
+import           NITTA.Model.ProcessorUnits
+import           NITTA.Model.ProcessorUnits.Types
+import           Data.Default                     (def)
+
+divPortConst = 4 
+anySpiConst = 0 
 
 -- manual add PU
 addManual el = get >>= \lst -> put $ lst ++ [el]
 
 -- manual create BusNetwork with width parameter counter
-endManual counter ioSync = get >>= (\pu -> return $ busNetwork counter ioSync pu)
+endManual counter ioSync = busNetwork counter ioSync <$> get  
 
--- eval state and create microarch
-createMicroarch net = evalState net []
+-- eval state and create microarch auto
+evalNetwork net = evalState net (0, [])  
 
+-- eval state and create microarch manual
+evalNetworkM net = evalState net (0, [])  
 
+-- add fram auto
+addFram name = do
+             networkState <- get
+             let (i, pu) = networkState
+                 puListOut = pu ++ [(name, PU def def FramPorts{ oe=SignalTag (i+5), wr=SignalTag (i+4), addr=map SignalTag [i+3, i+2, i+1, i] } FramIO)]
+                 counterOut = i + 6
+             put (counterOut, puListOut)
+
+-- add accum auto 
+addAccum name = do
+             networkState <- get
+             let (i, pu) = networkState
+                 puListOut = pu ++ [(name, PU def def AccumPorts{ init=SignalTag i, load=SignalTag (i+1), neg=SignalTag (i+2), oe=SignalTag (i+3) } AccumIO )]
+                 counterOut = i + 4 
+             put (counterOut, puListOut)
+
+-- add mul auto
+addMul name = do
+             networkState <- get
+             let (i, pu) = networkState
+                 puListOut = pu ++ [(name, PU def (multiplier True) MultiplierPorts{ wr=SignalTag i, wrSel=SignalTag (i+1), oe=SignalTag (i+2) } MultiplierIO )]
+                 counterOut = i + 3 
+             put (counterOut, puListOut)
+
+-- add div auto
+addDiv name = do
+             networkState <- get
+             let (i, pu) = networkState
+                 puListOut = pu ++ [(name, PU def (divider divPortConst True) DividerPorts{ wr=SignalTag i, wrSel=SignalTag (i+1), oe=SignalTag (i+2), oeSel=SignalTag (i+3) } DividerIO )]
+                 counterOut = i + 4 
+             put (counterOut, puListOut)
+
+-- add spi auto
+addSpi name pos = do
+             networkState <- get
+             let (i, pu) = networkState
+                 puListOut = pu ++ [(name, PU def
+                    (anySPI anySpiConst)
+                    SimpleIOPorts
+                        { wr=SignalTag i, oe=SignalTag (i+1) 
+                        , stop="stop"
+                        }
+                    $ case pos of
+                        "slave" -> SPISlave
+                            { slave_mosi=InputPortTag "mosi"
+                            , slave_miso=OutputPortTag "miso"
+                            , slave_sclk=InputPortTag "sclk"
+                            , slave_cs=InputPortTag "cs"
+                            }
+                        "master" -> SPIMaster
+                            { master_mosi=OutputPortTag "mosi"
+                            , master_miso=InputPortTag "miso"
+                            , master_sclk=OutputPortTag "sclk"
+                            , master_cs=OutputPortTag "cs"
+                            }
+                        _        -> error "error while configure SPI! Set 'master' or 'slave'"
+                    )]
+                 counterOut = i + 2 
+             put (counterOut, puListOut)
+
+-- end ioSync 
+end ioSync = busNetworkS ioSync <$> get 
+           
+busNetworkS ioSync (counter, pu) = busNetwork counter ioSync pu          
