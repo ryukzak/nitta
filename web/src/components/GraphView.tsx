07e329a7b24eb065ae137bbc16f7b98e1c3f1a66
iburakov <speedwatson@gmail.com>
2019-09-14 09:32:07 +0300

    Merge functions and types from GraphUtils back

--- web/src/components/GraphView.tsx
+++ web/src/components/GraphView.tsx
@@ -2,7 +2,6 @@ import * as React from "react";
 import "react-table/react-table.css";
 import { haskellAPI } from "../middleware/haskell-api";
 import { Graphviz } from "graphviz-react";
-import { renderGraphJsonToDot } from "./GraphUtils";
 
 /**
  * Component to display algorithm graph.
@@ -22,6 +21,24 @@ interface GraphViewState {
   view: string;
   status: boolean;
   graph: any;
+}
+
+export type EdgeId = number;
+
+export interface IEdgeJson {
+  from: EdgeId;
+  to: EdgeId;
+  label: string;
+}
+
+export interface INodeJson {
+  id: EdgeId;
+  label: string;
+}
+
+export interface IGraphJson {
+  edges: IEdgeJson[];
+  nodes: INodeJson[];
 }
 
 export class GraphView extends React.Component<GraphViewProps, GraphViewState> {
@@ -55,14 +72,15 @@ export class GraphView extends React.Component<GraphViewProps, GraphViewState> {
   graphMaker(nid: number) {
     haskellAPI.simpleSynthesisGraph(nid)
       .then((response: any) => {
+        // TODO: Replace with backend types
         let newNid = response.data;
         let graph = { nodes: [], edges: [] };
 
         newNid.nodes.map((anObjectMapped: any, index: number) => {
           graph.nodes[index] = { id: index + 1, label: String(anObjectMapped.label) };
         });
-        newNid.edges.map((anObjectMapped: any, index: number) => {
-          graph.edges[index] = ({ from: anObjectMapped.from, to: anObjectMapped.to, label: anObjectMapped.label });
+        newNid.edges.map((edge: any, index: number) => {
+          graph.edges[index] = edge;
         });
 
         if (this.state.view === "synthesisNode") {
@@ -86,3 +104,38 @@ export class GraphView extends React.Component<GraphViewProps, GraphViewState> {
     );
   }
 }
+
+function isString(obj: any) {
+  return typeof obj === "string" || obj instanceof String;
+}
+
+function renderDotOptions(options: object) {
+  let result = [];
+
+  for (let key in options) {
+    let representation = (isString(options[key])) ? `"${options[key]}"` : options[key];
+    result.push(`${key}=${representation}`);
+  }
+
+  return `[${result.join("; ")}]`;
+}
+
+function renderGraphJsonToDot(json: IGraphJson): string {
+  let lines = [
+    // "rankdir=LR"
+  ];
+
+  lines.push(...json.nodes.map(
+    node => node.id + " " + renderDotOptions({ label: node.label })
+  ));
+
+  lines.push(...json.edges.map(
+    edge => `${edge.from} -> ${edge.to} ` + renderDotOptions({ label: edge.label })
+  ));
+
+  const wrap = (content: string) => `\t${content};`;
+  let result = `digraph {\n${lines.map(wrap).join("\n")}\n}`;
+  console.log(result);
+  return result;
+}
+
