6e3e32b44859abc181fb01e09196f15205afe7d9
daniilprohorov <daniilprohorov@gmail.com>
2019-09-21 17:40:11 +0300

    change join to unlines

--- src/NITTA/Project/Snippets.hs
+++ src/NITTA/Project/Snippets.hs
@@ -35,21 +35,22 @@ import           Text.Regex
 
 inlineMarker = "###"
 
-inline str = S.join "\n" $ map (inlineMarker ++) $ lines str
+inline str = unlines $ map (inlineMarker ++ ) $ lines str
 
-codeBlock indent str0 = let
+codeBlock indent str0 = 
+    let
         str1 = drop 1 $ lines str0
         str2 = map lstripInline str1
         withBadIndent = filter (> 0) $ map (length . takeWhile (== ' ')) $ filter (not . isInline) str2
         badIndent = if null withBadIndent then 0 else minimum withBadIndent
-        str3 = S.join "\n" $ map (fixIndent_ badIndent) str2
+        str3 = unlines $ map (fixIndent_ badIndent) str2
     in S.replace inlineMarker "" str3
     where  
         lstripInline s = subRegex (mkRegex $ "^[[:space:]]*" ++ inlineMarker) s inlineMarker
         isInline = S.startswith inlineMarker
         fixIndent_ badIndent s 
             | isInline s = s
-            | otherwise = replicate (indent * 4) ' ' ++ (drop badIndent s)
+            | otherwise = replicate (indent * 4) ' ' ++ drop badIndent s
 
 codeLine indent str = replicate (indent * 4) ' ' ++ dropWhile (== ' ') str ++ "\n"
 
