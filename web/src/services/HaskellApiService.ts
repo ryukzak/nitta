295941d29fbdaaf82e9235268810d9f8a5b99f11
iburakov <speedwatson@gmail.com>
2019-09-28 10:13:19 +0300

    Add HaskellApiService from old project

---
+++ web/src/services/HaskellApiService.ts
@@ -0,0 +1,18 @@
+import api from "../gen/rest_api.js";
+
+// TODO: argument typing
+
+export const haskellApiService = {
+  getSynthesis: () => api.getSynthesis(),
+  getNode: (nid: any) => api.getSynthesisByNId(nid),
+  getEdge: (nid: any) => api.getSynthesisByNIdEdge(nid),
+  getEdges: (nid: string) => api.getSynthesisByNIdEdges(nid),
+  getTimelines: (nid: string) => api.getSynthesisByNIdTimelines(nid),
+  simpleSynthesis: (nid: any) => api.postSynthesisByNIdSimpleSynthesis(nid),
+  simpleSynthesisGraph: (nid: any) => api.getSynthesisByNIdModelAlg(nid),
+  smartBindSynthesisIO: (nid: any) => api.postSynthesisByNIdSmartBindSynthesisIO(nid),
+  allBestThread: (nid: any, n: any) => api.postSynthesisByNIdAllBestThread(nid, n),
+  obviousBindThread: (nid: any) => api.postSynthesisByNIdObviousBindThread(nid),
+  runTestBench: (nid: any, name: any) => api.getSynthesisByNIdTestBenchOutput(nid, name),
+  getEndpointOptions: (nid: any) => api.getSynthesisByNIdEndpointOptions(nid),
+};
