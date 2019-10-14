7ff16ae348f1e72a8f80fe6528d25c1bedc281ee
Nitser <trif.nitser@gmail.com>
2019-10-14 12:52:34 +0300

    combine into one SynthesisGraphView component

--- web/src/components/pages/main/SynthesisGraphView.tsx
+++ web/src/components/pages/main/SynthesisGraphView.tsx
@@ -1,38 +1,73 @@
 import * as React from "react";
-import { Container } from "react-bootstrap";
+import { Button } from "react-bootstrap";
+import SynthesisGraph from "./SynthesisGraph";
 import AppContext from "../../app/AppContext";
 
-export interface ISynthesisGraphProps { }
+export interface ITopButtonViewProps { }
 
-export interface ISynthesisGraphState { }
+export interface ITopButtonViewState {
+    changedSynthesisGraphHeight: (newSynthesisGraphHeight: number) => void;
+    synthesisGraphHeight: number;
+    minSynthesisGraphHeight: number;
+}
 
+export default class TopButtonView extends React.Component<ITopButtonViewProps, ITopButtonViewState> {
+    static contextType = AppContext;
+    context!: React.ContextType<typeof AppContext>;
 
-export default class SynthesisGraph extends React.Component<ISynthesisGraphProps, ISynthesisGraphState> {
-  static contextType = AppContext;
-  context!: React.ContextType<typeof AppContext>;
-  private myRef = React.createRef<HTMLDivElement>()
+    private containerRef = React.createRef<HTMLDivElement>();
+    private buttonRef = React.createRef<HTMLDivElement>();
 
-  constructor(props: ISynthesisGraphProps) {
-    super(props);
-
-    this.state = {}
-  }
-
-  componentDidMount() {
-    if (this.myRef.current != null && this.context.synthesisGraphHeight === 0) {
-      this.context.changedSynthesisGraphHeight((this.context.synthesisGraphHeight = this.myRef.current.clientHeight));
-      this.context.minSynthesisGraphHeight = this.context.synthesisGraphHeight;
+    constructor(props: ITopButtonViewProps) {
+        super(props);
+        this.state = {
+            synthesisGraphHeight: 0,
+            minSynthesisGraphHeight: 0,
+            changedSynthesisGraphHeight: (height: number) => { this.setState({ synthesisGraphHeight: height }) }
+        };
     }
-  }
 
-  public render() {
-    return (
-      <div className="text-center p-5 bg-light border m-2 flex-grow-1" ref={this.myRef} style={{height: this.context.synthesisGraphHeight}}>
-        <h1 className="text-black-50">SynthesisGraph</h1>
-        <Container className="mt-5">
-          <h1>{this.context.synthesisGraphHeight}</h1>
-        </Container>
-      </div>
-    );
-  }
+    componentDidMount = () => {
+        if (this.containerRef.current != null && this.buttonRef.current != null && this.state.synthesisGraphHeight === 0) {
+            let defaultHeight = this.containerRef.current.clientHeight - this.buttonRef.current.clientHeight;
+            console.log(defaultHeight);
+            this.state.changedSynthesisGraphHeight(defaultHeight);
+            this.setState({ minSynthesisGraphHeight: defaultHeight });
+        }
+    }
+
+    resizeSynthesisGraphView = (expand: boolean) => {
+        if (expand) {
+            this.state.changedSynthesisGraphHeight(this.state.synthesisGraphHeight + 100);
+        } else if (this.state.synthesisGraphHeight > this.state.minSynthesisGraphHeight) {
+            this.state.changedSynthesisGraphHeight(this.state.synthesisGraphHeight - 100);
+        }
+    }
+
+    reload = () => {
+        this.context.selectNode((this.context.selectedNodeId = null))
+    }
+
+    public render() {
+        return (
+            <div className="h-100" ref={this.containerRef}>
+                <div className="d-flex justify-content-between my-1  mx-2" ref={this.buttonRef}>
+                    <div className="d-flex flex-row my-auto">
+                        <Button className="mr-3" onClick={() => this.resizeSynthesisGraphView(true)}>Expand</Button>
+                        <Button className="mr-3" onClick={() => this.resizeSynthesisGraphView(false)}>Reduse</Button>
+                        <Button className="mr-3" onClick={() => this.reload()}>Refresh</Button>
+                    </div>
+                    <div className="d-flex flex-row-reverse my-auto">
+                        <h6>black - processed node; white - in progress node; green - succees synthesis</h6>
+                    </div>
+                </div>
+                <div className="justify-content-center bg-light border p-2" style={{ height: this.state.synthesisGraphHeight }}>
+                    <div className="h-100 d-flex align-items-center justify-content-center text-black-50">
+                        <h1>Empty graph</h1>
+                    </div>
+                </div>
+            </div>
+        );
+    }
+
 }
