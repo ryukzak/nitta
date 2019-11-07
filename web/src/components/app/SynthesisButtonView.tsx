de4bc394a391dbae628310b2d6449b9b3aa92aa4
Nitser <trif.nitser@gmail.com>
2019-11-07 21:50:28 +0300

    combining button handler functions

--- web/src/components/app/SynthesisButtonView.tsx
+++ web/src/components/app/SynthesisButtonView.tsx
@@ -2,43 +2,35 @@ import * as React from "react";
 import { Button } from "react-bootstrap";
 import { haskellApiService } from "../../services/HaskellApiService";
 import { AppContext, IAppContext, SelectedNodeId } from "./AppContext";
-import { AxiosResponse, AxiosError } from "axios";
+import { AxiosPromise, AxiosResponse, AxiosError } from "axios";
 
 import "./AppNavbar.scss";
 
 export const SynthesisButtonView: React.FC = () => {
   const appContext = React.useContext(AppContext) as IAppContext;
 
-  function simpleSynthesis(nid: SelectedNodeId) {
-    haskellApiService
-      .simpleSynthesis(nid)
-      .then((response: AxiosResponse<SelectedNodeId>) => {
-        appContext.selectNode(response.data);
-      })
-      .catch((err: AxiosError) => alert(err));
+  enum ButtonAct {
+    SympleSynthesis,
+    SmartBindSynthesisIO,
+    AllBestThread,
+    ObviousBindThread
   }
 
-  function smartBindSynthesisIO(nid: SelectedNodeId) {
-    haskellApiService
-      .smartBindSynthesisIO(nid)
-      .then((response: AxiosResponse<SelectedNodeId>) => {
-        appContext.selectNode(response.data);
-      })
-      .catch((err: AxiosError) => alert(err));
-  }
+  function buttonHandler(nid: SelectedNodeId, act: ButtonAct, n?: number) {
+    let fun: () => AxiosPromise = () => {
+      switch (act) {
+        case ButtonAct.SympleSynthesis:
+          return haskellApiService.simpleSynthesis(nid);
+        case ButtonAct.SmartBindSynthesisIO:
+          return haskellApiService.smartBindSynthesisIO(nid);
+        case ButtonAct.AllBestThread:
+          return haskellApiService.allBestThread(nid, n);
+        case ButtonAct.ObviousBindThread:
+          return haskellApiService.obviousBindThread(nid);
+      }
+    };
 
-  function allBestThread(nid: SelectedNodeId, n: number) {
-    haskellApiService
-      .allBestThread(nid, n)
-      .then((response: AxiosResponse<SelectedNodeId>) => {
-        appContext.selectNode(response.data);
-      })
-      .catch((err: AxiosError) => alert(err));
-  }
-
-  function obviousBindThread(nid: SelectedNodeId) {
-    haskellApiService
-      .obviousBindThread(nid)
+    fun()
       .then((response: AxiosResponse<SelectedNodeId>) => {
         appContext.selectNode(response.data);
       })
@@ -47,30 +39,33 @@ export const SynthesisButtonView: React.FC = () => {
 
   const buttonAttrs = {
     className: "mr-1 mb-1 btn-sm btn-secondary p-1.5",
-    bsPrefix: "syn",
+    bsPrefix: "syn"
   };
 
   return (
     <div className="d-flex">
       <div className="mr-3">
-        <Button {...buttonAttrs} onClick={() => simpleSynthesis(appContext.selectedNodeId)}>
+        <Button {...buttonAttrs} onClick={() => buttonHandler(appContext.selectedNodeId, ButtonAct.SympleSynthesis)}>
           Simple synthesis
         </Button>
-        <Button {...buttonAttrs} onClick={() => smartBindSynthesisIO(appContext.selectedNodeId)}>
+        <Button
+          {...buttonAttrs}
+          onClick={() => buttonHandler(appContext.selectedNodeId, ButtonAct.SmartBindSynthesisIO)}
+        >
           Smart bind synthesis
         </Button>
       </div>
       <div>
-        <Button {...buttonAttrs} onClick={() => allBestThread(appContext.selectedNodeId, 2)}>
+        <Button {...buttonAttrs} onClick={() => buttonHandler(appContext.selectedNodeId, ButtonAct.AllBestThread, 2)}>
           All best tread 2
         </Button>
-        <Button {...buttonAttrs} onClick={() => allBestThread(appContext.selectedNodeId, 1)}>
+        <Button {...buttonAttrs} onClick={() => buttonHandler(appContext.selectedNodeId, ButtonAct.AllBestThread, 1)}>
           All best tread 1
         </Button>
-        <Button {...buttonAttrs} onClick={() => allBestThread(appContext.selectedNodeId, 0)}>
+        <Button {...buttonAttrs} onClick={() => buttonHandler(appContext.selectedNodeId, ButtonAct.AllBestThread, 0)}>
           Best tread
         </Button>
-        <Button {...buttonAttrs} onClick={() => obviousBindThread(appContext.selectedNodeId)}>
+        <Button {...buttonAttrs} onClick={() => buttonHandler(appContext.selectedNodeId, ButtonAct.ObviousBindThread)}>
           Obvious bind thread
         </Button>
       </div>
