707f8832393978b8c343e9566e0c7724cfa01149
iburakov <speedwatson@gmail.com>
2019-11-19 20:18:50 +0300

    Revert "appContext deconstructing for rerendering"
    This reverts commit 93b1ce70e10073f39962c68049c7dac5b95dc93a.

--- web/src/components/pages/debug/DebugView.tsx
+++ web/src/components/pages/debug/DebugView.tsx
@@ -6,20 +6,24 @@ import { JsonView } from "../node/JsonView";
 
 export interface IDebugViewProps {}
 
+export interface IDebugViewState {
+  synthesisNode: any;
+}
+
 export const DebugView: React.FC<IDebugViewProps> = props => {
-  const appContext = React.useContext(AppContext) as IAppContext;
+  const { selectedNodeId } = React.useContext(AppContext) as IAppContext;
 
   const [synthesisNodeData, setSynthesisNodeData] = React.useState<any | null>(null);
   React.useEffect(() => {
     haskellApiService
-      .getNode(appContext.selectedNodeId)
+      .getNode(selectedNodeId)
       .then((response: any) => setSynthesisNodeData(response.data))
       .catch((err: any) => console.error(err));
-  }, [appContext.selectedNodeId]);
+  }, [selectedNodeId]);
 
   return (
     <div className="m-3">
-      {appContext.selectedNodeId ? (
+      {selectedNodeId ? (
         synthesisNodeData ? (
           <div className="d-flex flex-row">
             <JsonView src={synthesisNodeData} collapseStringsAfterLength={120} />
