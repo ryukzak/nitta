1d63d7babfb97b406d92d130c2874688d6f92e75
Nitser <trif.nitser@gmail.com>
2020-01-04 16:22:16 +0300

    Add forward syn graph navigation

--- web/src/components/pages/synthesis/SynthesisGraphContainer.tsx
+++ web/src/components/pages/synthesis/SynthesisGraphContainer.tsx
@@ -1,7 +1,9 @@
 import * as React from "react";
 import { Button } from "react-bootstrap";
 import { SynthesisGraphView } from "./SynthesisGraphView";
-import { AppContext, IAppContext, reLastNidStep } from "../../app/AppContext";
+import { AppContext, IAppContext, reLastNidStep, SelectedNodeId } from "../../app/AppContext";
+import { haskellApiService } from "../../../services/HaskellApiService";
+import { AxiosResponse, AxiosError } from "axios";
 
 export const SynthesisGraphContainer: React.FC = () => {
   const appContext = React.useContext(AppContext) as IAppContext;
@@ -9,6 +11,9 @@ export const SynthesisGraphContainer: React.FC = () => {
   const step = 100;
   const minHeight = 200;
   const [height, setHeight] = React.useState<number>(minHeight);
+
+  const nextNidStep = new RegExp(appContext.selectedNodeId + "-[^-]*");
+  const firstNidStep = /-[^-]*/;
 
   const buttonAttrs = {
     className: "btn btn-sm mr-3",
@@ -25,10 +30,23 @@ export const SynthesisGraphContainer: React.FC = () => {
     else appContext.selectNode("-");
   };
 
+  const forwardNavigation = () => {
+    haskellApiService
+      .allBestThread(appContext.selectedNodeId, 0)
+      .then((response: AxiosResponse<SelectedNodeId>) => {
+        let newId = response.data;
+        if (appContext.selectedNodeId === "-") newId = firstNidStep.exec(newId)![0];
+        else newId = nextNidStep.exec(newId)![0];
+
+        if (newId != null) appContext.selectNode(newId);
+      })
+      .catch((err: AxiosError) => console.log(err));
+  };
+
   return (
     <div className="flex-grow-1">
       <div className="d-flex justify-content-between m-2">
-        <div className="mr-3">
+        <div>
           <Button {...buttonAttrs} onClick={() => expandSynthesisGraphView()}>
             Expand
           </Button>
@@ -38,11 +56,11 @@ export const SynthesisGraphContainer: React.FC = () => {
           <Button {...buttonAttrs} onClick={() => appContext.reloadSelectedNode()}>
             Refresh
           </Button>
-        </div>
-
-        <div className="mr-3">
           <Button {...buttonAttrs} onClick={() => backNavigation()}>
             Back
+          </Button>
+          <Button {...buttonAttrs} onClick={() => forwardNavigation()}>
+            Forward
           </Button>
         </div>
         <span className="text-muted">black - processed node; white - in progress node; green - succees synthesis</span>
