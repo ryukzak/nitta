03bf060d8e9aa53dada27f11fef644a29f4d1655
Aleksandr Penskoi <aleksandr.penskoi@gmail.com>
2020-02-14 14:25:21 +0300

    Refactored. Added scheme with internal structure.

--- hdl/spi/pu_slave_spi.v
+++ hdl/spi/pu_slave_spi.v
@@ -1,8 +1,33 @@
 `timescale 1 ms/ 1 ms
 
-// FIXME: Почистить реализацию модуля. Перенести модули тестового окружения в поддиректорию "test".
+// FIXME: attr_in and attr_out
 
-// FIXME: Необходимо сделать корректную работу с атрибутами (архитектура, testbench-и, испытания в железе).
+//                             ----------------------\
+//                             signal_oe & signal_wr |
+//                                                   |                /------------------\
+//                                                   V    i2n_action  |       buffer_sel |        signal_oe  /-\
+//                             /-------------------->*--------------->|  +------------+  |      ------------>| |  data_out
+//                             | i2n_splitter_ready                   |  | i2n_buf[0] |  |                   | |----------->
+//                             |                        i2n_buf_mode  |  +------------+  |------------------>| |
+//                           +--------------+           ------------->|  +------------+  |  i2n_buf_data_out \-/
+//           /-------------->| i2n_splitter |------------------------>|  | i2n_buf[1] |  |
+//           | spi_ready     +--------------+  i2n_splitter_data_out  |  +------------+  |
+//           | i2n_splitter_from_spi                                  \------------------/
+//           |
+//        +------------+
+// ------>| spi_driver |
+//  miso  +------------+
+//  mosi     ^                                                        /------------------\  n2i_buf_mode
+//  sclk     |                                                        |       buffer_sel |<-------------
+//    cs     |                                                        |  +------------+  |
+//           |               +--------------+                         |  | n2i_buf[0] |  |  n2i_action
+//           \---------------| n2i_splitter |<------------------------|  +------------+  |<-------------*<-----------------------
+//             spi_prepare   +--------------+   n2i_buf_data_out      |  +------------+  |              ^  signal_wr & !signal_oe
+//             n2i_splitter_data_out   |                              |  | n2i_buf[1] |  |  data_in     |
+//                                     |                              |  +------------+  |<--------     |
+//                                     |                              \------------------/              |
+//                                     | n2i_splitter_ready                                             |
+//                                     \----------------------------------------------------------------/
 
 module pu_slave_spi #
         ( parameter DATA_WIDTH     = 32
@@ -45,71 +70,16 @@ reg disabled = 0;
 reg buffer_sel; // buffer selector
 
 
-
-///////////////////////////////////////////////////////////
-// data flow from NITTA to SPI driver (n2i)
-
-wire                  n2i_buf_mode[1:0];
-wire                  n2i_action[1:0];
-wire [DATA_WIDTH-1:0] n2i_buf_data_out[1:0];
-
-generate
-    genvar i;
-    for ( i = 0; i < 2; i = i + 1 ) begin : n2i_buf_i
-        buffer #
-                ( .BUF_SIZE( BUF_SIZE )
-                , .DATA_WIDTH( DATA_WIDTH )
-                , .I(i)
-                ) n2i_buf // from nitta to spi
-            ( .clk( clk )
-            , .rst( rst | flag_stop )
-
-            , .receive_mode( n2i_buf_mode[i] )
-            , .action( n2i_action[i] )
-
-            , .data_in( data_in )
-            , .data_out( n2i_buf_data_out[i] )
-            ); 
-    end
-endgenerate
-
-assign n2i_buf_mode[0] = signal_cycle ? !buffer_sel :  buffer_sel;
-assign n2i_buf_mode[1] = signal_cycle ?  buffer_sel : !buffer_sel;
-
-assign n2i_action[0] = n2i_buf_mode[0] ? (signal_wr & !signal_oe) : n2i_splitter_ready ;
-assign n2i_action[1] = n2i_buf_mode[1] ? (signal_wr & !signal_oe) : n2i_splitter_ready ;
-
-// splitter: translate from DATA_WIDTH to SPI_DATA_WIDTH
-wire [SPI_DATA_WIDTH-1:0] n2i_splitter_data_out;
-wire                      n2i_splitter_ready;
-wire spi_prepare;
-
-n2i_splitter #
-        ( .DATA_WIDTH( DATA_WIDTH )
-        , .ATTR_WIDTH( ATTR_WIDTH )
-        , .SPI_DATA_WIDTH( SPI_DATA_WIDTH )
-        ) n2i_splitter
-    ( .clk( clk )
-    , .rst( rst | flag_stop )
-
-    , .spi_ready( spi_prepare )
-    , .to_spi( n2i_splitter_data_out )
-
-    , .splitter_ready( n2i_splitter_ready )
-    , .from_nitta( n2i_buf_data_out[buffer_sel] )
-    );
-
-
-
 ///////////////////////////////////////////////////////////
 // data flow from SPI driver to NITTA (i2n)
 
-// splitter: translate from SPI_DATA_WIDTH to DATA_WIDTH
+wire spi_ready;
 wire [SPI_DATA_WIDTH-1:0] i2n_splitter_from_spi;
+
 wire i2n_splitter_ready;
 wire [DATA_WIDTH-1:0] i2n_splitter_data_out;
-wire spi_ready;
 
+// splitter: translate from SPI_DATA_WIDTH to DATA_WIDTH
 i2n_splitter #
         ( .DATA_WIDTH( DATA_WIDTH )
         , .ATTR_WIDTH( ATTR_WIDTH )
@@ -161,6 +131,63 @@ assign i2n_action[1] = i2n_buf_mode[1] ? i2n_splitter_ready : (signal_oe & signa
 
 
 ///////////////////////////////////////////////////////////
+// data flow from NITTA to SPI driver (n2i)
+
+wire                  n2i_buf_mode[1:0];
+wire                  n2i_action[1:0];
+wire [DATA_WIDTH-1:0] n2i_buf_data_out[1:0];
+
+generate
+    genvar i;
+    for ( i = 0; i < 2; i = i + 1 ) begin : n2i_buf_i
+        buffer #
+                ( .BUF_SIZE( BUF_SIZE )
+                , .DATA_WIDTH( DATA_WIDTH )
+                , .I(i)
+                ) n2i_buf // from nitta to spi
+            ( .clk( clk )
+            , .rst( rst | flag_stop )
+
+            , .receive_mode( n2i_buf_mode[i] )
+            , .action( n2i_action[i] )
+
+            , .data_in( data_in )
+            , .data_out( n2i_buf_data_out[i] )
+            );
+    end
+endgenerate
+
+assign n2i_buf_mode[0] = signal_cycle ? !buffer_sel :  buffer_sel;
+assign n2i_buf_mode[1] = signal_cycle ?  buffer_sel : !buffer_sel;
+
+assign n2i_action[0] = n2i_buf_mode[0] ? (signal_wr & !signal_oe) : n2i_splitter_ready ;
+assign n2i_action[1] = n2i_buf_mode[1] ? (signal_wr & !signal_oe) : n2i_splitter_ready ;
+
+
+// splitter: translate from DATA_WIDTH to SPI_DATA_WIDTH
+wire                      n2i_splitter_ready;
+wire [SPI_DATA_WIDTH-1:0] n2i_splitter_data_out;
+
+wire spi_prepare;
+
+n2i_splitter #
+        ( .DATA_WIDTH( DATA_WIDTH )
+        , .ATTR_WIDTH( ATTR_WIDTH )
+        , .SPI_DATA_WIDTH( SPI_DATA_WIDTH )
+        ) n2i_splitter
+    ( .clk( clk )
+    , .rst( rst | flag_stop )
+
+    , .spi_ready( spi_prepare )
+    , .to_spi( n2i_splitter_data_out )
+
+    , .splitter_ready( n2i_splitter_ready )
+    , .from_nitta( n2i_buf_data_out[buffer_sel] )
+    );
+
+
+
+///////////////////////////////////////////////////////////
 // SPI driver
 wire f_mosi, f_cs, f_sclk;
 
@@ -179,8 +206,8 @@ pu_slave_spi_driver #
     , .cs( f_cs )
     );
 
-// bounce filter
 
+// bounce filter
 // FIXME: looks bad. Original signals are synchoronised, but filtered signals not.
 bounce_filter #( .DIV(BOUNCE_FILTER) ) f_mosi_filter ( rst, clk, mosi, f_mosi );
 bounce_filter #( .DIV(BOUNCE_FILTER) ) f_cs_filter   ( rst, clk, cs,   f_cs   );
@@ -200,7 +227,6 @@ always @( posedge clk ) begin
 end
 
 wire transport_end = !prev_f_cs && f_cs;
-
 always @( posedge clk ) begin
     if      ( rst ) flag_stop <= 0;
     else if ( disabled ) flag_stop <= 1;
@@ -208,22 +234,7 @@ always @( posedge clk ) begin
     else flag_stop <= 0;
 end
 
-reg [1:0] count_word;
-always @( posedge clk ) begin
-    if      ( rst | flag_stop )    count_word <= 0;
-    else if ( i2n_splitter_ready ) count_word <= count_word + 1;
-end
-
-reg capture_word;
-always @(posedge clk) begin
-    if      ( rst )       capture_word <= 0;
-    else if ( flag_stop ) capture_word <= count_word != SIZE_WORDS; // TODO: Фиксированное значение 
-end
-
-// FIXME:
-assign attr_out[3:1] = 3'b000;
-assign attr_out[INVALID] = capture_word;
-
 assign data_out = signal_oe ? i2n_buf_data_out[buffer_sel] : {DATA_WIDTH{1'b0}};
+assign attr_out = {ATTR_WIDTH{1'b0}};
 
 endmodule
