3ee18bdefb19e04b68dcf9e0db5a7954d41f2d79
Aleksandr Penskoi <aleksandr.penskoi@gmail.com>
2020-07-03 21:22:34 +0300

    Refactor NITTA.Test.Refactor -> NITTA.Model.Problems.Refactor.Tests

--- test/Spec.hs
+++ test/Spec.hs
@@ -18,6 +18,7 @@ import           Data.Maybe
 import qualified NITTA.Intermediate.Simulation.Tests
 import qualified NITTA.Intermediate.Types.Tests
 import qualified NITTA.LuaFrontend.Tests
+import qualified NITTA.Model.Problems.Refactor.Tests
 import qualified NITTA.Model.ProcessorUnits.Divider.Tests
 import qualified NITTA.Model.ProcessorUnits.Fram.Tests
 import qualified NITTA.Model.ProcessorUnits.Multiplier.Tests
@@ -26,7 +27,6 @@ import qualified NITTA.Model.ProcessorUnits.Serial.Shift.Tests
 import           NITTA.Test.BusNetwork
 import           NITTA.Test.Locks
 import           NITTA.Test.LuaFrontend
-import           NITTA.Test.Refactor
 import qualified NITTA.Utils.CodeFormat.Tests
 import qualified NITTA.Utils.Tests
 import           System.Environment                            (lookupEnv,
@@ -43,12 +43,12 @@ main = do
     setEnv "TASTY_QUICKCHECK_TESTS" qtests
     defaultMain $ testGroup "NITTA"
         [ busNetworkTests
-        , refactorTests
         , luaTests
         , locksTest
         , NITTA.Intermediate.Simulation.Tests.tests
         , NITTA.Intermediate.Types.Tests.tests
         , NITTA.LuaFrontend.Tests.tests
+        , NITTA.Model.Problems.Refactor.Tests.tests
         , NITTA.Model.ProcessorUnits.Divider.Tests.tests
         , NITTA.Model.ProcessorUnits.Fram.Tests.tests
         , NITTA.Model.ProcessorUnits.Multiplier.Tests.tests
