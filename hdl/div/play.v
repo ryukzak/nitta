400404ce54ae5488c7dd45cb95441f22c53a156e
Artur Valiullin <ar.valiullin@yandex.ru>
2020-09-15 16:18:15 +0300

    hdl/div: fix codestyle

--- hdl/div/play.v
+++ hdl/div/play.v
@@ -1,49 +1,46 @@
-module play (
-  input CLOCK_50,
-  // input clk,
-  input RST, // dip 1
-  input MODE, // dip 2, 0 - ext, 1 - int
-  input SPEED, // key 0 - speed up
-  input SEL, // dip3; 0 - direct, 1 - reverse 
-  output [7:0] LED,
+module play
+ #( parameter DATA_WIDTH     = 32
+  , parameter ATTR_WIDTH     = 4
+  , parameter INVALID     = 1
+  )
+ ( input CLOCK_50
+  , input RST // dip 1
+  , input MODE // dip 2, 0 - ext, 1 - int
+  , input SPEED // key 0 - speed up
+  , input SEL // dip3; 0 - direct, 1 - reverse
+  , output [7:0] LED
 
-  output ext_master_mosi,
-  input  ext_master_miso,
-  output ext_master_sclk,
-  output ext_master_cs,
-  
-  input  ext_slave_mosi,
-  output ext_slave_miso,
-  input  ext_slave_sclk,
-  input  ext_slave_cs
+  , output ext_master_mosi
+  , input  ext_master_miso
+  , output ext_master_sclk
+  , output ext_master_cs
+
+  , input  ext_slave_mosi
+  , output ext_slave_miso
+  , input  ext_slave_sclk
+  , input  ext_slave_cs
   );
-
-parameter DATA_WIDTH     = 32; 
-parameter ATTR_WIDTH     = 4;  
-parameter INVALID     = 1;  
 
 wire clk;
 pll pll(
   .inclk0(CLOCK_50),
   .c0(clk)
   );
-  
-  
+
 reg [31:0] counter_data;
 always @(posedge clk) begin
    if (RST) counter_data <= 0;
    else counter_data <= counter_data + 1;
 end
 
-
 wire [31:0] data_out;
 wire [3:0] attr_out;
 
-pu_div 
+pu_div
   #( .DATA_WIDTH( DATA_WIDTH )
    , .ATTR_WIDTH( ATTR_WIDTH )
    , .INVALID( INVALID )
-   ) unit 
+   ) unit
   ( .clk( clk )
   , .rst( RST )
   , .signal_wr( MODE )
@@ -58,5 +55,4 @@ pu_div
 
 assign LED = data_out[31:24];
 
-  
 endmodule
