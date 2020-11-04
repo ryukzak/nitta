c923ce09c056c956128fc1a6bd18e35499857d2d
Aleksandr Penskoi <aleksandr.penskoi@gmail.com>
2020-11-04 04:39:43 +0300

    remove formik dependency

--- web/src/utils/componentUtils.ts
+++ web/src/utils/componentUtils.ts
@@ -1,10 +1,6 @@
 import { AxiosPromise, AxiosResponse, AxiosError } from "axios";
 import { NodeId, IAppContext } from "../components/app/AppContext";
 
-export function getFormikFieldClass(name: string, touched: any, errors: any) {
-  return `form-control ${touched[name] && errors[name] ? "is-invalid" : ""}`;
-}
-
 export function requestNidBy<T extends Array<any>>(
   context: IAppContext,
   requestJob: (...args: T) => AxiosPromise,
