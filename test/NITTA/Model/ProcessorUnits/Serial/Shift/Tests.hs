f043dae0e647ae8313f7366c9bebc417f12a5b72
daniilprohorov <daniilprohorov@gmail.com>
2020-11-28 20:28:35 +0300

    rewrite test

--- test/NITTA/Model/ProcessorUnits/Serial/Shift/Tests.hs
+++ test/NITTA/Model/ProcessorUnits/Serial/Shift/Tests.hs
@@ -20,12 +20,9 @@ import           Test.Tasty ( testGroup )
 
 tests = testGroup "Shift PU"
     [ nittaCoSimTestCase "shift test" march
-        [ constant 1 ["1"]
-        , constant 2 ["2"]
-        , constant 3 ["3"]
-        , loop 16 "g1" ["f1"]
-        , shiftL "f1" "3" ["g1"]
+        [ loop 16 "g1" ["f1"]
+        , shiftL 1 "f1" ["g1"]
         , loop 16 "g2" ["f2"]
-        , shiftR "f2" "3" ["g2"]
+        , shiftR 1 "f2" ["g2"]
         ]
     ]
