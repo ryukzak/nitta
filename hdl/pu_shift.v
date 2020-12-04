2a2e19ed8ff41c3d1694f5d2ef1db5dd3b75175f
daniilprohorov <daniilprohorov@gmail.com>
2020-12-04 17:09:31 +0300

    working 1 shift

--- hdl/pu_shift.v
+++ hdl/pu_shift.v
@@ -21,7 +21,7 @@ module pu_shift
   , output reg  [ATTR_WIDTH-1:0] attr_out
   );
 
-wire n = signal_step ? 1 : 8;
+wire n = signal_step ? 8 : 1;
 
 reg signed [DATA_WIDTH-1:0] data;
 reg [ATTR_WIDTH-1:0] attr;
