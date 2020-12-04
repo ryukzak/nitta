67e36138b8999843bd1529829179392868934c59
daniilprohorov <daniilprohorov@gmail.com>
2020-12-04 18:12:51 +0300

    add tests for shift

--- test/NITTA/Model/ProcessorUnits/Serial/Shift/Tests.hs
+++ test/NITTA/Model/ProcessorUnits/Serial/Shift/Tests.hs
@@ -1,4 +1,5 @@
 {-# LANGUAGE PartialTypeSignatures #-}
+{-# LANGUAGE QuasiQuotes #-}
 
 {-|
 Module      : NITTA.Model.ProcessorUnits.Serial.Shift.Tests
@@ -16,6 +17,8 @@ import           NITTA.Intermediate.Functions
 import           NITTA.Model.ProcessorUnits.Tests.Utils
 import           NITTA.Model.Tests.Microarchitecture
 import           Test.Tasty ( testGroup )
+import           Text.InterpolatedString.Perl6 ( qc )
+import           NITTA.LuaFrontend.Tests.Utils
 
 
 tests = testGroup "Shift PU"
@@ -25,4 +28,39 @@ tests = testGroup "Shift PU"
         , loop 16 "g2" ["f2"]
         , shiftR 1 "f2" ["g2"]
         ]
+
+    , luaTestCase "shift lua 1" [qc|
+        function shift(x)
+            local tmp = x << 1
+            shift(tmp)
+        end
+        shift(1)
+        |]
+
+    , luaTestCase "shift lua 2" [qc|
+        function shift(x)
+            local tmp = x << 2
+            shift(tmp)
+        end
+        shift(1)
+        |]
+
+    , luaTestCase "shift left 4 and shift right 3" [qc|
+        function shift(x)
+            local tmp = x << 4
+            local tmp2 = tmp >> 3
+            shift(tmp2)
+        end
+        shift(1)
+        |]
+
+    , luaTestCase "shift left 8 and shift right 7" [qc|
+        function shift(x)
+            local tmp = x << 8
+            local tmp2 = tmp >> 7
+            shift(tmp2)
+        end
+        shift(1)
+        |]
+
     ]
