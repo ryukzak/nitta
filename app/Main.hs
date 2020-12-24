2e42b09fd2085e12a8412cbc717d42dafc2b00f4
Aleksandr Penskoi <aleksandr.penskoi@gmail.com>
2020-12-24 21:24:30 +0300

    Reformat

--- app/Main.hs
+++ app/Main.hs
@@ -1,4 +1,3 @@
-{- FOURMOLU_DISABLE -}
 {-# LANGUAGE DataKinds #-}
 {-# LANGUAGE DeriveDataTypeable #-}
 {-# LANGUAGE DuplicateRecordFields #-}
@@ -7,109 +6,104 @@
 {-# LANGUAGE PartialTypeSignatures #-}
 {-# LANGUAGE ScopedTypeVariables #-}
 {-# LANGUAGE StandaloneDeriving #-}
+
 {-# OPTIONS -fno-warn-orphans #-}
 
-{-|
-Module      : Main
-Description : NITTA CAD executable
-Copyright   : (c) Aleksandr Penskoi, 2019
-License     : BSD3
-Maintainer  : aleksandr.penskoi@gmail.com
-Stability   : experimental
--}
-module Main ( main ) where
+-- |
+--Module      : Main
+--Description : NITTA CAD executable
+--Copyright   : (c) Aleksandr Penskoi, 2019
+--License     : BSD3
+--Maintainer  : aleksandr.penskoi@gmail.com
+--Stability   : experimental
+module Main (main) where
 
-import           Control.Exception
-import           Control.Monad ( when )
-import           Data.Default ( def )
-import           Data.Maybe
-import           Data.Proxy
+import Control.Exception
+import Control.Monad (when)
+import Data.Default (def)
+import Data.Maybe
+import Data.Proxy
 import qualified Data.Text.IO as T
-import           Data.Version
-import           GHC.TypeLits
-import           NITTA.Intermediate.Simulation
-import           NITTA.Intermediate.Types
-import           NITTA.LuaFrontend
-import           NITTA.Model.Microarchitecture
-import           NITTA.Model.Networks.Bus
-import           NITTA.Model.Networks.Types
-import           NITTA.Model.ProcessorUnits
-import           NITTA.Model.ProcessorUnits.Types
-import           NITTA.Project.Parts.TestBench
-import           NITTA.TargetSynthesis ( TargetSynthesis (..), mkModelWithOneNetwork, runTargetSynthesis )
-import           NITTA.UIBackend
-import           Paths_nitta
-import           System.Console.CmdArgs hiding ( def )
-import           System.Exit
-import           System.FilePath.Posix ( joinPath )
-import           Text.Read
-import           Text.Regex
+import Data.Version
+import GHC.TypeLits
+import NITTA.Intermediate.Simulation
+import NITTA.Intermediate.Types
+import NITTA.LuaFrontend
+import NITTA.Model.Microarchitecture
+import NITTA.Model.Networks.Bus
+import NITTA.Model.Networks.Types
+import NITTA.Model.ProcessorUnits
+import NITTA.Model.ProcessorUnits.Types
+import NITTA.Project.Parts.TestBench
+import NITTA.TargetSynthesis (TargetSynthesis (..), mkModelWithOneNetwork, runTargetSynthesis)
+import NITTA.UIBackend
+import Paths_nitta
+import System.Console.CmdArgs hiding (def)
+import System.Exit
+import System.FilePath.Posix (joinPath)
+import Text.Read
+import Text.Regex
 
 -- |Command line interface.
-data Nitta
-    = Nitta
-        { filename :: FilePath
-
-        , type_    :: String
-        , io_sync  :: IOSynchronization
-
-        , port     :: Int
-
-        , n        :: Int
-        , fsim     :: Bool
-        , lsim     :: Bool
-
-        , verbose  :: Bool
-        }
-    deriving ( Show, Data, Typeable )
+data Nitta = Nitta
+    { filename :: FilePath
+    , type_ :: String
+    , io_sync :: IOSynchronization
+    , port :: Int
+    , n :: Int
+    , fsim :: Bool
+    , lsim :: Bool
+    , verbose :: Bool
+    }
+    deriving (Show, Data, Typeable)
 
 deriving instance Data IOSynchronization
 
-nittaArgs = Nitta
-    { filename=def &= argPos 0 &= typFile
+nittaArgs =
+    Nitta
+        { filename = def &= argPos 0 &= typFile
+        , type_ = "fx32.32" &= help "Data type (default: 'fx32.32')"
+        , io_sync = Sync &= help "IO synchronization mode: sync, async, onboard"
+        , port = 0 &= help "Run nitta server for UI on specific port (by default - not run)"
+        , n = 10 &= help "Number of computation cycles for simulation and testbench"
+        , fsim = False &= help "Functional simulation with trace"
+        , lsim = False &= help "Logical (HDL) simulation with trace"
+        , verbose = False &= help "Verbose"
+        }
+        &= summary ("nitta v" ++ showVersion version ++ " - CAD for reconfigurable real-time ASIP")
 
-    , type_="fx32.32" &= help "Data type (default: 'fx32.32')"
-    , io_sync=Sync &= help "IO synchronization mode: sync, async, onboard"
-
-    , port=0 &= help "Run nitta server for UI on specific port (by default - not run)"
-
-    , n=10 &= help "Number of computation cycles for simulation and testbench"
-    , fsim=False &= help "Functional simulation with trace"
-    , lsim=False &= help "Logical (HDL) simulation with trace"
-
-    , verbose=False &= help "Verbose"
-    } &= summary ("nitta v" ++ showVersion version ++ " - CAD for reconfigurable real-time ASIP")
-
-
-parseFX input = let
-        typePattern = mkRegex "fx([0-9]+).([0-9]+)"
-        [ m, b ] = fromMaybe (error "incorrect Bus type input") $ matchRegex typePattern input
+parseFX input =
+    let typePattern = mkRegex "fx([0-9]+).([0-9]+)"
+        [m, b] = fromMaybe (error "incorrect Bus type input") $ matchRegex typePattern input
         convert = fromJust . someNatVal . read
-    in ( convert m, convert b )
-
+     in (convert m, convert b)
 
 main = do
-    Nitta{ port, filename, type_, io_sync, fsim, lsim, n, verbose } <- cmdArgs nittaArgs
+    Nitta{port, filename, type_, io_sync, fsim, lsim, n, verbose} <- cmdArgs nittaArgs
     src <- readSourceCode verbose filename
-    ( \( SomeNat (_ :: Proxy m), SomeNat (_ :: Proxy b) ) -> do
-            let FrontendResult{ frDataFlow, frTrace, frPrettyCntx } = lua2functions src
+    ( \(SomeNat (_ :: Proxy m), SomeNat (_ :: Proxy b)) -> do
+            let FrontendResult{frDataFlow, frTrace, frPrettyCntx} = lua2functions src
                 -- FIXME: https://nitta.io/nitta-corp/nitta/-/issues/50
                 -- data for sin_ident
-                received = [ ("u#0", map (\i -> read $ show $ sin ((2 :: Double) * 3.14 * 50 * 0.001 * i)) [0..toEnum n])]
-                ma = ( microarch io_sync :: BusNetwork String String (FX m b) Int)
+                received = [("u#0", map (\i -> read $ show $ sin ((2 :: Double) * 3.14 * 50 * 0.001 * i)) [0 .. toEnum n])]
+                ma = (microarch io_sync :: BusNetwork String String (FX m b) Int)
 
             when verbose $ putStr $ "> will trace: \n" ++ unlines (map ((">  " ++) . show) frTrace)
 
             when (port > 0) $ do
                 buf <- try $ readFile $ joinPath ["web", "src", "gen", "PORT"]
                 let expect = case buf of
-                        Right p             -> readMaybe p
+                        Right p -> readMaybe p
                         Left (_ :: IOError) -> Nothing
                 when (expect /= Just port) $
-                    putStrLn $ concat
-                        [ "WARNING: expected backend port: ", show expect, " actual: ", show port
-                        , " (maybe you need regenerate API by nitta-api-gen)"
-                        ]
+                    putStrLn $
+                        concat
+                            [ "WARNING: expected backend port: "
+                            , show expect
+                            , " actual: "
+                            , show port
+                            , " (maybe you need regenerate API by nitta-api-gen)"
+                            ]
                 backendServer port received $ mkModelWithOneNetwork ma frDataFlow
                 exitSuccess
 
@@ -117,12 +111,13 @@ main = do
 
             TestbenchReport
                 { tbLogicalSimulationCntx
-                } <- synthesizeAndTest verbose ma n frDataFlow received
+                } <-
+                synthesizeAndTest verbose ma n frDataFlow received
 
             when lsim $ do
                 putCntx $ frPrettyCntx tbLogicalSimulationCntx
-        ) $ parseFX type_
-
+        )
+        $ parseFX type_
 
 readSourceCode verbose filename = do
     when verbose $ putStrLn $ "> read source code from: " ++ show filename ++ "..."
@@ -132,35 +127,38 @@ readSourceCode verbose filename = do
     return src
 
 functionalSimulation verbose n received src = do
-    let FrontendResult{ frDataFlow, frPrettyCntx } = lua2functions src
+    let FrontendResult{frDataFlow, frPrettyCntx} = lua2functions src
         cntx = simulateDataFlowGraph n def received frDataFlow
     when verbose $ putStrLn "> run functional simulation..."
     putStr $ cntx2table $ frPrettyCntx cntx
     when verbose $ putStrLn "> run functional simulation...ok"
 
 synthesizeAndTest verbose ma n dataflow received = do
-    Right report <- runTargetSynthesis def
-        { tName="main"
-        , tMicroArch=ma
-        , tDFG=dataflow
-        , tVerbose=verbose
-        , tReceivedValues=received
-        , tSimulationCycleN=n
-        }
+    Right report <-
+        runTargetSynthesis
+            def
+                { tName = "main"
+                , tMicroArch = ma
+                , tDFG = dataflow
+                , tVerbose = verbose
+                , tReceivedValues = received
+                , tSimulationCycleN = n
+                }
     return report
 
 putCntx cntx = putStr $ cntx2table cntx
 
 microarch ioSync = evalNetwork ioSync $ do
-        addManual "fram1" (PU def (framWithSize 16) FramPorts{ oe=SignalTag 0, wr=SignalTag 1, addr=map SignalTag [2, 3, 4, 5] } FramIO )
-        addCustom "fram2" (framWithSize 32) FramIO
-        add "shift" ShiftIO
-        add "mul" MultiplierIO
-        add "accum" AccumIO
-        add "div" DividerIO
-        add "spi" $ SPISlave
+    addManual "fram1" (PU def (framWithSize 16) FramPorts{oe = SignalTag 0, wr = SignalTag 1, addr = map SignalTag [2, 3, 4, 5]} FramIO)
+    addCustom "fram2" (framWithSize 32) FramIO
+    add "shift" ShiftIO
+    add "mul" MultiplierIO
+    add "accum" AccumIO
+    add "div" DividerIO
+    add "spi" $
+        SPISlave
             { slave_mosi = InputPortTag "mosi"
             , slave_miso = OutputPortTag "miso"
             , slave_sclk = InputPortTag "sclk"
-            , slave_cs   = InputPortTag "cs"
+            , slave_cs = InputPortTag "cs"
             }
--- test/NITTA/Model/ProcessorUnits/IO/SPI/Tests.hs
+++ test/NITTA/Model/ProcessorUnits/IO/SPI/Tests.hs
@@ -1,89 +1,125 @@
-{- FOURMOLU_DISABLE -}
-{-# LANGUAGE NoMonomorphismRestriction #-}
 {-# LANGUAGE PartialTypeSignatures #-}
 {-# LANGUAGE QuasiQuotes #-}
 {-# LANGUAGE TemplateHaskell #-}
+{-# LANGUAGE NoMonomorphismRestriction #-}
+
 {-# OPTIONS -fno-warn-partial-type-signatures #-}
 
-{-|
-Module      : NITTA.Model.ProcessorUnits.IO.SPI.Tests
-Description :
-Copyright   : (c) Aleksandr Penskoi, 2020
-License     : BSD3
-Maintainer  : aleksandr.penskoi@gmail.com
-Stability   : experimental
--}
-module NITTA.Model.ProcessorUnits.IO.SPI.Tests
-    ( tests
-    ) where
+-- |
+--Module      : NITTA.Model.ProcessorUnits.IO.SPI.Tests
+--Description :
+--Copyright   : (c) Aleksandr Penskoi, 2020
+--License     : BSD3
+--Maintainer  : aleksandr.penskoi@gmail.com
+--Stability   : experimental
+module NITTA.Model.ProcessorUnits.IO.SPI.Tests (
+    tests,
+) where
 
-import           Control.Monad ( void )
-import           Data.Default
-import           NITTA.Intermediate.DataFlow
+import Control.Monad (void)
+import Data.Default
+import NITTA.Intermediate.DataFlow
 import qualified NITTA.Intermediate.Functions as F
-import           NITTA.Intermediate.Tests.Functions ()
-import           NITTA.LuaFrontend.Tests.Utils
-import           NITTA.Model.Networks.Types
-import           NITTA.Model.Tests.Microarchitecture
-import           NITTA.TargetSynthesis
-import           Test.Tasty ( TestTree, testGroup )
-import           Test.Tasty.HUnit
-import           Test.Tasty.TH
-import           Text.InterpolatedString.Perl6 ( qc )
-
+import NITTA.Intermediate.Tests.Functions ()
+import NITTA.LuaFrontend.Tests.Utils
+import NITTA.Model.Networks.Types
+import NITTA.Model.Tests.Microarchitecture
+import NITTA.TargetSynthesis
+import Test.Tasty (TestTree, testGroup)
+import Test.Tasty.HUnit
+import Test.Tasty.TH
+import Text.InterpolatedString.Perl6 (qc)
 
 test_multiple_receives =
-    [ testCase "receive two variables" $ void $ runTargetSynthesisWithUniqName (def :: TargetSynthesis _ _ _ Int)
-        { tName="receive_two_variables"
-        , tMicroArch=marchSPI True pInt
-        , tReceivedValues=[ ("a", [10..15]), ("b", [20..25]) ]
-        , tDFG=fsToDataFlowGraph
-            [ F.receive ["a"]
-            , F.receive ["b"]
-            , F.add "a" "b" ["c"]
-            , F.send "c"
-            ]
-        }
-
-    , testCase "receive variable two times" $ void $ runTargetSynthesisWithUniqName (def :: TargetSynthesis _ _ _ Int)
-        { tName="receive_variable_two_times"
-        , tMicroArch=marchSPI True pInt
-        , tReceivedValues=[ ("a", [10..15]), ("b", [20..25]) ]
-        , tDFG=fsToDataFlowGraph
-            [ F.receive ["a", "b"]
-            , F.add "a" "b" ["c"]
-            , F.send "c"
-            ]
-        }
-
+    [ testCase "receive two variables" $
+        void $
+            runTargetSynthesisWithUniqName
+                (def :: TargetSynthesis _ _ _ Int)
+                    { tName = "receive_two_variables"
+                    , tMicroArch = marchSPI True pInt
+                    , tReceivedValues = [("a", [10 .. 15]), ("b", [20 .. 25])]
+                    , tDFG =
+                        fsToDataFlowGraph
+                            [ F.receive ["a"]
+                            , F.receive ["b"]
+                            , F.add "a" "b" ["c"]
+                            , F.send "c"
+                            ]
+                    }
+    , testCase "receive variable two times" $
+        void $
+            runTargetSynthesisWithUniqName
+                (def :: TargetSynthesis _ _ _ Int)
+                    { tName = "receive_variable_two_times"
+                    , tMicroArch = marchSPI True pInt
+                    , tReceivedValues = [("a", [10 .. 15]), ("b", [20 .. 25])]
+                    , tDFG =
+                        fsToDataFlowGraph
+                            [ F.receive ["a", "b"]
+                            , F.add "a" "b" ["c"]
+                            , F.send "c"
+                            ]
+                    }
     ]
-
 
 -- TODO: proof test work
 test_different_type =
-    [ typedIOLuaTestCase (microarch Sync SlaveSPI) pIntX32 "pIntX32 IO by sync slave"
-        received alg
-    , typedIOLuaTestCase (microarch Sync SlaveSPI) pIntX48 "pIntX48 IO by sync slave"
-        received alg
-    , typedIOLuaTestCase (microarch Sync SlaveSPI) pIntX64 "pIntX64 IO by sync slave"
-        received alg
-    , typedIOLuaTestCase (microarch Sync SlaveSPI) pIntX128 "pIntX128 IO by sync slave"
-        received alg
-    , typedIOLuaTestCase (microarch Sync MasterSPI) pIntX32 "pIntX32 IO by sync master"
-        received alg
-    , typedIOLuaTestCase (microarch Sync MasterSPI) pIntX48 "pIntX48 IO by sync master"
-        received alg
-    , typedIOLuaTestCase (microarch Sync MasterSPI) pIntX64 "pIntX64 IO by sync master"
-        received alg
-    , typedIOLuaTestCase (microarch Sync MasterSPI) pIntX128 "pIntX128 IO by sync master"
-        received alg
+    [ typedIOLuaTestCase
+        (microarch Sync SlaveSPI)
+        pIntX32
+        "pIntX32 IO by sync slave"
+        received
+        alg
+    , typedIOLuaTestCase
+        (microarch Sync SlaveSPI)
+        pIntX48
+        "pIntX48 IO by sync slave"
+        received
+        alg
+    , typedIOLuaTestCase
+        (microarch Sync SlaveSPI)
+        pIntX64
+        "pIntX64 IO by sync slave"
+        received
+        alg
+    , typedIOLuaTestCase
+        (microarch Sync SlaveSPI)
+        pIntX128
+        "pIntX128 IO by sync slave"
+        received
+        alg
+    , typedIOLuaTestCase
+        (microarch Sync MasterSPI)
+        pIntX32
+        "pIntX32 IO by sync master"
+        received
+        alg
+    , typedIOLuaTestCase
+        (microarch Sync MasterSPI)
+        pIntX48
+        "pIntX48 IO by sync master"
+        received
+        alg
+    , typedIOLuaTestCase
+        (microarch Sync MasterSPI)
+        pIntX64
+        "pIntX64 IO by sync master"
+        received
+        alg
+    , typedIOLuaTestCase
+        (microarch Sync MasterSPI)
+        pIntX128
+        "pIntX128 IO by sync master"
+        received
+        alg
     ]
     where
         received =
-            [ ( "a:0", [ 10..15 ] )
-            , ( "b:0", [ 20..25 ] )
+            [ ("a:0", [10 .. 15])
+            , ("b:0", [20 .. 25])
             ]
-        alg = [qc|
+        alg =
+            [qc|
             function sum()
                 local a = receive()
                 local b = receive()
@@ -92,7 +128,6 @@ test_different_type =
             end
             sum()
             |]
-
 
 tests :: TestTree
 tests = $(testGroupGenerator)
