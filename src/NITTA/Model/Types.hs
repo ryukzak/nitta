4d1a730983d956e362a83a30c1f22f32a7a092d4
Aleksandr Penskoi <aleksandr.penskoi@gmail.com>
2021-01-12 01:27:15 +0300

    Refactor

--- src/NITTA/Model/Types.hs
+++ src/NITTA/Model/Types.hs
@@ -1,4 +1,3 @@
-{- FOURMOLU_DISABLE -}
 {-# LANGUAGE ConstraintKinds #-}
 {-# LANGUAGE DeriveGeneric #-}
 {-# LANGUAGE FlexibleInstances #-}
@@ -8,7 +7,7 @@
 {-# LANGUAGE TypeFamilies #-}
 {-# LANGUAGE UndecidableInstances #-}
 
-{-|
+{- |
 Module      : NITTA.Model.Types
 Description : Types for time description
 Copyright   : (c) Aleksandr Penskoi, 2019
@@ -16,75 +15,75 @@ License     : BSD3
 Maintainer  : aleksandr.penskoi@gmail.com
 Stability   : experimental
 -}
-module NITTA.Model.Types
-    ( VarValTime, Time
-    , TimeConstrain(..), TaggedTime(..)
-    ) where
+module NITTA.Model.Types (
+    VarValTime,
+    Time,
+    TimeConstrain (..),
+    TaggedTime (..),
+) where
 
-import           Data.Default
-import           Data.Typeable
-import           GHC.Generics
-import           NITTA.Intermediate.Types
-import           Numeric.Interval
-
+import Data.Default
+import Data.Typeable
+import GHC.Generics
+import NITTA.Intermediate.Types
+import Numeric.Interval
 
 -- |Shortcut for variable ('v'), value ('x') and time ('t') type constrains.
-type VarValTime v x t = ( Var v, Val x, Time t )
-
+type VarValTime v x t = (Var v, Val x, Time t)
 
 -- |Shortcut for time type constrain.
-type Time t = ( Default t, Num t, Bounded t, Ord t, Show t, Typeable t, Enum t, Integral t )
-
+type Time t = (Default t, Num t, Bounded t, Ord t, Show t, Typeable t, Enum t, Integral t)
 
 -- |Time constrain for processor activity.
-data TimeConstrain t
-    = TimeConstrain
-        { tcAvailable :: Interval t -- ^Inclusive interval, when value available to transfer.
-        , tcDuration  :: Interval t -- ^Inclusive interval, possible for value transfers.
-        } deriving ( Eq )
+data TimeConstrain t = TimeConstrain
+    { -- |Inclusive interval, when value available to transfer.
+      tcAvailable :: Interval t
+    , -- |Inclusive interval, possible for value transfers.
+      tcDuration :: Interval t
+    }
+    deriving (Eq)
 
-instance ( Show t, Eq t, Bounded t ) => Show (TimeConstrain t) where
-    show TimeConstrain{ tcAvailable, tcDuration } = showInf tcAvailable ++ " /P " ++ showInf tcDuration
+instance (Show t, Eq t, Bounded t) => Show (TimeConstrain t) where
+    show TimeConstrain{tcAvailable, tcDuration} = showInf tcAvailable ++ " /P " ++ showInf tcDuration
         where
-            showInf i = let
-                    a = inf i
+            showInf i =
+                let a = inf i
                     b = sup i
-                in if b == maxBound
-                    then show a ++ "..∞"
-                    else show a ++ ".." ++ show b
+                 in if b == maxBound
+                        then show a ++ "..∞"
+                        else show a ++ ".." ++ show b
 
 -- |Forgoten implementation of tagged time for speculative if statement. Current - dead code.
-data TaggedTime tag t
-    = TaggedTime
-    { tTag   :: Maybe tag
+data TaggedTime tag t = TaggedTime
+    { tTag :: Maybe tag
     , tClock :: t
     }
-    deriving ( Typeable, Generic )
+    deriving (Typeable, Generic)
 
-instance ( Default t ) => Default (TaggedTime tag t) where
+instance (Default t) => Default (TaggedTime tag t) where
     def = TaggedTime Nothing def
 
-instance ( Time t, Show tag ) => Show (TaggedTime tag t) where
+instance (Time t, Show tag) => Show (TaggedTime tag t) where
     show (TaggedTime tag t) = show t ++ maybe "" (("!" ++) . show) tag
 
-instance {-# OVERLAPS #-} ( Time t ) => Show (TaggedTime String t) where
+instance {-# OVERLAPS #-} (Time t) => Show (TaggedTime String t) where
     show (TaggedTime tag t) = show t ++ maybe "" ("!" ++) tag
 
-instance ( Eq t ) => Eq (TaggedTime tag t) where
+instance (Eq t) => Eq (TaggedTime tag t) where
     (TaggedTime _ a) == (TaggedTime _ b) = a == b
 
-instance ( Ord t ) => Ord (TaggedTime tag t) where
+instance (Ord t) => Ord (TaggedTime tag t) where
     (TaggedTime _ a) `compare` (TaggedTime _ b) = a `compare` b
 
-instance ( Enum t ) => Enum (TaggedTime tag t) where
+instance (Enum t) => Enum (TaggedTime tag t) where
     toEnum i = TaggedTime Nothing $ toEnum i
     fromEnum (TaggedTime _ i) = fromEnum i
 
-instance ( Num t ) => Bounded (TaggedTime tag t) where
+instance (Num t) => Bounded (TaggedTime tag t) where
     minBound = TaggedTime Nothing 0
     maxBound = TaggedTime Nothing 1000
 
-instance ( Num t, Show tag, Eq tag ) => Num (TaggedTime tag t) where
+instance (Num t, Show tag, Eq tag) => Num (TaggedTime tag t) where
     (TaggedTime Nothing a) + (TaggedTime Nothing b) = TaggedTime Nothing (a + b)
     (TaggedTime (Just tag) a) + (TaggedTime Nothing b) = TaggedTime (Just tag) (a + b)
     (TaggedTime Nothing a) + (TaggedTime (Just tag) b) = TaggedTime (Just tag) (a + b)
@@ -92,7 +91,7 @@ instance ( Num t, Show tag, Eq tag ) => Num (TaggedTime tag t) where
         | tag_a == tag_b = TaggedTime tag_a (a + b)
         | otherwise = error $ "Not equal time tag! " ++ show tag_a ++ " " ++ show tag_b
     fromInteger = TaggedTime Nothing . fromInteger
-    negate t = t{ tClock=negate $ tClock t }
+    negate t = t{tClock = negate $ tClock t}
     (*) = undefined
     abs = undefined
     signum = undefined
