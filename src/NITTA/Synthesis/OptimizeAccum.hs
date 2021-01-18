f65fde34ed38eb1dad4146b8601d0a1f025b3814
Aleksandr Penskoi <aleksandr.penskoi@gmail.com>
2021-01-18 16:28:58 +0300

    Split NITTA.Synthesis.Refactor to separate modules

---
+++ src/NITTA/Synthesis/OptimizeAccum.hs
@@ -0,0 +1,49 @@
+{-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE GADTs #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE NamedFieldPuns #-}
+{-# LANGUAGE NoMonomorphismRestriction #-}
+
+{-# OPTIONS -fno-warn-orphans #-}
+
+{- |
+Module      : NITTA.Synthesis.OptimizeAccum
+Description :
+Copyright   : (c) Aleksandr Penskoi, 2021
+License     : BSD3
+Maintainer  : aleksandr.penskoi@gmail.com
+Stability   : experimental
+-}
+module NITTA.Synthesis.OptimizeAccum (
+    OptimizeAccumMetrics (..),
+) where
+
+import Data.Aeson (ToJSON)
+import GHC.Generics
+import NITTA.Model.Networks.Bus
+import NITTA.Model.Problems.Refactor
+import NITTA.Model.TargetSystem
+import NITTA.Model.Types
+import NITTA.Synthesis.Types
+
+data OptimizeAccumMetrics = OptimizeAccumMetrics
+    deriving (Generic)
+
+instance ToJSON OptimizeAccumMetrics
+
+instance
+    (VarValTime v x t) =>
+    SynthesisDecisionCls
+        (SynthesisState (TargetSystem (BusNetwork tag v x t) v x) tag v x t)
+        (TargetSystem (BusNetwork tag v x t) v x)
+        (OptimizeAccum v x)
+        (OptimizeAccum v x)
+        OptimizeAccumMetrics
+    where
+    decisions SynthesisState{sTarget} o = [(o, optimizeAccumDecision sTarget o)]
+
+    parameters SynthesisState{} OptimizeAccum{} _ = OptimizeAccumMetrics
+
+    estimate _ctx _o _d OptimizeAccumMetrics = 5000
--- src/NITTA/Synthesis/Refactor.hs
+++ src/NITTA/Synthesis/Refactor.hs
@@ -18,114 +18,18 @@ Maintainer  : aleksandr.penskoi@gmail.com
 Stability   : experimental
 -}
 module NITTA.Synthesis.Refactor (
-    BreakLoopMetrics (..),
-    OptimizeAccumMetrics (..),
-    ResolveDeadlockMetrics (..),
+    module NITTA.Synthesis.BreakLoop,
+    module NITTA.Synthesis.OptimizeAccum,
+    module NITTA.Synthesis.ResolveDeadlock,
     isRefactor,
 ) where
 
-import Data.Aeson (ToJSON)
 import Data.Maybe
-import qualified Data.Set as S
 import Data.Typeable
-import GHC.Generics
-import NITTA.Intermediate.Types
-import NITTA.Model.Networks.Bus
-import NITTA.Model.Problems.Refactor
-import NITTA.Model.ProcessorUnits
-import NITTA.Model.TargetSystem
-import NITTA.Model.Types
+import NITTA.Synthesis.BreakLoop
+import NITTA.Synthesis.OptimizeAccum
+import NITTA.Synthesis.ResolveDeadlock
 import NITTA.Synthesis.Types
-
-data OptimizeAccumMetrics = OptimizeAccumMetrics
-    deriving (Generic)
-
-instance ToJSON OptimizeAccumMetrics
-
-instance
-    (VarValTime v x t) =>
-    SynthesisDecisionCls
-        (SynthesisState (TargetSystem (BusNetwork tag v x t) v x) tag v x t)
-        (TargetSystem (BusNetwork tag v x t) v x)
-        (OptimizeAccum v x)
-        (OptimizeAccum v x)
-        OptimizeAccumMetrics
-    where
-    decisions SynthesisState{sTarget} o = [(o, optimizeAccumDecision sTarget o)]
-
-    parameters SynthesisState{} OptimizeAccum{} _ = OptimizeAccumMetrics
-
-    estimate _ctx _o _d OptimizeAccumMetrics = 5000
-
-data ResolveDeadlockMetrics = ResolveDeadlockMetrics
-    { pNumberOfLockedVariables :: Float
-    , pBufferCount :: Float
-    , pNumberOfTransferableVariables :: Float
-    }
-    deriving (Generic)
-
-instance ToJSON ResolveDeadlockMetrics
-
-instance
-    (UnitTag tag, VarValTime v x t) =>
-    SynthesisDecisionCls
-        (SynthesisState (TargetSystem (BusNetwork tag v x t) v x) tag v x t)
-        (TargetSystem (BusNetwork tag v x t) v x)
-        (ResolveDeadlock v x)
-        (ResolveDeadlock v x)
-        ResolveDeadlockMetrics
-    where
-    decisions SynthesisState{sTarget} o = [(o, resolveDeadlockDecision sTarget o)]
-
-    parameters SynthesisState{transferableVars} ResolveDeadlock{buffer} _ =
-        let buffered = outputs buffer
-         in ResolveDeadlockMetrics
-                { pNumberOfLockedVariables = fromIntegral $ S.size buffered
-                , pBufferCount = fromIntegral $ sum $ map countSuffix $ S.elems buffered
-                , pNumberOfTransferableVariables = fromIntegral (S.size $ buffered `S.intersection` transferableVars)
-                }
-
-    estimate SynthesisState{sParent} _o d _ | 0 < decisionRepeats d sParent = -2
-    estimate SynthesisState{} _o _d ResolveDeadlockMetrics{pNumberOfLockedVariables, pBufferCount, pNumberOfTransferableVariables} =
-        1000 + pNumberOfLockedVariables - pBufferCount * 1000
-            - 20 * pNumberOfTransferableVariables
-
-data BreakLoopMetrics = BreakLoopMetrics
-    deriving (Generic)
-
-instance ToJSON BreakLoopMetrics
-
-instance
-    (UnitTag tag, VarValTime v x t) =>
-    SynthesisDecisionCls
-        (SynthesisState (TargetSystem (BusNetwork tag v x t) v x) tag v x t)
-        (TargetSystem (BusNetwork tag v x t) v x)
-        (BreakLoop v x)
-        (BreakLoop v x)
-        BreakLoopMetrics
-    where
-    decisions SynthesisState{sTarget} o = [(o, breakLoopDecision sTarget o)]
-
-    parameters SynthesisState{} BreakLoop{} _ = BreakLoopMetrics
-
-    estimate _ctx _o _d BreakLoopMetrics = 5000
-
-decisionRepeats d parent =
-    let ds = toRootDecisionStrings parent
-     in length $ takeWhile (== show d) ds
-
-toRootDecisionStrings parent =
-    map
-        ( ( \case
-                SynthesisDecision{decision} -> show decision
-                _ -> ""
-          )
-            . sDecision
-        )
-        $ toRoot parent
-
-toRoot (Just tree@Tree{sState = SynthesisState{sParent}}) = tree : toRoot sParent
-toRoot _ = []
 
 isRefactor SynthesisDecision{metrics}
     | isJust (cast metrics :: Maybe BreakLoopMetrics) = True
