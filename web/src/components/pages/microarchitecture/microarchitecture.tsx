db00d283cc62a936e9dd98145c7413f1e58a4cf5
Aleksandr Penskoi <aleksandr.penskoi@gmail.com>
2021-03-01 23:01:23 +0300

    Add endpoints to microarchitecture view

--- web/src/components/pages/microarchitecture/microarchitecture.tsx
+++ web/src/components/pages/microarchitecture/microarchitecture.tsx
@@ -5,6 +5,7 @@ import { Graphviz } from "graphviz-react";
 
 import { AppContext, IAppContext } from "components/app/AppContext";
 import { api, Microarchitecture } from "services/HaskellApiService";
+import { PUEndpoints } from "services/HaskellApiService";
 
 import "./graphviz.scss";
 
@@ -17,40 +18,88 @@ export interface IIntermediateViewProps {}
 export const MicroarchitectureView: React.FC<IIntermediateViewProps> = (props) => {
   const { selectedSID } = React.useContext(AppContext) as IAppContext;
 
-  const [dot, setDot] = React.useState<string | null>(null);
+  const [ma, setMA] = React.useState<Microarchitecture | null>(null);
+  const [endpoints, setEndpoints] = React.useState<Endpoints | null>(null);
 
   React.useEffect(() => {
     api
       .getMicroarchitecture(selectedSID)
-      .then((response: AxiosResponse<Microarchitecture>) => {
-        var lines: string[] = [];
-        var units: string[] = [];
-        var vars: string[] = [];
+      .then((response: AxiosResponse<Microarchitecture>) => setMA(response.data))
+      .catch((err: AxiosError) => console.error(err));
 
-        lines.push("digraph {");
-        lines.push("  rankdir=LR;");
-        response.data.networks.forEach((net) => {
-          lines.push(`  ${net.networkTag}[label="${net.networkTag} :: ${net.valueType}"];`);
-          net.units.forEach((unit) => {
-            const name = `${net.networkTag}_${unit.unitTag}`;
-            units.push(name);
-            lines.push(`  ${name}[label="${unit.unitTag} :: ${unit.unitType}"];`);
-            lines.push(`  ${net.networkTag} ->  ${name}[dir="both"];`);
-          });
-        });
-
-        lines.push(`  { rank = some; ${units.join("; ")} }`);
-        lines.push(`  { rank = some; ${vars.join("; ")} }`);
-        lines.push("}");
-
-        setDot(lines.join("\n"));
-      })
+    api
+      .getEndpoints(selectedSID)
+      .then((response: AxiosResponse<PUEndpoints[]>) => setEndpoints(collectEndpoints(response.data)))
       .catch((err: AxiosError) => console.error(err));
   }, [selectedSID]);
 
   return (
     <div className="bg-light border graphvizContainer">
-      {dot && <Graphviz dot={dot} options={{ height: 399, width: "100%", zoom: true }} />}
+      {ma && endpoints && (
+        <Graphviz
+          dot={renderMicroarchitectureDot(ma, endpoints)}
+          options={{ height: 399, width: "100%", zoom: true }}
+        />
+      )}
     </div>
   );
 };
+
+type Endpoints = {
+  [tag: string]: {
+    sources: string[];
+    targets: string[];
+  };
+};
+
+function collectEndpoints(data: PUEndpoints[]): Endpoints {
+  let result: Endpoints = {};
+  data.forEach((eps: PUEndpoints) => {
+    const [tag, endpoints] = eps;
+    result[tag] = { sources: [], targets: [] };
+    endpoints.forEach((e) => {
+      let role = e.epRole;
+      if (role.tag === "Source") {
+        result[tag].sources.push(...role.contents);
+      }
+      if (role.tag === "Target") {
+        result[tag].targets.push(role.contents);
+      }
+    });
+  });
+  return result;
+}
+
+function renderMicroarchitectureDot(ma: Microarchitecture, endpoints: Endpoints): string {
+  var lines: string[] = [];
+  var units: string[] = [];
+  var vars: string[] = [];
+
+  lines.push("digraph {");
+  lines.push("  rankdir=LR;");
+  ma.networks.forEach((net) => {
+    lines.push(`  ${net.networkTag}[label="${net.networkTag} :: ${net.valueType}"];`);
+    net.units.forEach((unit) => {
+      const name = `${net.networkTag}_${unit.unitTag}`;
+      units.push(name);
+      lines.push(`  ${name}[label="${unit.unitTag} :: ${unit.unitType}"];`);
+      lines.push(`  ${net.networkTag} -> ${name}[dir="both"];`);
+
+      endpoints[unit.unitTag].sources.forEach((e) => {
+        lines.push(`  ${name} -> "${e}";`);
+        vars.push(`"` + e + `"`);
+      });
+      endpoints[unit.unitTag].targets.forEach((e) => {
+        lines.push(`  "${e}" -> ${name};`);
+        vars.push(`"` + e + `"`);
+      });
+    });
+  });
+
+  lines.push(`  { rank = some; ${units.join("; ")} }`);
+  lines.push(`  { rank = max; ${vars.join("; ")} }`);
+  lines.push("}");
+
+  console.log(lines.join("\n"));
+  return lines.join("\n");
+}
