28b41dbccb593919b528de9effae66475c4011b5
qr-mipro <Kostychik97@gmail.com>
2021-03-07 14:21:59 +0300

    feat: now check multiplier completes bind all vars

--- src/NITTA/Model/TestDslProt.hs
+++ src/NITTA/Model/TestDslProt.hs
@@ -1,8 +1,5 @@
-{-# LANGUAGE DataKinds #-}
-{-# LANGUAGE DuplicateRecordFields #-}
 {-# LANGUAGE FlexibleContexts #-}
 {-# LANGUAGE PartialTypeSignatures #-}
-{-# LANGUAGE ScopedTypeVariables #-}
 
 {- |
 Module      : NITTA.Model.Microarchitecture
@@ -16,56 +13,81 @@ module NITTA.Model.TestDslProt (
     ) where
 
 import Control.Monad.State.Lazy
-import Data.Default (def)
-import Debug.Trace
 import qualified NITTA.Intermediate.Functions as F
 import NITTA.Intermediate.Types
-import NITTA.Model.Networks.Bus
-import NITTA.Model.Networks.Types
 import NITTA.Model.Problems
-import NITTA.Model.Problems.Endpoint
 import NITTA.Model.ProcessorUnits
-import NITTA.Project
+import NITTA.Utils
 import Numeric.Interval.NonEmpty
-
--- Store state - st; and endpointOptions -- (endpointOpt, state)
-evalProcessUnit = flip runState []
 
 -- bind shoud put State
 -- bindFunc :: ProcessorUnit u v x t => F v x -> u ->
 bindFunc f st =
-    let res = tryBind f st
-     in case res of
-            Right v -> put [v]
-            --- TODO add assertFalse when wrong bind
-            Left err -> error $ "can't get report: " ++ err
-
--- what if [] is empty??
-makeDecision endpSt = do
-    sts <- get
-    put $ endpointDecision (head sts) endpSt : sts
+    case tryBind f st of
+        Right v -> put [v]
+        --- TODO add assertFalse when wrong bind
+        Left err -> error $ "can't get report: " ++ err
 
 -- TODO: fix not only first decision?
 --       Check for null
-provideDecision st = endpointOptionToDecision $ head $ endpointOptions st
+getDecision st = endpointOptionToDecision . head . endpointOptions $ st
+
+-- what if [] is empty??
+doDecision endpSt = do
+    sts <- get
+    put $ endpointDecision (head sts) endpSt : sts
+
+-- doDecision endpSt = modify (endpointDecision (head <$> get) endpSt :)
+
+getAndDoDecision st = doDecision . getDecision . head $ st
+
+getAndDoDecision2 = runState $ doDecision . getDecision . head <$> get
+
+-- TODO: rewrite, because `functions a` has `F [Char] Int` type
+isBinded f pu =
+    let fu = functions pu
+     in not (null fu) && (head fu == f)
+
+evalMultiplierChain st bindF [] = error ""
+evalMultiplierChain st bindF fs = head $
+    flip execState [] $ do
+        modify (bindFunc bindF st :)
+        --isBinded f st
+        evalMultiplierChain' fs
+
+evalMultiplierChain' [] = get
+evalMultiplierChain' fs = do
+    modify (head fs get :)
+    evalMultiplierChain' $ tail fs
 
 --- To check whether piping work
-checkPipe f st = evalProcessUnit $ do
+--testMultChain = evalMultiplierChain st0 fDef []
+
+checkPipe f st = flip runState [] $ do
     bindFunc f st
+    --isBinded st
 
     -- TODO assert between steps
     let ep = EndpointSt (Target "a") (1 ... 2)
-    makeDecision ep
+    doDecision ep
+
+    --    isProcessComplete (head sts) $ variables f
 
     sts <- get
-    makeDecision $ provideDecision $ head sts
+    getAndDoDecision sts
 
     sts <- get
-    makeDecision $ provideDecision $ head sts
+    getAndDoDecision sts
 
-    return $ head sts
+    sts <- get
+    return $ isProcessComplete (head sts) [f]
 
 fDef = F.multiply "a" "b" ["c", "d"] :: F String Int
 
 st0 = multiplier True :: Multiplier String Int Int
 Right st1def = tryBind fDef st0
+
+-- TODO: clean/combine with utils
+isProcessComplete pu fs = unionsMap variables fs == processedVars pu
+
+processedVars pu = unionsMap variables $ getEndpoints $ process pu
