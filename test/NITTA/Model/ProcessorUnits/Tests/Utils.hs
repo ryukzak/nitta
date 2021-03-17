1f76821005c299b6657eb7543daddc3518fe847a
qr-mipro <Kostychik97@gmail.com>
2021-03-17 19:15:32 +0300

    feat_integrity: added check integrity: functions should be present in steps, their variables should have Endpoints

--- test/NITTA/Model/ProcessorUnits/Tests/Utils.hs
+++ test/NITTA/Model/ProcessorUnits/Tests/Utils.hs
@@ -42,6 +42,7 @@ import NITTA.Model.Networks.Bus
 import NITTA.Model.Networks.Types
 import NITTA.Model.Problems hiding (Bind, BreakLoop)
 import NITTA.Model.ProcessorUnits
+import NITTA.Model.ProcessorUnits.Tests.IntegrityCheck
 import NITTA.Model.TargetSystem ()
 import NITTA.Model.Tests.Microarchitecture
 import NITTA.Project
@@ -132,6 +133,7 @@ finitePUSynthesisProp name pu0 fsGen =
         return $
             isProcessComplete pu fs
                 && null (endpointOptions pu)
+                && null (checkIntegrity pu fs)
 
 isProcessComplete pu fs = unionsMap variables fs == processedVars pu
 
