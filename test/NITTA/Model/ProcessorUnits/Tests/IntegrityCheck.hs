0db27a5033cc53e9bd660ecf3c3321c5dc0e5567
qr-mipro <Kostychik97@gmail.com>
2021-03-19 02:30:34 +0300

    feat_integrity: written functions to create maps

--- test/NITTA/Model/ProcessorUnits/Tests/IntegrityCheck.hs
+++ test/NITTA/Model/ProcessorUnits/Tests/IntegrityCheck.hs
@@ -4,9 +4,9 @@
 {-# LANGUAGE QuasiQuotes #-}
 
 {- |
-Module      : NITTA.Model.Microarchitecture
-Description : Create micro architecture functions
-Copyright   : (c) Daniil Prohorov, 2019
+Module      : NITTA.Model.ProcessorUnits.Tests.IntegrityCheck
+Description : Tests vertical relations in PU
+Copyright   : (c) co0ll3r, 2021
 License     : BSD3
 Maintainer  : aleksandr.penskoi@gmail.com
 Stability   : experimental
@@ -15,35 +15,58 @@ module NITTA.Model.ProcessorUnits.Tests.IntegrityCheck (
     checkIntegrity,
 ) where
 
+import qualified Data.Map.Strict as M
+import Data.Maybe
 import qualified Data.Set as S
 import Data.String.Interpolate
-import qualified Data.Text as T
 import NITTA.Intermediate.Types
 import NITTA.Model.Problems
 import NITTA.Model.ProcessorUnits
 
 checkIntegrity pu fs =
     let pr = process pu
-        vars = map variables fs
-        pids = checkFunction fs $ steps pr
-     in concat [checkEndpoints var pr pid | pid <- pids, var <- vars]
+        -- TODO: possible to rewrite without unpacking?
+        -- TODO check whetehr it null
+        toIntermidiateMap = foldr findIntermidiate M.empty $ steps pr
+        toEndpointMap = foldr findEndpoint M.empty $ steps pr
+        --toInstructionMap = foldr findInstruction M.empty $ steps pr
+        --- TODO pattern match at params
+        findIntermidiate step m = case pDesc step of
+            (FStep fun) -> M.insert (pID step) fun m
+            _ -> m
+        findEndpoint step m = case pDesc step of
+            (EndpointRoleStep (Source s)) -> M.insert (pID step) s m
+            (EndpointRoleStep (Target t)) -> M.insert (pID step) (S.fromList [t]) m
+            _ -> m
+     in {-
+        findInstruction step m = case pDesc step of
+            (InstructionStep instr) -> M.insert (pID step) instr m
+            _ -> m
+            -}
+        checkIntermidiateToEndpointRelation pr fs toIntermidiateMap toEndpointMap
 
-{- | Find requested functions fs in steps of a given process
- | if not found then error
--}
-checkFunction fs stps =
-    let nullSteps f =
-            if not . null $ iterSteps f
-                then iterSteps f
-                else
-                    error
-                        [__i| Requested function not found: #{ show fs }
-                                                  in steps: #{ show stps }
-                        |]
-        iterSteps f = [pID stp | stp <- stps, compFun f $ pDesc stp]
-        compFun f (FStep F{fun}) = T.pack (show f) == T.replace (T.pack "\"") T.empty (T.pack $ show fun)
-        compFun _ _ = False
-     in concatMap nullSteps fs
+--     in concat [checkEndpoints var pr pid | pid <- pids, var <- vars]
+
+checkCadToIntermidiate = undefined
+
+checkIntermidiateToEndpointRelation pr fs ieMap epMap =
+    let exec = foldr compRel S.empty rels
+        rels = relations pr
+        -- TODO is it work???
+        vars = foldr (S.intersection . variables) S.empty fs
+        compRel (Vertical r1 r2) s =
+            S.union s $
+                S.intersection vars $
+                    compRel' (M.lookup r1 ieMap) $ M.lookup r2 epMap
+        compRel _ _ = S.empty
+        compRel' (Just fun) (Just ep) = ep
+        -- TODO remove?
+        compRel' (Just fun) Nothing = error "fun to Nothing"
+        compRel' Nothing (Just ep) = error "Nothing to EP"
+        compRel' _ _ = S.empty
+     in exec
+
+checkEndpointToInstructionRelation epMap isMap = undefined
 
 {- | For a given pID finds and returns pIDs of Steps which have Endpoints
  | if pids not found then error
@@ -59,7 +82,7 @@ checkEndpoints vars pr pid =
                     function with pID=#{pid} should have more endpoints at least: #{S.difference vars foundRel};
                     expected: [ #{ show vars } ];
                        found: [ #{ show foundRel } ]
-                          pr: [ #{pr} ]
+                        step: [ #{ reverse (steps pr) !! pid } ]
                     |]
 
 -- | Goes through process relations and finds pIDs related to given pID
