0045f2b186662a6e970c82be12d2b5d7860e2964
Aleksandr Penskoi <aleksandr.penskoi@gmail.com>
2021-03-22 14:54:22 +0300

    Use callback instead closure

--- web/src/utils/download.tsx
+++ web/src/utils/download.tsx
@@ -1,16 +1,16 @@
 import * as React from "react";
 
 export const DownloadTextFile = (props: { name: string; text: string }) => {
-  const downloadTextFileCallback = (name: string, text: string) => () => {
+  const downloadTextFileCallback = React.useCallback(() => {
     const element = document.createElement("a");
-    const file = new Blob([text], { type: "text/plain" });
+    const file = new Blob([props.text], { type: "text/plain" });
     element.href = URL.createObjectURL(file);
-    element.download = name;
+    element.download = props.name;
     document.body.appendChild(element); // Required for this to work in FireFox
     element.click();
-  };
+  }, [props.name, props.text]);
   return (
-    <button className="btn btn-link" onClick={downloadTextFileCallback(props.name, props.text)}>
+    <button className="btn btn-link" onClick={downloadTextFileCallback}>
       <small>download</small>
     </button>
   );
