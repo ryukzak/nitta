3f7e4e9f6020466902092e80b52799e65d94e020
qr-mipro <Kostychik97@gmail.com>
2021-03-25 03:21:15 +0300

    feat_integrity: rewritten check integrity method (not working)

--- test/NITTA/Model/ProcessorUnits/Tests/IntegrityCheck.hs
+++ test/NITTA/Model/ProcessorUnits/Tests/IntegrityCheck.hs
@@ -1,7 +1,5 @@
 {-# LANGUAGE FlexibleContexts #-}
-{-# LANGUAGE NamedFieldPuns #-}
 {-# LANGUAGE PartialTypeSignatures #-}
-{-# LANGUAGE QuasiQuotes #-}
 
 {- |
 Module      : NITTA.Model.ProcessorUnits.Tests.IntegrityCheck
@@ -17,75 +15,29 @@ module NITTA.Model.ProcessorUnits.Tests.IntegrityCheck (
 
 import qualified Data.Map.Strict as M
 import qualified Data.Set as S
-import Data.String.Interpolate
 import NITTA.Intermediate.Types
 import NITTA.Model.ProcessorUnits
 
 checkIntegrity pu fs =
     let pr = process pu
-        vars = foldr (S.union . variables) S.empty fs
 
-        toIntermidiateMap =
-            let getInterMap = foldr findIntermidiate M.empty $ steps pr
-             in if M.null getInterMap
-                    then Nothing
-                    else Just getInterMap
-        findIntermidiate step m = case pDesc step of
-            (FStep fun) -> M.insert (pID step) fun m
-            _ -> m
+        getFuncMap = foldr M.union M.empty filterFunc
+        filterFunc = [M.insert pid (pid, f) M.empty | (FStep f) <- map pDesc $ steps pr, pid <- map pID $ steps pr]
 
-        toEndpointMap =
-            let getEpMap = foldr findEndpoint M.empty $ steps pr
-             in if foldr S.union S.empty (M.elems getEpMap) == vars
-                    then Just getEpMap
-                    else Nothing
-        findEndpoint step m = case pDesc step of
-            (EndpointRoleStep t) -> M.insert (pID step) (variables t) m
-            _ -> m
+        getEpMap = foldr M.union M.empty filterEp
+        filterEp = [M.insert pid (pid, variables e) M.empty | (EndpointRoleStep e) <- map pDesc $ steps pr, pid <- map pID $ steps pr]
+     in and [checkIntermidiateToEndpointRelation getFuncMap getEpMap pu]
 
-        toInstructionMap =
-            let getInstrMap = foldr findInstruction M.empty $ steps pr
-             in if M.null getInstrMap
-                    then Nothing
-                    else Just getInstrMap
-        findInstruction step m = case pDesc step of
-            instr@(InstructionStep _) -> M.insert (pID step) instr m
-            _ -> m
-     in not $
-            null $
-                checkEndpointToInstructionRelation pr toEndpointMap toInstructionMap $
-                    checkIntermidiateToEndpointRelation pr toIntermidiateMap toEndpointMap
-
-checkIntermidiateToEndpointRelation _ Nothing _ = error "No function found in steps of PU!"
-checkIntermidiateToEndpointRelation _ _ Nothing = error "Not all variables have their Endpoint!"
-checkIntermidiateToEndpointRelation pr (Just ieMap) (Just epMap) =
-    let checkRelation = foldr compRel S.empty $ relations pr
-
-        compRel (Vertical r1 r2) =
-            S.union $
-                compRel' r2 (M.lookup r1 ieMap) $ M.lookup r2 epMap
-        compRel' pid (Just _) (Just _) = S.fromList [pid]
-        compRel' _ _ _ = S.empty
-     in if S.size checkRelation == M.size epMap
-            then checkRelation
-            else printError epMap checkRelation
-
--- TODO is it possible to combine with checkIntermidiateToEndpointRelation?
-checkEndpointToInstructionRelation _ Nothing _ _ = error "Not all variables have their Endpoint!"
-checkEndpointToInstructionRelation _ _ Nothing _ = error "Instruction steps not found!"
-checkEndpointToInstructionRelation pr (Just epMap) (Just isMap) epPids =
-    let checkRelation = foldr compRel S.empty $ relations pr
-
-        compRel (Vertical r1 r2) =
-            S.union $
-                compRel' r2 (M.lookup r1 epMap) $ M.lookup r2 isMap
-        compRel' pid (Just _) (Just _) = S.fromList [pid]
-        compRel' _ _ _ = S.empty
-     in if S.size checkRelation == S.size epPids
-            then checkRelation
-            else printError epMap checkRelation
-
-printError epMap foundPids =
-    error --- TODO pretty print
-        [i| Steps #{ M.withoutKeys epMap foundPids } 
-                          not related to any FStep!|]
+checkIntermidiateToEndpointRelation fs eps pu = S.isSubsetOf makeRelationList fromRelation
+    where
+        fromRelation = S.fromList $ map (\(Vertical r1 r2) -> (r1, r2)) $ relations pu
+        makeRelationList =
+            S.fromList $
+                concatMap
+                    ( \(h, f) ->
+                        concatMap
+                            ( \v -> [(h, fst (eps M.! v))]
+                            )
+                            $ variables f
+                    )
+                    $ M.elems fs
