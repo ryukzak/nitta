5511070ef6016196e8ddfd94c2e624b90c926bd4
qr-mipro <Kostychik97@gmail.com>
2021-03-31 03:52:05 +0300

    feat_integrity: fixed endpoint map generation

--- src/NITTA/Model/IntegrityCheck.hs
+++ src/NITTA/Model/IntegrityCheck.hs
@@ -1,4 +1,5 @@
 {-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE NamedFieldPuns #-}
 {-# LANGUAGE PartialTypeSignatures #-}
 
 {- |
@@ -9,53 +10,43 @@ License     : BSD3
 Maintainer  : aleksandr.penskoi@gmail.com
 Stability   : experimental
 -}
-module NITTA.Model.IntegrityCheck where
+module NITTA.Model.IntegrityCheck (
+    checkIntegrity,
+) where
 
 import qualified Data.Map.Strict as M
 import qualified Data.Set as S
-import qualified Debug.Trace
 import NITTA.Intermediate.Types
 import NITTA.Model.ProcessorUnits
+import NITTA.Utils
 
---checkIntegrity :: ProcessorUnit (Process t1 i) ProcessStepID x t2 => Process t1 i -> p -> Bool
---checkIntegrity :: (ProcessorUnit u v x t, Suffix ProcessStepID) => u -> p -> Bool
 checkIntegrity pu fs =
-    let pr = process $ Debug.Trace.traceShow pu pu
-        getFuncMap = foldr M.union M.empty filterFunc
-        filterFunc = [M.insert pid (pid, f) M.empty | (FStep f) <- map pDesc filterFunc', pid <- map pID filterFunc']
-        filterFunc' = filter (\stp -> case pDesc stp of FStep _ -> True; _ -> False) $ steps pr
-
-        getEpMap = foldr M.union M.empty filterEp
-        filterEp = [M.insert pid (pid, variables e) M.empty | (EndpointRoleStep e) <- map pDesc filterEp', pid <- map pID filterEp']
-        filterEp' = filter (\stp -> case pDesc stp of EndpointRoleStep _ -> True; _ -> False) $ steps pr
+    let pr = process pu
+        getFuncMap =
+            M.fromList $
+                [ (pID, (pID, f))
+                | step@Step{pID, pDesc} <- steps pr
+                , isFB step
+                , f <- case pDesc of
+                    (FStep f) -> [f]
+                    _ -> []
+                ]
+        --- TODO what if we have 2 same variables?
+        getEpMap =
+            M.fromList $
+                concat
+                    [ concatMap (\v -> [(v, (pID, ep))]) $ variables ep
+                    | step@Step{pID, pDesc} <- steps pr
+                    , isEndpoint step
+                    , ep <- case pDesc of
+                        (EndpointRoleStep e) -> [e]
+                        _ -> []
+                    ]
      in and [checkIntermidiateToEndpointRelation getFuncMap getEpMap pr]
 
-toIntermidiateMap pu =
-    let getInterMap = foldr findIntermidiate M.empty $ steps pr
-        pr = process pu
-     in if M.null getInterMap
-            then Nothing
-            else Just getInterMap
-findIntermidiate step m = case pDesc step of
-    (FStep fun) -> M.insert (pID step) fun m
-    _ -> m
-
-getFuncMap pu = foldr M.union M.empty $ filterFunc $ process pu
-filterFunc pr = [M.insert pid (pid, f) M.empty | (FStep f) <- map pDesc $ filterFunc' pr, pid <- map pID $ filterFunc' pr]
-filterFunc' pr = filter (\stp -> case pDesc stp of FStep _ -> True; _ -> False) $ steps pr
-
-getEpMap pu = foldr M.union M.empty $ filterEp $ process pu
-filterEp pr = [M.insert pid (pid, variables e) M.empty | step <- filterEp' pr, let e = getEp $ pDesc step, let pid = pID step]
-filterEp' pr = filter (\stp -> case pDesc stp of EndpointRoleStep _ -> True; _ -> False) $ steps pr
-
-getEp (EndpointRoleStep e) = e
-getEp _ = error ""
-
--- checkIntermidiateToEndpointRelation :: (Ord k1, Variables a k1) => M.Map k2 (ProcessStepID, a) -> M.Map k1 (ProcessStepID, b) -> Process t i -> Bool
-
-checkIntermidiateToEndpointRelation fs eps pu = S.isSubsetOf makeRelationList fromRelation
+checkIntermidiateToEndpointRelation fs eps pr = S.isSubsetOf makeRelationList fromRelation
     where
-        fromRelation = S.fromList $ map (\(Vertical r1 r2) -> (r1, r2)) $ relations pu
+        fromRelation = S.fromList $ map (\(Vertical r1 r2) -> (r1, r2)) $ relations pr
         makeRelationList =
             S.fromList $
                 concatMap
