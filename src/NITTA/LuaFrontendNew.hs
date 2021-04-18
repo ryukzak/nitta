7df1cadf0642dfa25e013c48716833ab6327b389
ArturGogiyan <artur.gogiyan@gmail.com>
2021-04-18 12:32:59 +0300

    formatting

--- src/NITTA/LuaFrontendNew.hs
+++ src/NITTA/LuaFrontendNew.hs
@@ -1,87 +1,78 @@
 {-# LANGUAGE LambdaCase #-}
 {-# LANGUAGE OverloadedStrings #-}
 
-
-module NITTA.LuaFrontendNew (
-    parseLuaSources,
+module NITTA.LuaFrontendNew
+  ( parseLuaSources,
     AlgBuilderItem (..),
-    -- *Internal
+
+    -- * Internal
     findStartupFunction,
     getLuaBlockFromSources,
     processStatement,
-) where
+  )
+where
 
-import Language.Lua
 import Control.Monad.State
 import Data.Text (Text)
+import Language.Lua
 
-
-data AlgBuilderItem =
-     Constant {cName :: Text, cValueString :: Text, cValueType :: NumberType }
-        deriving (Eq)
+data AlgBuilderItem = Constant {cName :: Text, cValueString :: Text, cValueType :: NumberType}
+  deriving (Eq)
 
 instance Show AlgBuilderItem where
---    show (Function ins outs funcName values ints) = "Function { fIn = {" ++ show ins ++ "}, fOut = {" ++ show outs ++ "}, name = " ++ show funcName ++ ", values = {" ++ show values ++ "}, fInt = {" ++ show ints
-    show (Constant name value _) = "Constant { name = {" ++ show name ++ "}, value = {" ++ show value ++ "} }"
+  --    show (Function ins outs funcName values ints) = "Function { fIn = {" ++ show ins ++ "}, fOut = {" ++ show outs ++ "}, name = " ++ show funcName ++ ", values = {" ++ show values ++ "}, fInt = {" ++ show ints
+  show (Constant name value _) = "Constant { name = {" ++ show name ++ "}, value = {" ++ show value ++ "} }"
 
 funAssignStatements (FunAssign _ (FunBody _ _ (Block statements _))) = statements
 funAssignStatements _ = error "funAssignStatements : not a function assignment"
 
-
 parseLeftExp = map $ \case
-    VarName (Name v) -> v
-    e -> error $ "bad left expression: " ++ show e
+  VarName (Name v) -> v
+  e -> error $ "bad left expression: " ++ show e
 
 processStatement :: Text -> Stat -> State [AlgBuilderItem] Int
 --Lua language Stat structure parsing
 --LocalAssign
 processStatement _ (LocalAssign _names Nothing) = do
-    return 0
+  return 0
 processStatement fn (LocalAssign names (Just exps)) =
-    processStatement fn $ Assign (map VarName names) exps
-
+  processStatement fn $ Assign (map VarName names) exps
 --Assign
 processStatement fn (Assign lexps@[_] [Unop Neg (Number ntype ntext)]) =
-    processStatement fn (Assign lexps [Number ntype ("-" <> ntext)])
-
+  processStatement fn (Assign lexps [Number ntype ("-" <> ntext)])
 processStatement _ (Assign lexps@[_] [n@(Number _ _)]) = do
-    let [name] = parseLeftExp lexps
-    expConstant name n
-
+  let [name] = parseLeftExp lexps
+  expConstant name n
 processStatement startupFunctionName (Assign vars exps) | length vars == length exps = do
-    mapM_ (\(var,expr) -> processStatement startupFunctionName (Assign [var] [expr])) $ zip vars exps
-    return 0
-
-
+  mapM_ (\(var, expr) -> processStatement startupFunctionName (Assign [var] [expr])) $ zip vars exps
+  return 0
 processStatement _ _ = undefined
 
 expConstant :: Text -> Exp -> State [AlgBuilderItem] Int
 expConstant name (Number valueType valueString) = do
-    items <- get
-    put (items ++ [Constant{cName=name, cValueString=valueString, cValueType=valueType}]) 
-    return 0
+  items <- get
+  put (items ++ [Constant {cName = name, cValueString = valueString, cValueType = valueType}])
+  return 0
 expConstant _ _ = undefined
 
 buildAlg syntaxTree = flip execState st $ do
-    mapM_ (processStatement startupFunctionName) $ funAssignStatements startupFunctionDef
-    where
-        (startupFunctionName, _, startupFunctionDef) = findStartupFunction syntaxTree
-        st = []::[AlgBuilderItem]
-
+  mapM_ (processStatement startupFunctionName) $ funAssignStatements startupFunctionDef
+  where
+    (startupFunctionName, _, startupFunctionDef) = findStartupFunction syntaxTree
+    st = [] :: [AlgBuilderItem]
 
 findStartupFunction (Block statements Nothing)
-    | [call] <- filter (\case FunCall{} -> True; _ -> False) statements
-      , [funAssign] <- filter (\case FunAssign{} -> True; _ -> False) statements
-      , (FunCall (NormalFunCall (PEVar (VarName (Name fnCall))) _)) <- call
-      , (FunAssign (FunName (Name fnAssign) _ _) _) <- funAssign
-      , fnCall == fnAssign =
-        (fnCall, call, funAssign)
+  | [call] <- filter (\case FunCall {} -> True; _ -> False) statements,
+    [funAssign] <- filter (\case FunAssign {} -> True; _ -> False) statements,
+    (FunCall (NormalFunCall (PEVar (VarName (Name fnCall))) _)) <- call,
+    (FunAssign (FunName (Name fnAssign) _ _) _) <- funAssign,
+    fnCall == fnAssign =
+    (fnCall, call, funAssign)
 findStartupFunction _ = error "can't find startup function in lua source code"
-
 
 getLuaBlockFromSources src = either (\e -> error $ "Exception while parsing Lua sources: " ++ show e) id $ parseText chunk src
 
 parseLuaSources src =
-    let syntaxTree = getLuaBlockFromSources src
-        alg = buildAlg syntaxTree
-    in alg
+  let syntaxTree = getLuaBlockFromSources src
+      alg = buildAlg syntaxTree
+   in alg
--- test/NITTA/LuaFrontend/TestsNew.hs
+++ test/NITTA/LuaFrontend/TestsNew.hs
@@ -10,24 +10,23 @@
 {-# LANGUAGE TemplateHaskell #-}
 {-# LANGUAGE NoMonomorphismRestriction #-}
 
-module NITTA.LuaFrontend.TestsNew (
-    tests,
-) where
+module NITTA.LuaFrontend.TestsNew
+  ( tests,
+  )
+where
 
-
+import Control.Monad.State
 import Data.String.Interpolate
-import NITTA.LuaFrontendNew
-import Test.Tasty.HUnit
-import Test.Tasty.TH
-import Test.Tasty
 import Data.Text
 import Language.Lua
-import Control.Monad.State
+import NITTA.LuaFrontendNew
+import Test.Tasty
+import Test.Tasty.HUnit
+import Test.Tasty.TH
 
 case_find_startup_function =
-    let
-        src =
-            [__i|
+  let src =
+        [__i|
                 function sum(a)
                     local t = 2
                     local r = -1
@@ -35,24 +34,24 @@ case_find_startup_function =
                 end
                 sum(1)
             |]
-        (actualName, FunCall (NormalFunCall _ (Args actualArgValue)), FunAssign _ (FunBody actualArg _ _)) = findStartupFunction (getLuaBlockFromSources src)
-        expectedValues = (pack "sum", [Name $ pack "a"], [Number IntNum $ pack "1"])
-     in expectedValues @?= (actualName, actualArg, actualArgValue)
+      (actualName, FunCall (NormalFunCall _ (Args actualArgValue)), FunAssign _ (FunBody actualArg _ _)) = findStartupFunction (getLuaBlockFromSources src)
+      expectedValues = (pack "sum", [Name $ pack "a"], [Number IntNum $ pack "1"])
+   in expectedValues @?= (actualName, actualArg, actualArgValue)
 
 case_process_local_assignment_statement =
-    let assignment = LocalAssign [Name $ pack "a"] (Just [Number IntNum (pack "2")])
-        result = [Constant { cName = pack "a", cValueString = pack "2", cValueType = IntNum }]
-    in result @?= execState (processStatement (pack "_") assignment) []
+  let assignment = LocalAssign [Name $ pack "a"] (Just [Number IntNum (pack "2")])
+      result = [Constant {cName = pack "a", cValueString = pack "2", cValueType = IntNum}]
+   in result @?= execState (processStatement (pack "_") assignment) []
 
 case_process_assignment_statement =
-    let assignment = Assign [VarName (Name $ pack "a")] [Number IntNum (pack "2")]
-        result = [Constant { cName = pack "a", cValueString = pack "2", cValueType = IntNum }]
-    in result @?= execState (processStatement (pack "_") assignment) []
+  let assignment = Assign [VarName (Name $ pack "a")] [Number IntNum (pack "2")]
+      result = [Constant {cName = pack "a", cValueString = pack "2", cValueType = IntNum}]
+   in result @?= execState (processStatement (pack "_") assignment) []
 
 case_process_multiple_assignments_statement =
-    let assignment = Assign [VarName (Name $ pack "a"), VarName (Name $ pack "b")] [Number IntNum $ pack "2", Number FloatNum $ pack "2.5"]
-        result = [Constant { cName = pack "a", cValueString = pack "2", cValueType = IntNum}, Constant { cName = pack "b", cValueString = pack "2.5", cValueType = FloatNum}]
-    in result @?= execState (processStatement (pack "_") assignment) []
+  let assignment = Assign [VarName (Name $ pack "a"), VarName (Name $ pack "b")] [Number IntNum $ pack "2", Number FloatNum $ pack "2.5"]
+      result = [Constant {cName = pack "a", cValueString = pack "2", cValueType = IntNum}, Constant {cName = pack "b", cValueString = pack "2.5", cValueType = FloatNum}]
+   in result @?= execState (processStatement (pack "_") assignment) []
 
 tests :: TestTree
 tests = $(testGroupGenerator)
