09f36da34de38cb59d744e0eab13df2c259277ec
artyom <Kostychik97@gmail.com>
2021-04-25 19:57:57 +0300

    refactor_edsl: separated DSL tests

--- test/NITTA/Model/ProcessorUnits/Multiplier/Tests.hs
+++ test/NITTA/Model/ProcessorUnits/Multiplier/Tests.hs
@@ -22,7 +22,6 @@ import NITTA.Model.ProcessorUnits.Tests.Providers
 import NITTA.Model.Tests.Providers
 import Test.QuickCheck
 import Test.Tasty (testGroup)
-import Test.Tasty.ExpectedFailure
 
 tests =
     testGroup
@@ -95,64 +94,6 @@ tests =
             decide $ consume "b"
             decide $ provide ["c", "d"]
             assertCoSimulation
-        , expectFail $
-            puUnitTestCase "should error, when proccess is not done" u $ do
-                assign $ multiply "a" "b" ["c", "d"]
-                decideAt 1 2 $ consume "a"
-                assertSynthesisDone
-        , expectFail $
-            puUnitTestCase "should fail coSim, when variables not set" u $ do
-                assign $ multiply "a" "b" ["c", "d"]
-                decide $ consume "a"
-                decide $ consume "b"
-                decide $ provide ["c", "d"]
-                assertCoSimulation
-        , expectFail $
-            puUnitTestCase "should fail coSim, when variables incorrect" u $ do
-                assign $ multiply "a" "b" ["c", "d"]
-                setValue "a" 1
-                setValue "b" 1
-                decide $ consume "a"
-                decide $ consume "b"
-                decide $ provide ["c", "d"]
-                assertCoSimulation
-        , expectFail $
-            puUnitTestCase "should not bind, when PU incompatible with F" u $ do
-                assign $ sub "a" "b" ["c"]
-        , expectFail $
-            puUnitTestCase "decide should error, when Target in Decision is not present" u $ do
-                assign $ multiply "a" "b" ["c", "d"]
-                decideAt 1 1 $ consume "aa"
-        , expectFail $
-            puUnitTestCase "Multiplier should error, when Source in Decision is Targets" u $ do
-                assign $ multiply "a" "b" ["c", "d"]
-                decideAt 1 1 $ provide ["a"]
-        , expectFail $
-            puUnitTestCase "decide should error, when Target in Decision is Source" u $ do
-                assign $ multiply "a" "b" ["c", "d"]
-                decide $ consume "a"
-                decide $ consume "b"
-                decideAt 4 4 $ consume "c"
-        , expectFail $
-            puUnitTestCase "decide should error, when Interval is not correct" u $ do
-                assign $ multiply "a" "b" ["c", "d"]
-                decideAt 2 2 $ consume "a"
-                decideAt 1 1 $ consume "b"
-        , expectFail $
-            puUnitTestCase "should error: breakLoop is not supportd" u $ do
-                assign $ multiply "a" "b" ["c", "d"]
-                breakLoop 10 "a" ["c"]
-        , expectFail $
-            puUnitTestCase "should error: setValue variable is unavailable" u $ do
-                assign $ multiply "a" "b" ["c", "d"]
-                setValue "e" 10
-        , expectFail $
-            puUnitTestCase "should error: setValue variable is unavailable" u $ do
-                assign $ multiply "a" "b" ["c", "d"]
-                setValue "a" 10
-                setValue "b" 11
-                setValue "a" 15
-                assertSynthesisDone -- to force evaluation
         ]
     where
         u = multiplier True :: Multiplier String Int Int
