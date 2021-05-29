80779948f13371897be012c22769a067b7c8a2d8
iburakov <speedwatson@gmail.com>
2021-05-30 00:39:34 +0300

    Add TreeInfo json view to NodeScreen

--- web/src/screens/NodeScreen.tsx
+++ web/src/screens/NodeScreen.tsx
@@ -1,14 +1,34 @@
-import React, { FC, useContext } from "react";
+import React, { FC, useContext, useEffect, useState } from "react";
 import "react-table/react-table.css";
 
 import { AppContext, IAppContext } from "app/AppContext";
 import { IntermediateView } from "components/IntermediateView";
 import { MicroarchitectureView } from "components/MicroarchitectureView";
+import { api } from "services/HaskellApiService";
+import { TreeInfo } from "services/gen/types";
+import { AxiosResponse } from "axios";
+import { JsonView } from "components/JsonView";
+import { RequestStatusInfo } from "components/RequestStatusInfo";
 
 export interface INodeScreenProps {}
 
 export const NodeScreen: FC<INodeScreenProps> = (props) => {
   const { selectedSID } = useContext(AppContext) as IAppContext;
+
+  const [treeInfo, setTreeInfo] = useState<TreeInfo | null>(null);
+  const [treeInfoErrorMessage, setTreeInfoErrorMessage] = useState<string | null>(null);
+  const [treeInfoRefreshTrigger, setTreeInfoRefreshTrigger] = useState(false);
+  useEffect(() => {
+    setTreeInfoErrorMessage(null);
+    setTreeInfo(null);
+    api
+      .getTreeInfo()
+      .then((response: AxiosResponse<TreeInfo>) => setTreeInfo(response.data))
+      .catch((err: Error) => {
+        console.error(err);
+        setTreeInfoErrorMessage(`Couldn't load TreeInfo: ${err.message}`);
+      });
+  }, [selectedSID, treeInfoRefreshTrigger]);
 
   if (!selectedSID) return <pre> synthesis is not selected </pre>;
 
@@ -16,6 +36,19 @@ export const NodeScreen: FC<INodeScreenProps> = (props) => {
     <div className="m-3">
       <h3>sid:</h3>
       <pre>{selectedSID}</pre>
+
+      <h3>Tree info:</h3>
+      {treeInfo ? (
+        <JsonView src={treeInfo} />
+      ) : (
+        <div className="m-5">
+          <RequestStatusInfo
+            errorMessage={treeInfoErrorMessage}
+            refreshButtonProps={{ onClick: () => setTreeInfoRefreshTrigger((v) => !v) }}
+            isSpinnerCentered={false}
+          />
+        </div>
+      )}
 
       <h3>Dataflow graph:</h3>
       <IntermediateView />
