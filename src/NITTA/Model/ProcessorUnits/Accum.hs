b2e2d30d4ee23673941a6c36ef8bee5f7e316286
Aleksandr Penskoi <aleksandr.penskoi@gmail.com>
2021-06-16 03:11:25 +0300

    Optimize Accum PU scheduling and locks

--- src/NITTA/Model/ProcessorUnits/Accum.hs
+++ src/NITTA/Model/ProcessorUnits/Accum.hs
@@ -29,9 +29,9 @@ module NITTA.Model.ProcessorUnits.Accum (
 import Control.Monad (when)
 import Data.Bifunctor
 import Data.Default
-import Data.List (find, partition, (\\))
+import qualified Data.List as L
 import Data.Maybe (fromMaybe)
-import Data.Set (elems, fromList, member)
+import qualified Data.Set as S
 import Data.String.Interpolate
 import Data.String.ToString
 import qualified Data.Text as T
@@ -57,59 +57,67 @@ import Prettyprinter
 data Job v x = Job
     { -- |Contains future parts expression to eval (c + d = e)
       tasks :: [[(Bool, v)]]
-    , -- |Contain current parts expression (a + b = c)
-      current :: [[(Bool, v)]]
     , -- |Func of this expression
       func :: F v x
-    , -- |Flag indicates when evaluation ended
-      calcEnd :: Bool
+    , state :: JobState
     }
-    deriving (Eq)
+
+data JobState
+    = Initialize
+    | WaitArguments
+    | Calculate
+    | WaitResults
+    | ArgumentAfterResult
+    deriving (Show)
+
+taskVars lst = S.fromList $ map snd lst
 
 instance (Var v) => Show (Job v x) where
-    show Job{tasks, current, func, calcEnd} =
-        [i|Job{tasks=#{ show' tasks }, current=#{ show' current }, func=#{ func }, calcEnd=#{ calcEnd }}|]
+    show Job{tasks, func, state} =
+        [i|Job{tasks=#{ show' tasks }, func=#{ func }, state=#{ state }}|]
         where
             show' = map (map (second toString))
 
 data Accum v x t = Accum
     { -- |List of jobs (expressions)
-      work :: [Job v x]
+      remainJobs :: [Job v x]
     , -- |Current job
-      currentWork :: Maybe (Job v x)
+      currentJob :: Maybe (Job v x)
     , -- |Process
       process_ :: Process t (StepInfo v x t)
-    , -- |Flag is indicated when new job starts
-      isInit :: Bool
     }
 
 instance (VarValTime v x t) => Pretty (Accum v x t) where
     pretty a =
         [__i|
             Accum:
-                work                 = #{ work a }
-                currentWork          = #{ currentWork a }
-                isInit               = #{ isInit a }
+                remainJobs: #{ remainJobs a }
+                currentJob: #{ currentJob a }
                 #{ indent 4 $ pretty $ process_ a }
             |]
+
+instance (VarValTime v x t) => Show (Accum v x t) where
+    show = show . pretty
 
 instance (VarValTime v x t) => Default (Accum v x t) where
     def =
         Accum
-            { work = []
-            , currentWork = Nothing
+            { remainJobs = []
+            , currentJob = Nothing
             , process_ = def
-            , isInit = True
             }
 
 instance Default x => DefaultX (Accum v x t) x
 
-tryBindJob f@Acc{actions} =
-    Job
-        { tasks = concat $ actionGroups actions
-        , current = []
-        , func = packF f
-        , calcEnd = False
+registerAcc f@Acc{actions} pu@Accum{remainJobs} =
+    pu
+        { remainJobs =
+            Job
+                { tasks = concat $ actionGroups actions
+                , func = packF f
+                , state = Initialize
+                } :
+            remainJobs
         }
 
 actionGroups [] = []
@@ -117,101 +125,95 @@ actionGroups as =
     let (pushs, as') = span isPush as
         (pulls, as'') = span isPull as'
      in [ map (\(Push sign (I v)) -> (sign == Minus, v)) pushs
-        , concatMap (\(Pull (O vs)) -> map (True,) $ elems vs) pulls
+        , concatMap (\(Pull (O vs)) -> map (True,) $ S.elems vs) pulls
         ] :
         actionGroups as''
 
-endpointOptionsJob Job{tasks = []} = []
-endpointOptionsJob Job{tasks = (t : _), current = []} = map snd t
-endpointOptionsJob Job{tasks = (t : ts), current = (c : _)}
-    | null (t \\ c) && null ts = []
-    | null $ t \\ c = map snd $ head ts
-    | otherwise = map snd $ t \\ c
+targetTask tasks
+    | even $ length tasks = Just $ head tasks
+    | otherwise = Nothing
 
-endpointDecisionJob j@Job{tasks = []} _ = j
-endpointDecisionJob j@Job{tasks = tasks@(t : _), current = []} v = j{tasks = updateTasks current' tasks, current = current'}
-    where
-        ([(neg, _)], _) = partition ((== v) . snd) t
-        current' = [[(neg, v)]]
-endpointDecisionJob j@Job{tasks = tasks@(t : ts), current = (c : cs)} v
-    | null $ t \\ c = endpointDecisionJob j{tasks = ts} v
-    | t \\ c /= t && length t > length c = j{tasks = updateTasks currentInsert tasks, current = currentInsert}
-    | otherwise = j{tasks = updateTasks currentAdd tasks, current = currentAdd}
-    where
-        (lst, _) = partition ((== v) . snd) t
-        currentInsert = (lst ++ c) : cs
-        currentAdd = lst : c : cs
-
-updateTasks (c : _) tasks@(t : ts)
-    | null $ t \\ c = ts
-    | otherwise = tasks
-updateTasks [] _ = error "Current is null"
-updateTasks _ _ = error "Matching error updateTasks"
-
-toTarget = even . length
-toSource = odd . length
+sourceTask tasks
+    | odd $ length tasks = Just $ head tasks
+    | otherwise = Nothing
 
 instance (VarValTime v x t, Num x) => ProcessorUnit (Accum v x t) v x t where
-    tryBind f pu@Accum{work}
-        | Just (Add a b c) <- castF f = Right pu{work = tryBindJob (Acc [Push Plus a, Push Plus b, Pull c]) : work}
-        | Just (Sub a b c) <- castF f = Right pu{work = tryBindJob (Acc [Push Plus a, Push Minus b, Pull c]) : work}
-        | Just f'@Acc{} <- castF f = Right pu{work = tryBindJob f' : work}
+    tryBind f pu
+        | Just (Add a b c) <- castF f =
+            Right $ registerAcc (Acc [Push Plus a, Push Plus b, Pull c]) pu
+        | Just (Sub a b c) <- castF f =
+            Right $ registerAcc (Acc [Push Plus a, Push Minus b, Pull c]) pu
+        | Just f'@Acc{} <- castF f =
+            Right $ registerAcc f' pu
         | otherwise = Left $ "The function is unsupported by Accum: " ++ show f
 
     process = process_
 
 instance (VarValTime v x t, Num x) => EndpointProblem (Accum v x t) v t where
-    endpointOptions pu@Accum{currentWork = Just a@Job{tasks, calcEnd}}
-        | toTarget tasks = targets
-        | toSource tasks = sources
-        where
-            tick = nextTick pu
-            targets = map (\v -> EndpointSt (Target v) $ TimeConstraint (tick + 1 ... maxBound) (singleton 1)) (endpointOptionsJob a)
-            sources = [EndpointSt (Source $ fromList (endpointOptionsJob a)) $ TimeConstraint (max tick (tickSource calcEnd) ... maxBound) (1 ... maxBound)]
-            tickSource True = tick + 1
-            tickSource _ = tick + 3
-    endpointOptions p@Accum{work, currentWork = Nothing} =
-        concatMap (\a -> endpointOptions p{currentWork = Just a}) work
-    endpointOptions _ = error "Error in matching in endpointOptions function"
+    endpointOptions pu@Accum{currentJob = Just Job{tasks, state}}
+        | Just task <- targetTask tasks =
+            let from = case state of
+                    ArgumentAfterResult -> nextTick pu + 1
+                    Initialize -> nextTick pu `withShift` 1
+                    _ -> nextTick pu
+             in map
+                    (\v -> EndpointSt (Target v) $ TimeConstraint (from ... maxBound) (singleton 1))
+                    $ S.elems $ taskVars task
+        | Just task <- sourceTask tasks =
+            let from = case state of
+                    Calculate -> nextTick pu + 2
+                    WaitResults -> nextTick pu + 1
+                    _ -> nextTick pu
+             in [EndpointSt (Source $ taskVars task) $ TimeConstraint (from ... maxBound) (1 ... maxBound)]
+    endpointOptions pu@Accum{remainJobs, currentJob = Nothing} =
+        concatMap (\j -> endpointOptions pu{currentJob = Just j}) remainJobs
+    endpointOptions pu = error [i|incorrect state for #{ pretty pu }|]
 
+    endpointDecision pu@Accum{remainJobs, currentJob = Nothing} d
+        | ([job], jobs') <- L.partition ((oneOf (variables d) `S.member`) . taskVars . head . tasks) remainJobs =
+            endpointDecision
+                pu
+                    { remainJobs = jobs'
+                    , currentJob = Just job
+                    }
+                d
     endpointDecision
-        pu@Accum{currentWork = Just j@Job{tasks}, isInit}
+        pu@Accum{currentJob = Just job@Job{tasks, state}}
         d@EndpointSt{epRole = Target v, epAt}
-            | not (null tasks) && toTarget tasks =
-                let job@Job{tasks = tasks', current = (((neg, _) : _) : _)} = endpointDecisionJob j v
-                    sel = if isInit then ResetAndLoad neg else Load neg
+            | Just task <- targetTask tasks =
+                let ([(neg, _v)], task') = L.partition ((== v) . snd) task
+                    instr = case state of
+                        Initialize -> ResetAndLoad neg
+                        _ -> Load neg
                     (_, process_') = runSchedule pu $ do
-                        scheduleEndpoint d $ scheduleInstructionUnsafe epAt sel
+                        scheduleEndpoint d $ scheduleInstructionUnsafe epAt instr
                  in pu
                         { process_ = process_'
-                        , currentWork = Just job{calcEnd = False}
-                        , isInit = null tasks'
+                        , currentJob = case (task', tail tasks) of
+                            ([], []) -> Nothing
+                            ([], tasks') -> Just job{tasks = tasks', state = Calculate}
+                            (_task', tasks') -> Just job{tasks = task' : tasks', state = WaitArguments}
                         }
     endpointDecision
-        pu@Accum{currentWork = Just j@Job{tasks, current, func}, process_}
-        d@EndpointSt{epRole = Source v, epAt}
-            | not (null current) && toSource tasks =
-                let job@Job{tasks = tasks'} = foldl endpointDecisionJob j (elems v)
-                    a = inf $ stepsInterval $ relatedEndpoints process_ $ variables func
+        pu@Accum{currentJob = Just job@Job{tasks, func}, process_}
+        d@EndpointSt{epRole = Source vs, epAt}
+            | Just task <- sourceTask tasks =
+                let (_, task') = L.partition ((`S.member` vs) . snd) task
                     (_, process_') = runSchedule pu $ do
-                        endpoints <- scheduleEndpoint d $ scheduleInstructionUnsafe (epAt -1) Out
-                        when (null tasks') $ do
+                        endpoints <- scheduleEndpoint d $ scheduleInstructionUnsafe (epAt - 1) Out
+                        when (null task' && length tasks == 1) $ do
+                            let endpoints' = relatedEndpoints process_ $ variables func
+                                a = inf $ stepsInterval endpoints'
+                                low = endpoints ++ map pID endpoints'
                             high <- scheduleFunction (a ... sup epAt) func
-                            let low = endpoints ++ map pID (relatedEndpoints process_ $ variables func)
                             establishVerticalRelations high low
-
-                        return endpoints
                  in pu
                         { process_ = process_'
-                        , currentWork = if null tasks' then Nothing else Just job{calcEnd = True}
-                        , isInit = null tasks'
+                        , currentJob = case (task', tail tasks) of
+                            ([], []) -> Nothing
+                            ([], tasks') -> Just job{tasks = tasks', state = ArgumentAfterResult}
+                            (_task', tasks') -> Just job{tasks = task' : tasks', state = WaitResults}
                         }
-    endpointDecision pu@Accum{work, currentWork = Nothing} d
-        | Just job <- getJob work =
-            endpointDecision pu{work = work \\ [job], currentWork = Just job{calcEnd = False}, isInit = True} d
-        where
-            getJob = find (\Job{func} -> d `isIn` func)
-            e `isIn` f = oneOf (variables e) `member` variables f
     endpointDecision pu d = error [i|incorrect decision #{ d } for #{ pretty pu }|]
 
 instance Connected (Accum v x t) where
@@ -259,20 +261,20 @@ instance UnambiguouslyDecode (Accum v x t) where
     decodeInstruction Out = def{oeSignal = True}
 
 instance (Var v) => Locks (Accum v x t) v where
-    locks Accum{currentWork = Nothing} = []
-    locks Accum{currentWork = Just job, work} = locks' job work
-        where
-            locks' Job{tasks = []} _ = []
-            locks' Job{current = []} _ = []
-            locks' Job{tasks = (m : ms), current = (r : _)} other =
-                [ Lock{lockBy, locked}
-                | locked <- concatMap (map snd) ms
-                , lockBy <- map snd (m \\ r)
+    locks Accum{currentJob = Nothing, remainJobs} = concatMap (locks . func) remainJobs
+    locks Accum{currentJob = Just Job{tasks = []}} = error "Accum locks: internal error"
+    locks Accum{currentJob = Just Job{tasks = t : ts}, remainJobs} =
+        let current =
+                [ Lock{locked, lockBy}
+                | locked <- S.elems $ unionsMap taskVars ts
+                , lockBy <- S.elems $ taskVars t
                 ]
-                    ++ [ Lock{lockBy, locked}
-                       | locked <- concatMap (concatMap (map snd) . tasks) other
-                       , lockBy <- map snd (m ++ r)
-                       ]
+            remain =
+                [ Lock{locked, lockBy}
+                | locked <- S.elems $ unionsMap (variables . func) remainJobs
+                , lockBy <- S.elems $ taskVars t
+                ]
+         in current ++ remain
 
 instance (VarValTime v x t) => TargetSystemComponent (Accum v x t) where
     moduleName _ _ = "pu_accum"
@@ -308,8 +310,8 @@ instance (VarValTime v x t) => TargetSystemComponent (Accum v x t) where
     hardwareInstance _title _pu _env = error "internal error"
 
 instance (Ord t) => WithFunctions (Accum v x t) (F v x) where
-    functions Accum{process_, work} =
-        functions process_ ++ map func work
+    functions Accum{process_, remainJobs} =
+        functions process_ ++ map func remainJobs
 
 instance (VarValTime v x t) => Testable (Accum v x t) v x where
     testBenchImplementation prj@Project{pName, pUnit} =
