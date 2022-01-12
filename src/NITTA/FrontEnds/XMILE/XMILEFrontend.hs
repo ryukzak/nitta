d0281ee886c7d4244e00593954a7333801abc8ed
ArturGogiyan <artur.gogiyan@gmail.com>
2022-01-12 15:22:05 +0300

    teacup implementation

--- src/NITTA/FrontEnds/XMILE/XMILEFrontend.hs
+++ src/NITTA/FrontEnds/XMILE/XMILEFrontend.hs
@@ -26,20 +26,35 @@ module NITTA.FrontEnds.XMILE.XMILEFrontend (
     TraceVar (..),
 ) where
 
+import Control.Monad.State
 import qualified Data.HashMap.Strict as HM
+import qualified Data.List as L
+import Data.Maybe (fromMaybe)
 import Data.String.Interpolate
 import qualified Data.Text as T
 import NITTA.FrontEnds.Common
 import NITTA.FrontEnds.XMILE.MathParser
+import NITTA.Intermediate.DataFlow
+import qualified NITTA.Intermediate.Functions as F
 import Text.XML.HXT.Arrow.ReadDocument
 import Text.XML.HXT.Arrow.XmlState
 import Text.XML.HXT.Core
+import NITTA.Utils (showText)
 
-data XMILEAlgBuilder x = XMILEAlgBuilder
-    { algSimSpecs :: XMILESimSpec
-    , algFlows :: [XMILEFlow]
-    , algAuxs :: [XMILEAux]
-    , algStocks :: [XMILEStock]
+data XMILEContent = XMILEContent
+    { xcSimSpecs :: XMILESimSpec
+    , xcFlows :: [XMILEFlow]
+    , xcAuxs :: [XMILEAux]
+    , xcStocks :: [XMILEStock]
+    }
+    deriving (Show)
+
+data XMILEAlgBuilder v x = XMILEAlgBuilder
+    { algDataFlowGraph :: DataFlowGraph v x
+    , algUsagesCount :: HM.HashMap T.Text Int
+    , algDefaultValues :: HM.HashMap T.Text Double
+    , algNextFreeNameIndex :: HM.HashMap T.Text Int
+    , algNextArgIndex :: Int
     }
     deriving (Show)
 
@@ -85,38 +100,187 @@ _xmileSample =
             |]
 
 xmile2functions src = do
-    [algBuilder] <- parseXMILEDocument src
-    let hm = getDefaultValues algBuilder
+    [xmContent] <- parseXMILEDocument src
+    return $ processXMILEGraph xmContent
+    where
+        processXMILEGraph xmContent = flip execState emptyBuilder $ do
+            getDefaultValuesAndUsages xmContent
+            createDataFlowGraph xmContent
+            where
+                emptyBuilder = XMILEAlgBuilder{algDataFlowGraph = DFCluster []
+                                             , algNextFreeNameIndex = HM.empty
+                                             , algDefaultValues = HM.empty
+                                             , algUsagesCount = HM.empty
+                                             , algNextArgIndex = 0
+                                             }
 
-    return hm
 
 --return FrontendResult{frDataFlow = undefined, frTrace = undefined, frPrettyLog = undefined frTrace}
 
-getDefaultValues algBuilder =
-    let hm = foldl addConstantAuxEquation HM.empty $ algAuxs algBuilder
-     in foldl addConstantStockEquation hm $ algStocks algBuilder
+createDataFlowGraph xmContent = do
+    x@XMILEAlgBuilder{algUsagesCount} <- get 
+    put x{algNextFreeNameIndex = foldl (\hm key -> HM.insert key 0 hm) HM.empty $ HM.keys algUsagesCount}
+    mapM_ processStock $ xcStocks xmContent
+    mapM_ processAux $ xcAuxs xmContent
+    mapM_ processFlow $ xcFlows xmContent
     where
-        addConstantAuxEquation hm aux
-            | isConstantEquation $ xaEquation aux = HM.insert (xaName aux) (getValue $ xaEquation aux) hm
-            | otherwise = hm
-        addConstantStockEquation hm stock
-            | isConstantEquation $ xsEquation stock = HM.insert (xsName stock) (getValue $ xsEquation stock) hm
-            | otherwise = hm
-        isConstantEquation (Val _) = True
-        isConstantEquation _ = False
-        getValue (Val value) = value
-        getValue _ = undefined
+        processStock XMILEStock{xsName, xsOutflow, xsInflow} = do
+            outputs <- getAllOutGraphNodes xsName
+            case (xsOutflow, xsInflow) of
+                (Nothing, Nothing) -> do
+                    x@XMILEAlgBuilder{algDataFlowGraph} <- get
+                    input <- getUniqueName xsName
+                    argIndex <- getNextArgIndex
+                    put x{algDataFlowGraph = addFuncToDataFlowGraph (F.loop argIndex input outputs) algDataFlowGraph}
+                    return()
+                (Nothing, Just _) ->
+                    return()
+                (Just outflow, Nothing) -> do
+                    x@XMILEAlgBuilder{algDataFlowGraph} <- get
+                    stockUniqueName <- getUniqueName xsName
+                    flowUniqueName <- getUniqueName outflow
+                    argIndex <- getNextArgIndex
+                    let tmpName = xsName <> "_tmp"
+                    let algDataFlowGraph' = addFuncToDataFlowGraph (F.sub stockUniqueName flowUniqueName [tmpName]) algDataFlowGraph
+                    put x{algDataFlowGraph = addFuncToDataFlowGraph (F.loop argIndex tmpName outputs) algDataFlowGraph'}
+                    return()
+                (Just _, Just _) ->
+                    return()
+
+            return ()
+
+        processAux XMILEAux{xaName, xaEquation} = do
+            case xaEquation of
+                (Val value) -> do
+                    x@XMILEAlgBuilder{algDataFlowGraph} <- get
+                    outputs <- getAllOutGraphNodes xaName
+                    put x{algDataFlowGraph = addFuncToDataFlowGraph (F.constant (round value) outputs) algDataFlowGraph}
+                    return ()
+                _ -> return () 
+
+        processFlow XMILEFlow{xfName, xfEquation} = do
+            x@XMILEAlgBuilder{algDataFlowGraph} <- get
+            (flowName,_) <- processFlowEquation xfEquation (1::Int)
+            outputs <- getAllOutGraphNodes xfName
+            argIndex <- getNextArgIndex
+            put x{algDataFlowGraph = addFuncToDataFlowGraph (F.loop argIndex flowName outputs) algDataFlowGraph}
+
+            return ()
+            where
+                processFlowEquation (Var name) index = do
+                    n <- getUniqueName $ T.pack name
+                    return (n, index)
+                processFlowEquation (Duo op leftExpr rightExpr) tempNameIndex = do
+                    (leftName, tempNameIndex') <- processFlowEquation leftExpr tempNameIndex
+                    (rightName, tempNameIndex'') <- processFlowEquation rightExpr tempNameIndex'
+                    let tmpName = xfName <> T.pack "_tmp" <> showText tempNameIndex''
+                    x@XMILEAlgBuilder{algDataFlowGraph} <- get
+                    case op of
+                        Add -> do
+                            put x{algDataFlowGraph = addFuncToDataFlowGraph (F.add leftName rightName [tmpName]) algDataFlowGraph}
+                        Sub -> do
+                            put x{algDataFlowGraph = addFuncToDataFlowGraph (F.sub leftName rightName [tmpName]) algDataFlowGraph}
+                        Mul -> do
+                            put x{algDataFlowGraph = addFuncToDataFlowGraph (F.multiply leftName rightName [tmpName]) algDataFlowGraph}
+                        Div -> do
+                            let divName = tmpName <> "div"
+                            put x{algDataFlowGraph = addFuncToDataFlowGraph (F.division leftName rightName [tmpName] [divName]) algDataFlowGraph}
+                    return (tmpName, tempNameIndex'' + 1)
+                processFlowEquation _ _ = undefined 
+
+
+        getUniqueName name = do
+            x@XMILEAlgBuilder{algNextFreeNameIndex} <- get
+            let index = fromMaybe (error $ "name not found : " <> T.unpack name) $ HM.lookup name algNextFreeNameIndex
+            put x{algNextFreeNameIndex = HM.insert name (index + 1) algNextFreeNameIndex}
+            return $ getGraphName name index
+
+        getAllOutGraphNodes name = do
+            XMILEAlgBuilder{algUsagesCount} <- get
+            let usages = fromMaybe (error $ "name not found : " <> T.unpack name) $ HM.lookup name algUsagesCount
+            return $ map (\num -> getGraphName name num) [0..usages]
+            
+        getNextArgIndex = do
+            x@XMILEAlgBuilder{algNextArgIndex} <- get
+            put x{algNextArgIndex = algNextArgIndex + 1}
+            return algNextArgIndex
+
+
+        getGraphName name index = name <> T.pack "#" <> showText index
+
+
+getDefaultValuesAndUsages algBuilder = do
+    mapM_ processStock $ xcStocks algBuilder
+    where
+        nameToEquationMap = flip execState HM.empty $ do
+            mapM_ (addToMap . (\a -> (xaName a, xaEquation a))) (xcAuxs algBuilder)
+            mapM_ (addToMap . (\a -> (xfName a, xfEquation a))) (xcFlows algBuilder)
+            mapM (addToMap . (\a -> (xsName a, xsEquation a))) (xcStocks algBuilder)
+            where
+                addToMap (name, eqn) = do
+                    hm <- get
+                    put $ HM.insert name eqn hm
+        processStock XMILEStock{xsEquation, xsName, xsInflow, xsOutflow} = do
+            let val = computeDefaultValue xsEquation
+            alg@XMILEAlgBuilder{algDefaultValues} <- get
+            put alg{algDefaultValues = HM.insert xsName val algDefaultValues}
+            processFlow xsInflow
+            processFlow xsOutflow
+
+            return ()
+            where
+                computeDefaultValue (Val v) = v
+                computeDefaultValue _ = undefined
+
+                processFlow Nothing = do return ()
+                processFlow (Just name) = do
+                    addUsages name
+                    processEquation $ xfEquation $ findFlow name
+
+                findFlow name =
+                    fromMaybe
+                        (error $ "cannot find expected flow flow with name " <> T.unpack name <> show (xcFlows algBuilder))
+                        (L.find (\XMILEFlow{xfName} -> xfName == name) $ xcFlows algBuilder)
+
+                processEquation (Val _) = do return ()
+                processEquation (Duo _ expr expl) = do
+                    processEquation expr
+                    processEquation expl
+                processEquation (Var name) = do
+                    addUsages $ T.pack name
+                    addDefaultValueIfNeeded
+                    where
+                        addDefaultValueIfNeeded = do
+                            x@XMILEAlgBuilder{algDefaultValues} <- get
+                            case HM.lookup (T.pack name) algDefaultValues of
+                                Just _ -> do return ()
+                                Nothing -> do
+                                    case HM.lookup (T.pack name) nameToEquationMap of
+                                        Just val -> do
+                                            put x{algDefaultValues = HM.insert (T.pack name) (computeDefaultValue val) algDefaultValues}
+                                        Nothing -> error $ "equation for name " <> name <> " not found."
+                            return ()
+
+                addUsages name = do
+                    x@XMILEAlgBuilder{algUsagesCount} <- get
+                    case HM.lookup name algUsagesCount of
+                        Just val -> do
+                            put x{algUsagesCount = HM.insert name (val + 1) algUsagesCount}
+                        Nothing -> do
+                            put x{algUsagesCount = HM.insert name 1 algUsagesCount}
+                    return ()
+
 
 parseXMILEDocument src =
     runX $
         readString [withValidate no] src
             >>> removeAllWhiteSpace
-            >>> proc state -> do
-                simSpec <- parseSimSpec -< state
-                flows <- parseFlows -< state
-                auxs <- parseAuxs -< state
-                stocks <- parseStocks -< state
-                returnA -< XMILEAlgBuilder{algSimSpecs = simSpec, algFlows = flows, algAuxs = auxs, algStocks = stocks}
+            >>> proc st -> do
+                simSpec <- parseSimSpec -< st
+                flows <- parseFlows -< st
+                auxs <- parseAuxs -< st
+                stocks <- parseStocks -< st
+                returnA -< XMILEContent{xcSimSpecs = simSpec, xcFlows = flows, xcAuxs = auxs, xcStocks = stocks}
 
 parseSimSpec =
     atTag "sim_specs"
@@ -169,7 +333,7 @@ parseStocks =
             (atTag name >>> text)
                 `orElse` arr (const "")
 
-replaceSpaces str = T.map repl str
+replaceSpaces str = T.filter (/='"') $ T.map repl str
     where
         repl ' ' = '_'
         repl c = c
