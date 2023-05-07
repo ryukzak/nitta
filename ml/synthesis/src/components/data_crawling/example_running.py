8f6b8c2fb7d470d851897d96ccb2815589b11cda
iburakov <speedwatson@gmail.com>
2023-05-07 23:37:56 +0000

    Disable tree coverage estimation
    Doesn't work for other examples and I've run out of time for that :(

--- ml/synthesis/src/components/data_crawling/example_running.py
+++ ml/synthesis/src/components/data_crawling/example_running.py
@@ -235,9 +235,10 @@ async def run_example_and_sample_tree_parallel(
     n_unique_sids = len(results_df.index.unique())  # already deduplicated
     clash_ratio = n_results / n_unique_sids - 1
     tree_cov = _estimate_tree_coverage_based_on_clash_ratio(clash_ratio, n_unique_sids)
+    tree_cov_str = f"{tree_cov * 100:.3f}%" if tree_cov else "unknown :("
     logger.info(
         f"TREE SAMPLING DONE: {n_unique_sids} unique nodes, clash ratio: {clash_ratio * 100:.3f}%, "
-        + f"estimated tree coverage: {tree_cov * 100:.3f}%"
+        + f"estimated tree coverage: {tree_cov_str}"
     )
     return n_unique_sids, clash_ratio
 
@@ -324,6 +325,10 @@ def _build_df_and_save_sampling_results(
     """
     Builds a DataFrame from the sampling results and saves it to the `data_dir`.
     """
+    logger.info(
+        f"Building a DataFrame from sampling results ({len(results)} entries)..."
+    )
+
     results_df = pd.DataFrame(results)
     sampling_run_id = datetime.now().strftime("%y%m%d_%H%M%S")
 
@@ -339,14 +344,17 @@ def _build_df_and_save_sampling_results(
     results_df = results_df[~results_df.index.duplicated(keep="first")]
     results_df.label = results_df_labels
 
-    results_df.to_csv(data_dir / f"{example_name}.sampling.{sampling_run_id}.csv")
+    output_fn = data_dir / f"{example_name}.sampling.{sampling_run_id}.csv"
+    results_df.to_csv(output_fn)
+
+    logger.info(f"Results saved to {output_fn}")
 
     return results_df
 
 
 def _estimate_tree_coverage_based_on_clash_ratio(
     clash_ratio: float, n_nodes: int
-) -> float:
+) -> Optional[float]:
     """
     This is a bit sketchy yet state-of-the-art ad hoc heuristic estimation of tree coverage based on clash ratio.
 
@@ -357,10 +365,19 @@ def _estimate_tree_coverage_based_on_clash_ratio(
 
     All approximation functions and coefficients are derived empirically from a set of sampling runs.
     All formulas are arbitrarily chosen to mathematically describe empirical data as close as my math skills allow.
+
+    Error is estimated to be up to 10-15%, but it may depend on the tree peculiarities.
+
+    UPD: found not suitable for large trees (leading to very small cron value) :(
+    More data and runs needed, but I'm out of time.
     """
-    cr = clash_ratio * 100  # clash ratio in percents
+    # clash ratio in percents with a small epsilon to avoid division by zero
+    cr = clash_ratio * 100
     n = n_nodes  # number of unique nodes sampled from the tree
     cron = cr / n  # short for "cr over n". like cr, but independent of n.
+
+    if cron < 0.02:
+        return None
 
     # nr - tree coverage, i.e. number of nodes sampled from the tree in this run
 
@@ -382,6 +399,6 @@ def _estimate_tree_coverage_based_on_clash_ratio(
     approx = approx_lt100 * (1 - blender_value) + approx_gt100 * blender_value
 
     # approx = cron * nr
-    approx_nr = approx / cron
+    approx_nr = max(0, min(1, approx / cron))
 
     return approx_nr
--- ml/synthesis/src/scripts/crawl_data_by_tree_sampling.py
+++ ml/synthesis/src/scripts/crawl_data_by_tree_sampling.py
@@ -17,7 +17,7 @@ if __name__ == "__main__":
     examples = [
         # "examples/fibonacci.lua",
         # "examples/spi2.lua",
-        "examples/counter.lua",
+        # "examples/counter.lua",
         # "examples/sum.lua",  # too slow
         # "examples/constantFolding.lua",  # too much memory needed
         # "examples/spi3.lua",  # too much memory needed
@@ -29,56 +29,8 @@ if __name__ == "__main__":
     else:
         logger.info(f"Used default examples {examples}")
 
-    results = []
-
-    for n_samples in sum(
-        [
-            [50000] * 1,
-            [25000] * 1,
-            [10000] * 1,
-            [5000] * 1,
-            [2500] * 1,
-            [2000] * 1,
-            [1500] * 1,
-            [1300] * 2,
-            [1200] * 2,
-            [1100] * 2,
-            [1000] * 3,
-            [900] * 3,
-            [800] * 4,
-            [700] * 4,
-            [600] * 5,
-            [500] * 5,
-            [400] * 6,
-            [300] * 6,
-            [250] * 7,
-            [200] * 7,
-            [150] * 8,
-            [100] * 8,
-            [75] * 8,
-            [50] * 8,
-            [25] * 8,
-            [15] * 8,
-            [10] * 8,
-            [5] * 8,
-            [1] * 8,
-        ],
-        [],
-    ):
-        (n_unique_sids, node_clash_percent) = asyncio.run(
-            run_example_and_sample_tree_parallel(
-                Path(examples[0]), n_samples=n_samples, n_workers=1, n_nittas=1
-            )
+    asyncio.run(
+        run_example_and_sample_tree_parallel(
+            Path(examples[0]), n_samples=500, n_workers=1, n_nittas=1
         )
-        logger.info(
-            f"n_samples,uniq,clash: {n_samples}\t{n_unique_sids}\t{node_clash_percent}"
-        )
-        results.append(
-            {
-                "n_samples": n_samples,
-                "n_unique_sids": n_unique_sids,
-                "node_clash_percent": node_clash_percent,
-            }
-        )
-
-    pd.DataFrame(results).to_csv(DATA_DIR / "treecov_estimates.csv", index=False)
+    )
