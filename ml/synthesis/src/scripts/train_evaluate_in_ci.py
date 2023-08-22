95ca917d52449c52b6445e9b2afb0091f389ab8b
iburakov <speedwatson@gmail.com>
2023-08-22 21:09:02 +0000

    Implement --skip-crawling arg for convenience

--- ml/synthesis/src/scripts/train_evaluate_in_ci.py
+++ ml/synthesis/src/scripts/train_evaluate_in_ci.py
@@ -1,6 +1,7 @@
 from __future__ import annotations
 
 import asyncio
+from argparse import ArgumentParser, Namespace
 
 from components.common.data_loading import load_all_existing_training_data
 from components.common.logging import configure_logging, get_logger
@@ -14,10 +15,26 @@ from consts import MODELS_DIR
 logger = get_logger(__name__)
 
 
+def _parse_args() -> Namespace:
+    argparser = ArgumentParser(
+        prog="train_evaluate_in_ci.py",
+    )
+    argparser.add_argument(
+        "--skip-crawling",
+        help="Do not crawl new training data, try to use existing.",
+        action="store_true",
+    )
+
+    return argparser.parse_args()
+
+
 if __name__ == "__main__":
     configure_logging()
 
-    asyncio.run(crawl_data_from_many_examples())
+    args = _parse_args()
+
+    if not args.skip_crawling:
+        asyncio.run(crawl_data_from_many_examples())
 
     model_dir = MODELS_DIR / "production"
     try:
