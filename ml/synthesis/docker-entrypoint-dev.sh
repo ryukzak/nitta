206c20e725139341698b15cf8a1ba77f200ff2b2
iburakov <speedwatson@gmail.com>
2023-08-29 18:31:19 +0000

    Update and improve the dev container Dockerfile
    - Update the Python tooling:
        - python installation is now managed by pyenv
        - dependencies are now managed by poetry
        - make pyenv, python, poetry, ruff, black, mypy and vulture
          available on PATH
    - Add markdownlint to the dev environment
    - Improve SSH security (forbid password auth by default)
    - Change SSH port to prevent conflicts and protect from bots if exposed
    - Remove tensorflow-gpu reinstall (obsolete for new versions)
    - Update the tensorflow version check to work with pyproject.toml
    - Extract stack/ghc/python versions and a resolver to more visible ARGs
    - Improve docker build layering to save some unnecessary rebuilds
    - Merge some of the fixes for aarch64, live tests pending
    - Fix some formatting and typos, clarify comments

--- ml/synthesis/docker-entrypoint-dev.sh
+++ ml/synthesis/docker-entrypoint-dev.sh
@@ -10,38 +10,27 @@ if [ ! -f "$ssh_key_location" ]; then
     echo "New key can be found at <repo_root>/$ssh_key_location, it will be automatically authorized in the container on every launch."
 fi
 mkdir -p "$HOME/.ssh"
-cat "$ssh_key_location.pub" >> ~/.ssh/authorized_keys
-
-jupyter_port=${JUPYTER_PORT:-8888} 
-jupyter_token_filepath=ml/synthesis/.dev/jupyter_token
-if [ ! -f "$jupyter_token_filepath" ]; then
-    jupyter_token=$(python3 -c "import secrets; print(secrets.token_hex(24))")
-    echo "$jupyter_token" > "$jupyter_token_filepath"
-else
-    jupyter_token=$(cat "$jupyter_token_filepath")
-fi
+# create authorized_keys if the file doesn't exist
+[ ! -f ~/.ssh/authorized_keys ] && touch ~/.ssh/authorized_keys
+# append the key to authorized_keys if it's not already there
+grep -qxF "$(cat "$ssh_key_location.pub")" ~/.ssh/authorized_keys || cat "$ssh_key_location.pub" >> ~/.ssh/authorized_keys
 
 # make profile settings available in this script
 [ -f ~/.profile ] && . ~/.profile
 
 # start a ssh server as a screen daemon
 screen -dmS sshd sudo -s /usr/sbin/sshd -D
-echo "SSH into this container if needed for remote debugging in IDEs: ssh -i $ssh_key_location -p 2222 -o "UserKnownHostsFile=/dev/null" -o "StrictHostKeyChecking=no" $(whoami)@localhost"
-
-# start a jupyter notebook server as a screen daemon
-screen -dmS jupyter-notebook jupyter notebook --port $jupyter_port --NotebookApp.token="$jupyter_token" --ip 0.0.0.0
-echo "Jupyter Notebook should be available at: http://localhost:$jupyter_port/?token=$jupyter_token"
+echo "SSH into this container for remote development in IDEs: ssh -i $ssh_key_location -p 31032 $(whoami)@localhost"
 
 # print info about GPUs available to Tensorflow
-python3 -c "import shutup, os; shutup.please(); os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'; import tensorflow as tf; gpu_count = len(tf.config.list_physical_devices('GPU')); print(f'Num of GPUs available to Tensorflow: {gpu_count}')"
+python -c "import shutup, os; shutup.please(); os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'; import tensorflow as tf; gpu_count = len(tf.config.list_physical_devices('GPU')); print(f'Num of GPUs available to Tensorflow: {gpu_count}')"
 
+# remove defunct screens
 screen -wipe > /dev/null
 
 echo "Available screens (attach with screen -r <name>):"
 screen -ls | sed '1d;$d'
 
-# remove the sudo tutorial on startup
-touch ~/.sudo_as_admin_successful
 
 # fallback to shell for interactivity
 TERM=xterm-256color /bin/bash
