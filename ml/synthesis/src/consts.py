ffa93028f8f30ab34b84643dc658cbfe66c0dca9
Aleksandr Penskoi <aleksandr.penskoi@gmail.com>
2024-01-07 23:29:13 +0100

    Refactoring: store envvar name in EnvVarNames

--- ml/synthesis/src/consts.py
+++ ml/synthesis/src/consts.py
@@ -9,7 +9,7 @@ from pathlib import Path
 
 class EnvVarNames:
     MODELS_DIR = "NITTA_ML_SYNTHESIS_MODELS_DIR"
-
+    NITTA_RUN_COMMAND_OVERRIDE = "NITTA_RUN_COMMAND_OVERRIDE"
 
 _examples_dir_name = "examples"
 
@@ -51,4 +51,4 @@ MODELS_DIR = Path(_models_dir_env) if _models_dir_env else ML_SYNTHESIS_DIR / "m
 ML_BACKEND_BASE_URL_FILEPATH = ".ml_backend_base_url"
 
 # high priority env var which overrides command provided in the config file
-NITTA_RUN_COMMAND_OVERRIDE = os.environ.get("NITTA_RUN_COMMAND", None)
+NITTA_RUN_COMMAND_OVERRIDE = os.environ.get(EnvVarNames.NITTA_RUN_COMMAND_OVERRIDE, None)
