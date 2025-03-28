95ef8d6732d2eb66c478c32275d098186d86c015
mopstream <golikov.andrei1@gmail.com>
2025-03-28 13:57:49 +0300

    Fixed compatibility for Translatable

--- src/NITTA/Frontends.hs
+++ src/NITTA/Frontends.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE ConstraintKinds #-}
 {- |
 Module      : NITTA.Frontends.FrontendIdentifier
 Description : Chooses a frontend based on source file extension or format
@@ -10,6 +11,7 @@ module NITTA.Frontends (
     FrontendType (..),
     FrontendResult (..),
     TraceVar (..),
+    Translatable,
     prettyLog,
     getTraceVarFormat,
     identifyFrontendType,
@@ -22,6 +24,10 @@ import NITTA.Frontends.Common
 import NITTA.Frontends.Lua
 import NITTA.Frontends.XMILE.Frontend
 import System.FilePath
+import Data.Text qualified as T
+import NITTA.Intermediate.Variable (Var)
+
+type Translatable a = (TranslatableLua a, TranslatableXMILE a)
 
 data FrontendType = Lua | XMILE
     deriving (Show, Data)
@@ -33,6 +39,6 @@ identifyFrontendType fileName frontendType = fromMaybe identifyByExtension front
                 ".lua" -> Lua
                 ".xmile" -> XMILE
                 ext -> error $ "unknown file extensions: " <> ext <> " for " <> fileName
-
+translate :: (Var v, Translatable x) => FrontendType -> T.Text -> FrontendResult v x
 translate Lua = translateLua
 translate XMILE = translateXMILE
