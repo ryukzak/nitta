d6105097903c1d452c56b305b2e034502ef386c4
mopstream <golikov.andrei1@gmail.com>
2025-05-09 20:14:47 +0300

    fix fx divider

--- hdl/div/div_mock.v
+++ hdl/div/div_mock.v
@@ -3,6 +3,7 @@
 module div
   #( parameter DATA_WIDTH = 32
    , parameter PIPELINE   = 4
+   , parameter SCALING_FACTOR_POWER = 0
    )
   ( input signed clock
   , input signed [DATA_WIDTH-1:0] denom
@@ -14,9 +15,12 @@ module div
 reg signed [DATA_WIDTH-1:0] buf_quotient[PIPELINE-1:0];
 reg signed [DATA_WIDTH-1:0] buf_remain  [PIPELINE-1:0];
 
+wire [DATA_WIDTH + SCALING_FACTOR_POWER:0] shifted_numer;
+assign shifted_numer = {numer, {SCALING_FACTOR_POWER{1'b0}}};
+
 integer i;
 always @(posedge clock) begin
-  buf_quotient[0] <= numer / denom;
+  buf_quotient[0] <= shifted_numer / denom;
   buf_remain[0] <= numer % denom;
   for ( i = 1; i < PIPELINE; i = i + 1 ) begin
     buf_quotient[i] <= buf_quotient[i-1];
