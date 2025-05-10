31e91f6f4e86f6eb618fc72724f0a535b87d08eb
mopstream <golikov.andrei1@gmail.com>
2025-05-10 12:43:27 +0300

    fix fx divider with negative values

--- hdl/div/div_mock.v
+++ hdl/div/div_mock.v
@@ -15,12 +15,19 @@ module div
 reg signed [DATA_WIDTH-1:0] buf_quotient[PIPELINE-1:0];
 reg signed [DATA_WIDTH-1:0] buf_remain  [PIPELINE-1:0];
 
-wire [DATA_WIDTH + SCALING_FACTOR_POWER:0] shifted_numer;
-assign shifted_numer = {numer, {SCALING_FACTOR_POWER{1'b0}}};
+wire sign;
+assign sign = (numer < 0) ^ (denom < 0);
+
+wire [DATA_WIDTH + SCALING_FACTOR_POWER-1:0] shifted_numer;
+assign shifted_numer = {numer < 0 ? -numer : numer, {SCALING_FACTOR_POWER{1'b0}}};
+
+wire [DATA_WIDTH-1:0] abs_denom;
+assign abs_denom = denom < 0 ? -denom : denom;
+
 
 integer i;
 always @(posedge clock) begin
-  buf_quotient[0] <= shifted_numer / denom;
+  buf_quotient[0] <= sign ? -(shifted_numer / abs_denom) : (shifted_numer / abs_denom);
   buf_remain[0] <= numer % denom;
   for ( i = 1; i < PIPELINE; i = i + 1 ) begin
     buf_quotient[i] <= buf_quotient[i-1];
