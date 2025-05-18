e55fa6c31b3ed2f25983a1359800c67e19012d79
Ckaf <borisnovoselov602@gmail.com>
2025-05-18 15:54:21 +0300

    add frontend for lut optimization

--- web/src/components/SubforestTables.tsx
+++ web/src/components/SubforestTables.tsx
@@ -33,6 +33,7 @@ export const SubforestTables: FC<SubforestTablesProps> = ({ nodes }) => {
     "BreakLoopView",
     "ConstantFoldingView",
     "OptimizeAccumView",
+    "OptimizeLutView",
     "ResolveDeadlockView",
   ];
 
@@ -101,7 +102,14 @@ export const SubforestTables: FC<SubforestTablesProps> = ({ nodes }) => {
       <Table
         name="Refactor"
         nodes={nodes.filter((e) =>
-          ["BreakLoopView", "ConstantFoldingView", "AllocationView", "ResolveDeadlockView"].includes(e.decision.tag),
+          [
+            "BreakLoopView",
+            "ConstantFoldingView",
+            "AllocationView",
+            "ResolveDeadlockView",
+            "OptimizeAccumView",
+            "OptimizeLutView",
+          ].includes(e.decision.tag),
         )}
         columns={[
           sidColumn(appContext.setSid),
